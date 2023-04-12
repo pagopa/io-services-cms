c2472b9bcd68213866395ac0fd06d9b8ba98c09c
Emanuele De Cupis <ema.decu@gmail.com>
2023-04-12 12:25:01 +0200

    fix typecheck

--- packages/fp-ts-fsm/vitest.config.ts
+++ packages/fp-ts-fsm/vitest.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    typecheck: { include: ["**/*.test-d.ts"], ignoreSourceErrors: true },
+    typecheck: {
+      ignoreSourceErrors: true,
+    },
   },
 });
