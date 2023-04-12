bde801b8a60a8aae26df522a90b1600adab3c864
Emanuele De Cupis <ema.decu@gmail.com>
2023-04-12 15:17:25 +0200

    configure linter

--- packages/io-services-cms-models/README.md
+++ packages/io-services-cms-models/README.md
@@ -1,2 +1,3 @@
-# Models
+# Models for IO-Services-CMS
+
 Define models for all the applications of the system. This is the module that implements schema, validation, and persistence for data.
---
+++ packages/models/.eslintrc.js
@@ -0,0 +1,13 @@
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["@pagopa/eslint-config/recommended"],
+  ignorePatterns: ["**/models/*.ts", "*.yaml", "**/*.test.ts"],
+  rules: {
+    "max-classes-per-file": "off",
+  },
+};
