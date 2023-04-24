8042530f282060af68c250c7a8af3df6a6015d54
Emanuele De Cupis <ema.decu@gmail.com>
2023-04-24 19:51:02 +0200

    refactor: routes in the same file

--- apps/io-services-cms-webapp/src/webservice/index.ts
+++ apps/io-services-cms-webapp/src/webservice/index.ts
@@ -3,7 +3,7 @@ import * as express from "express";
 import { secureExpressApp } from "@pagopa/io-functions-commons/dist/src/utils/express";
 import { setAppContext } from "@pagopa/io-functions-commons/dist/src/utils/middlewares/context_middleware";
 import createAzureFunctionHandler from "@pagopa/express-azure-functions/dist/src/createAzureFunctionsHandler";
-import router from "./routes";
+import { makeInfoHandler } from "./controllers/info";
 
 export const toAzureFunction =
   (app: express.Express): AzureFunction =>
@@ -13,9 +13,15 @@ export const toAzureFunction =
   };
 
 export const createWebServer = ({ basePath }: { basePath: string }) => {
+  // mount all routers on router
+  const router = express.Router();
+  router.get("/info", makeInfoHandler());
+
+  // configure app
   const app = express();
   secureExpressApp(app);
 
+  // mount router to respond on base path
   app.use(`/${basePath}`, router);
 
   return app;
--- apps/io-services-cms-webapp/src/webservice/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { makeInfoHandler } from "./controllers/info";
-
-const router = Router();
-
-// Info
-router.get("/info", makeInfoHandler());
-
-export default router;
