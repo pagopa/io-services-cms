85a9f7d81741a209e238276661aaedac893e6f75
Emanuele De Cupis <ema.decu@gmail.com>
2023-04-27 10:08:36 +0200

    Refactor: `webservice` module (#17)
    * refactor: create webservice submodule
    
    * refactor: link http entrypoint
    
    * fix reference
    
    * refactor: routes in the same file
    
    * refactor: move azure code in dedicated module
    
    * refactor: make handlers independent on express

--- apps/io-services-cms-webapp/src/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { makeInfoHandler } from "./functions/info";
-
-const router = Router();
-
-// Info
-router.get("/info", makeInfoHandler());
-
-export default router;
