756c7af8ee81d9fa49d4196b89c6fd386710c482
Emanuele De Cupis <ema.decu@gmail.com>
2023-04-27 11:58:32 +0200

    configure integration tests

--- packages/io-services-cms-models/vitest.config.ts
+++ packages/io-services-cms-models/vitest.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig, defaultExclude } from "vitest/config";
 
 export default defineConfig({
   test: {
+    exclude: [...defaultExclude, "__integrations__"],
     typecheck: {
       ignoreSourceErrors: true,
     },
