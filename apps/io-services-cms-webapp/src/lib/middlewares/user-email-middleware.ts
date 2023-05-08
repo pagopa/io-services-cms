4ca17960ca9877a378a96bce8691b8d5f7e7422d
Emanuele De Cupis <ema.decu@gmail.com>
2023-05-08 18:06:06 +0200

    add user email middleware

---
+++ apps/io-services-cms-webapp/src/lib/middlewares/user-email-middleware.ts
@@ -0,0 +1,37 @@
+import * as E from "fp-ts/lib/Either";
+
+import { EmailString } from "@pagopa/ts-commons/lib/strings";
+import {
+  ResponseErrorForbiddenNotAuthorized,
+  ResponseErrorInternal,
+} from "@pagopa/ts-commons/lib/responses";
+import * as O from "fp-ts/lib/Option";
+import { flow, pipe } from "fp-ts/lib/function";
+import { IRequestMiddleware } from "@pagopa/ts-commons/lib/request_middleware";
+
+/**
+ * Extracts the user email from the provided header.
+ * Block the request if the value is not provided or is not a valid email
+ *
+ * @returns either the user email or an error
+ */
+export const UserEmailMiddleware =
+  (): IRequestMiddleware<
+    "IResponseErrorForbiddenNotAuthorized" | "IResponseErrorInternal",
+    EmailString
+  > =>
+  async (request) =>
+    pipe(
+      // The user email will be passed in this header by the API Gateway
+      request.header("x-user-email"),
+      O.fromNullable,
+      E.fromOption(() => ResponseErrorForbiddenNotAuthorized),
+      E.chainW(
+        flow(
+          EmailString.decode,
+          E.mapLeft((_) =>
+            ResponseErrorInternal(`Failed to decode provided email`)
+          )
+        )
+      )
+    );
