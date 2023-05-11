7675e8e25917c053d4b5ce4ca6c2cf60dec46b31
Emanuele De Cupis <ema.decu@gmail.com>
2023-05-11 08:43:37 +0200

    this seems to solve

--- apps/io-services-cms-webapp/src/_to-solve.ts
+++
@@ -1,9 +0,0 @@
-/**
- * WARNING
- * ************************************************************************************
- * The line of code below avoids an unexplained error that occurs in the project build.
- * This file will be deleted as soon as we have a structured solution to the problem.
- */
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import * as TE from "fp-ts/lib/TaskEither";
-// ************************************************************************************
