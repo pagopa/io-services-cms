6435084327dcc00e5b662cb5fa3b4364f74b71ae
Giuseppe Di Pinto <giuseppeantonio.dipinto@pagopa.it>
2023-05-16 12:05:41 +0200

    Add ServicePublication FSM

---
+++ packages/io-services-cms-models/src/service-publication/index.ts
@@ -0,0 +1,292 @@
+import * as t from "io-ts";
+import * as TE from "fp-ts/TaskEither";
+import * as O from "fp-ts/Option";
+import * as E from "fp-ts/Either";
+import * as RA from "fp-ts/ReadonlyArray";
+import { ReaderTaskEither } from "fp-ts/lib/ReaderTaskEither";
+import { pipe, flow } from "fp-ts/function";
+import {
+  FSMStore,
+  StateMetadata,
+  Transition,
+  WithState,
+  StateSet,
+  EmptyState,
+} from "../lib/fsm";
+import { Service, ServiceId } from "../service-lifecycle/definitions";
+
+// utility type: turn a list of states into a record
+type ToRecord<Q> = Q extends []
+  ? {} // eslint-disable-line @typescript-eslint/ban-types
+  : Q extends [WithState<infer State, infer T>, ...infer Rest extends unknown[]]
+  ? { [K in State]: Omit<T, "fsm"> } & ToRecord<Rest>
+  : never;
+
+// commodity aliases
+type AllStateNames = keyof FSM["states"];
+type AllResults = States[number];
+
+// All the states admitted by the FSM
+type States = t.TypeOf<typeof States>;
+const States = t.tuple([
+  WithState("published", Service),
+  WithState("unpublished", Service),
+]);
+
+type Actions = {
+  override: { data: Service };
+  publish: void;
+  unpublish: void;
+};
+
+// helpers
+type Action<S extends keyof Actions> = [S, Actions[S]];
+type State<S extends keyof ToRecord<States>> = [S, ToRecord<States>[S]];
+
+// Definition of the FSM for service publication
+type FSM = {
+  states: StateSet<ToRecord<States>>;
+  transitions: [
+    Transition<Action<"override">, void, State<"unpublished">>,
+    Transition<Action<"override">, State<"unpublished">, State<"unpublished">>,
+    Transition<Action<"override">, State<"published">, State<"published">>,
+    Transition<Action<"publish">, State<"unpublished">, State<"published">>,
+    Transition<Action<"unpublish">, State<"published">, State<"unpublished">>
+  ];
+};
+
+// implementation
+const FSM: FSM = {
+  states: {
+    published: Service,
+    unpublished: Service,
+  },
+  transitions: [
+    {
+      id: "apply override on *",
+      action: "override",
+      from: "*",
+      to: "unpublished",
+      exec: ({ args: { data } }) =>
+        E.right({
+          ...data,
+          fsm: {
+            state: "unpublished",
+            lastTransition: "apply override on empty",
+          },
+        }),
+    },
+    {
+      id: "apply override on unpublished",
+      action: "override",
+      from: "unpublished",
+      to: "unpublished",
+      exec: ({ current, args: { data } }) =>
+        E.right({
+          ...current,
+          ...data,
+          fsm: {
+            state: "unpublished",
+            lastTransition: "apply override on unpublished",
+          },
+        }),
+    },
+    {
+      id: "apply override on published",
+      action: "override",
+      from: "published",
+      to: "published",
+      exec: ({ current, args: { data } }) =>
+        E.right({
+          ...current,
+          ...data,
+          fsm: {
+            state: "published",
+            lastTransition: "apply override on published",
+          },
+        }),
+    },
+    {
+      id: "apply publish on unpublished",
+      action: "publish",
+      from: "unpublished",
+      to: "published",
+      exec: ({ current }) =>
+        E.right({
+          ...current,
+          fsm: {
+            state: "published",
+            lastTransition: "apply publish on unpublished",
+          },
+        }),
+    },
+    {
+      id: "apply unpublish on published",
+      action: "unpublish",
+      from: "published",
+      to: "unpublished",
+      exec: ({ current }) =>
+        E.right({
+          ...current,
+          fsm: {
+            state: "unpublished",
+            lastTransition: "apply unpublish on published",
+          },
+        }),
+    },
+  ],
+};
+
+type PublicationStore = FSMStore<States[number]>;
+
+// TODO: apply function is meant to be agnostic on the FSM defintion.
+// Unfortunately, we didn't achieve the result yet, hence we opted for an actual implementation.
+// The algorithm itself is not related to the current FSM implementation, but the type system is
+/**
+ * Apply an action on the FSM.
+ *
+ * Expect a store as a dependency to persist element data.
+ *
+ * @example
+ * apply("create", "my-id", { data: aService })(myStore)
+ *
+ * @param appliedAction the action to apply
+ * @param id the id of the element
+ * @param args optional arguments, depending on the action definition
+ * @returns either an error or the element in the new state
+ */
+function apply(
+  appliedAction: "override",
+  id: ServiceId,
+  args: { data: Service }
+): ReaderTaskEither<
+  PublicationStore,
+  Error,
+  WithState<"published", Service> | WithState<"unpublished", Service>
+>;
+function apply(
+  appliedAction: "unpublish",
+  id: ServiceId
+): ReaderTaskEither<PublicationStore, Error, WithState<"unpublished", Service>>;
+function apply(
+  appliedAction: "publish",
+  id: ServiceId
+): ReaderTaskEither<PublicationStore, Error, WithState<"published", Service>>;
+function apply(
+  appliedAction: FSM["transitions"][number]["action"],
+  id: ServiceId,
+  args?: Parameters<FSM["transitions"][number]["exec"]>[number]["args"]
+): ReaderTaskEither<PublicationStore, Error, AllResults> {
+  return (store) => {
+    // select transitions for the action to apply
+    const applicableTransitions = FSM.transitions.filter(
+      ({ action }) => action === appliedAction
+    );
+    if (!applicableTransitions.length) {
+      return TE.left(
+        new Error(
+          `No transition has been declared for the action ${appliedAction}`
+        )
+      );
+    }
+
+    return pipe(
+      // fetch the item from the store by its id
+      store.fetch(id),
+
+      // filter transitions that can be applied to the current item status
+      TE.map(
+        flow(
+          // We can either find the element in the store or not
+          //   we have a O.Some(item) or a O.none respectively
+          //
+          // These two scenarios differ on how they match applicable transitions:
+          //    + on found item -> we must select transitions which have as "from" state
+          //                         the very same value of the one in the item.
+          //                       We also must decode the incoming item in order to provide
+          //                         the value to the exec() function.
+          //    + on no item    -> we must match only transitions starting from the empty state
+          O.fold(
+            () =>
+              pipe(
+                applicableTransitions,
+                // this filter is also a type guard to narrow possible from states to EmptyState only
+                RA.filter(
+                  <T extends FSM["transitions"][number]>(
+                    tr: T
+                  ): tr is T & { from: "*" } => tr.from === EmptyState
+                ),
+                // bind all data into a lazy implementation of exec
+                RA.map((tr) => ({
+                  exec: (): E.Either<Error, AllResults> =>
+                    // FIXME: avoid this forcing
+                    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                    // @ts-ignore
+                    tr.exec({ args }),
+                })),
+                // the following type lift is to keep the same interface with the Some branch
+                RA.map(E.right)
+              ),
+            (item) =>
+              pipe(
+                applicableTransitions,
+                // this filter is also a type guard to narrow possible from states to any state but EmptyState
+                RA.filter(
+                  <T extends FSM["transitions"][number]>(
+                    tr: T
+                  ): tr is T & { from: AllStateNames } => tr.from !== EmptyState
+                ),
+                RA.map((tr) =>
+                  pipe(
+                    // build the codec that will match an element in the "from" state
+                    t.intersection([
+                      FSM.states[tr.from],
+                      StateMetadata(tr.from),
+                    ]),
+                    // match&decode
+                    (codec) => codec.decode(item),
+
+                    // for those transitions whose from state has been matched,
+                    //  bind all data into a lazy implementation of exec
+                    E.map((current) => ({
+                      exec: (): E.Either<Error, AllResults> =>
+                        tr.exec({
+                          // FIXME: avoid this forcing
+                          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                          // @ts-ignore
+                          args,
+                          // FIXME: avoid this forcing
+                          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                          // @ts-ignore
+                          current,
+                        }),
+                    }))
+                  )
+                )
+              )
+          ),
+          // skim unmatched transitions
+          RA.filter(E.isRight)
+        )
+      ),
+      // avoid indeterminism: fail if more than a transition is applicable
+      TE.filterOrElse(
+        // we must have matched exactly ONE transition
+        (matchedTransitions) => matchedTransitions.length === 1,
+        (matchedTransitions) =>
+          matchedTransitions.length === 0
+            ? new Error("no transition matched")
+            : new Error("too many transitions")
+      ),
+      // apply the only transition to turn the element in the new state
+      TE.map((_) => _[0].right.exec()),
+      TE.chain(TE.fromEither),
+      // save new status in the store
+      TE.chain((newItem) => store.save(id, newItem))
+    );
+  };
+}
+
+type ItemType = t.TypeOf<typeof ItemType>;
+const ItemType = t.union(States.types);
+export { apply, FSM, ItemType };
