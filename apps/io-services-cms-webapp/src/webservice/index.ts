9088413e5aa36ed45babaf0e42a74136b8047c9f
Rodolfo Viti <62432865+rodoviti@users.noreply.github.com>
2023-05-18 17:21:09 +0200

    feat: [IOPAE-325] create review service controller (#58)
    * create review-service controller
    
    * add unit tests
    
    * changeset

--- apps/io-services-cms-webapp/src/webservice/index.ts
+++ apps/io-services-cms-webapp/src/webservice/index.ts
@@ -1,17 +1,21 @@
-import express from "express";
-import bodyParser from "body-parser";
 import { secureExpressApp } from "@pagopa/io-functions-commons/dist/src/utils/express";
 import { wrapRequestHandler } from "@pagopa/io-functions-commons/dist/src/utils/request_middleware";
+import bodyParser from "body-parser";
+import express from "express";
 
-import { pipe } from "fp-ts/lib/function";
-import { ServiceLifecycle, FSMStore } from "@io-services-cms/models";
 import { ApiManagementClient } from "@azure/arm-apimanagement";
+import { FSMStore, ServiceLifecycle } from "@io-services-cms/models";
+import { pipe } from "fp-ts/lib/function";
 import { IConfig } from "../config";
+import {
+  applyRequestMiddelwares as applyCreateServiceRequestMiddelwares,
+  makeCreateServiceHandler,
+} from "./controllers/create-service";
 import { makeInfoHandler } from "./controllers/info";
 import {
-  makeCreateServiceHandler,
-  applyRequestMiddelwares,
-} from "./controllers/create-service";
+  applyRequestMiddelwares as applyReviewServiceRequestMiddelwares,
+  makeReviewServiceHandler,
+} from "./controllers/review-service";
 
 type Dependencies = {
   basePath: string;
@@ -40,7 +44,18 @@ export const createWebServer = ({
         apimClient,
         apimConfig: config,
       }),
-      applyRequestMiddelwares,
+      applyCreateServiceRequestMiddelwares,
+      wrapRequestHandler
+    )
+  );
+
+  router.put(
+    "/services/:serviceId/review",
+    pipe(
+      makeReviewServiceHandler({
+        store: serviceLifecycleStore,
+      }),
+      applyReviewServiceRequestMiddelwares,
       wrapRequestHandler
     )
   );
