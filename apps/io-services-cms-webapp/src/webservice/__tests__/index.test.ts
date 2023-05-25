4c558e9f2d55c6d90b3eed8908540ab2751a7722
Giuseppe Di Pinto <giuseppeantonio.dipinto@pagopa.it>
2023-05-25 11:24:23 +0200

    Add unit tests

--- apps/io-services-cms-webapp/src/webservice/__tests__/index.test.ts
+++ apps/io-services-cms-webapp/src/webservice/__tests__/index.test.ts
@@ -160,6 +160,87 @@ describe("WebService", () => {
     });
   });
 
+  describe("deleteService", () => {
+    const aService = {
+      id: "aServiceId",
+      data: {
+        name: "aServiceName",
+        description: "aServiceDescription",
+        authorized_recipients: [],
+        max_allowed_payment_amount: 123,
+        metadata: {
+          address: "via tal dei tali 123",
+          email: "service@email.it",
+          pec: "service@pec.it",
+          scope: "LOCAL",
+        },
+        organization: {
+          name: "anOrganizationName",
+          fiscal_code: "12345678901",
+        },
+        require_secure_channel: false,
+      },
+    } as unknown as ServiceLifecycle.ItemType;
+
+    it("should fail when cannot find requested service", async () => {
+      const response = await request(app)
+        .delete("/api/services/nonExistentServiceId")
+        .send()
+        .set("x-user-email", "example@email.com")
+        .set("x-user-groups", UserGroup.ApiServiceWrite)
+        .set("x-user-id", "any-user-id")
+        .set("x-subscription-id", "any-subscription-id");
+
+      expect(response.statusCode).toBe(500); // FIXME: should be 404 (or 409)
+    });
+
+    it("should fail when requested operation in not allowed (transition's preconditions fails)", async () => {
+      serviceLifecycleStore.save(aService.id, {
+        ...aService,
+        fsm: { state: "deleted" },
+      });
+
+      const response = await request(app)
+        .delete(`/api/services/${aService.id}`)
+        .send()
+        .set("x-user-email", "example@email.com")
+        .set("x-user-groups", UserGroup.ApiServiceWrite)
+        .set("x-user-id", "any-user-id")
+        .set("x-subscription-id", "any-subscription-id");
+
+      expect(response.statusCode).toBe(500); // FIXME: should be 409
+    });
+
+    it("should not allow the operation without right group", async () => {
+      const response = await request(app)
+        .delete(`/api/services/${aService.id}`)
+        .send()
+        .set("x-user-email", "example@email.com")
+        .set("x-user-groups", "OtherGroup")
+        .set("x-user-id", "any-user-id")
+        .set("x-subscription-id", "any-subscription-id");
+
+      expect(response.statusCode).toBe(403);
+    });
+
+    it("should delete a service", async () => {
+      serviceLifecycleStore.save(aService.id, {
+        ...aService,
+        fsm: { state: "draft" },
+      });
+
+      const response = await request(app)
+        .delete(`/api/services/${aService.id}`)
+        .send()
+        .set("x-user-email", "example@email.com")
+        .set("x-user-groups", UserGroup.ApiServiceWrite)
+        .set("x-user-id", "any-user-id")
+        .set("x-subscription-id", "any-subscription-id");
+
+      expect(response.statusCode).toBe(204);
+    });
+  });
+
   describe("reviewService", () => {
     const aService = {
       id: "aServiceId",
