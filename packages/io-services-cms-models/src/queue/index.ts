f20c77b060e628b31bd3f45a6d2607bec425ee2f
Giuseppe Di Pinto <giuseppeantonio.dipinto@pagopa.it>
2023-05-25 16:13:56 +0200

    add new history queue item model

--- packages/io-services-cms-models/src/queue/index.ts
+++ packages/io-services-cms-models/src/queue/index.ts
@@ -3,3 +3,6 @@ import { Service } from "../service-lifecycle/definitions";
 
 export type RequestReviewItem = t.TypeOf<typeof RequestReviewItem>;
 export const RequestReviewItem = Service;
+
+export type RequestHistoryItem = t.TypeOf<typeof RequestHistoryItem>;
+export const RequestHistoryItem = Service;
