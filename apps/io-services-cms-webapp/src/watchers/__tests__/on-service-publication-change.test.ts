0ad9f3f1b40a8bb569178688806c12679f0c3c43
Giuseppe Di Pinto <giuseppeantonio.dipinto@pagopa.it>
2023-05-25 16:24:57 +0200

    add unit tests

---
+++ apps/io-services-cms-webapp/src/watchers/__tests__/on-service-publication-change.test.ts
@@ -0,0 +1,39 @@
+import { ServicePublication } from "@io-services-cms/models";
+import * as E from "fp-ts/lib/Either";
+import { describe, expect, it } from "vitest";
+import { handler } from "../on-service-publication-change";
+
+const aService = {
+  id: "aServiceId",
+  data: {
+    name: "aServiceName",
+    description: "aServiceDescription",
+    authorized_recipients: [],
+    max_allowed_payment_amount: 123,
+    metadata: {
+      address: "via tal dei tali 123",
+      email: "service@email.it",
+      pec: "service@pec.it",
+      scope: "LOCAL",
+    },
+    organization: {
+      name: "anOrganizationName",
+      fiscal_code: "12345678901",
+    },
+    require_secure_channel: false,
+  },
+} as unknown as ServicePublication.ItemType;
+
+describe("On Service Publication Change Handler", () => {
+  it.each`
+    scenario             | item                                              | expected
+    ${"request-history"} | ${{ ...aService, fsm: { state: "unpublished" } }} | ${{ requestHistory: aService }}
+    ${"request-history"} | ${{ ...aService, fsm: { state: "published" } }}   | ${{ requestHistory: aService }}
+  `("should map an item to a $scenario action", async ({ item, expected }) => {
+    const res = await handler({ item })();
+    expect(E.isRight(res)).toBeTruthy();
+    if (E.isRight(res)) {
+      expect(res.right).toStrictEqual(expected);
+    }
+  });
+});
