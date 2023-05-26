ab46c0cf5bcd2370a6c5bf08444e22e22813bcf6
Stefano.D'aversa <stefano.daversa@pagopa.it>
2023-05-26 16:04:45 +0200

    refactor controller tests

---
+++ apps/io-services-cms-webapp/src/webservice/controllers/__test__/unpublish-service.test.ts
@@ -0,0 +1,128 @@
+import { ApiManagementClient } from "@azure/arm-apimanagement";
+import {
+  ServiceLifecycle,
+  ServicePublication,
+  stores,
+} from "@io-services-cms/models";
+import { UserGroup } from "@pagopa/io-functions-commons/dist/src/utils/middlewares/azure_api_auth";
+import * as O from "fp-ts/lib/Option";
+import * as TE from "fp-ts/lib/TaskEither";
+import request from "supertest";
+import { afterEach, describe, expect, it, vi } from "vitest";
+import { IConfig } from "../../../config";
+import { createWebServer } from "../../index";
+
+vi.mock("../../lib/clients/apim-client", async () => {
+  const anApimResource = { id: "any-id", name: "any-name" };
+
+  return {
+    getProductByName: vi.fn((_) => TE.right(O.some(anApimResource))),
+    getUserByEmail: vi.fn((_) => TE.right(O.some(anApimResource))),
+    upsertSubscription: vi.fn((_) => TE.right(anApimResource)),
+  };
+});
+
+// memory implementation, for testing
+const serviceLifecycleStore =
+  stores.createMemoryStore<ServiceLifecycle.ItemType>();
+
+const servicePublicationStore =
+  stores.createMemoryStore<ServicePublication.ItemType>();
+
+const mockApimClient = {} as unknown as ApiManagementClient;
+const mockConfig = {} as unknown as IConfig;
+
+const aServicePub = {
+  id: "aServiceId",
+  data: {
+    name: "aServiceName",
+    description: "aServiceDescription",
+    authorized_recipients: [],
+    max_allowed_payment_amount: 123,
+    metadata: {
+      address: "via tal dei tali 123",
+      email: "service@email.it",
+      pec: "service@pec.it",
+      scope: "LOCAL",
+    },
+    organization: {
+      name: "anOrganizationName",
+      fiscal_code: "12345678901",
+    },
+    require_secure_channel: false,
+  },
+} as unknown as ServicePublication.ItemType;
+
+describe("WebService", () => {
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  const app = createWebServer({
+    basePath: "api",
+    apimClient: mockApimClient,
+    config: mockConfig,
+    serviceLifecycleStore,
+    servicePublicationStore,
+  });
+
+  describe("unPublishService", () => {
+    it("should fail when cannot find requested service", async () => {
+      const response = await request(app)
+        .delete("/api/services/s2/release")
+        .send()
+        .set("x-user-email", "example@email.com")
+        .set("x-user-groups", UserGroup.ApiServiceWrite)
+        .set("x-user-id", "any-user-id")
+        .set("x-subscription-id", "any-subscription-id");
+
+      expect(response.statusCode).toBe(500); // FIXME: should be 404 (or 409)
+    });
+
+    it("should fail when requested operation in not allowed (transition's preconditions fails)", async () => {
+      servicePublicationStore.save("s2", {
+        ...aServicePub,
+        fsm: { state: "unpublished" },
+      });
+
+      const response = await request(app)
+        .delete("/api/services/s2/release")
+        .send()
+        .set("x-user-email", "example@email.com")
+        .set("x-user-groups", UserGroup.ApiServiceWrite)
+        .set("x-user-id", "any-user-id")
+        .set("x-subscription-id", "any-subscription-id");
+
+      expect(response.statusCode).toBe(500); // FIXME: should be 409
+    });
+
+    it("should not allow the operation without right group", async () => {
+      const response = await request(app)
+        .delete("/api/services/s2/release")
+        .send()
+        .set("x-user-email", "example@email.com")
+        .set("x-user-groups", "OtherGroup")
+        .set("x-user-id", "any-user-id")
+        .set("x-subscription-id", "any-subscription-id");
+
+      expect(response.statusCode).toBe(403);
+    });
+
+    it("should unpublish a service", async () => {
+      servicePublicationStore.save("s2", {
+        ...aServicePub,
+        fsm: { state: "published" },
+      });
+
+      const response = await request(app)
+        .delete("/api/services/s2/release")
+        .send()
+        .set("x-user-email", "example@email.com")
+        .set("x-user-groups", UserGroup.ApiServiceWrite)
+        .set("x-user-id", "any-user-id")
+        .set("x-subscription-id", "any-subscription-id");
+
+      expect(response.statusCode).toBe(204);
+    });
+  });
+});
