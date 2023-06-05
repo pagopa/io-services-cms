38bbbf821db7ab3d08d2185c3588130944bb5f53
Rodolfo Viti <rodolfo.viti@pagopa.it>
2023-06-05 18:47:31 +0200

    refactor with FSM lifecycle client

--- apps/io-services-cms-webapp/src/webservice/controllers/create-service.ts
+++ apps/io-services-cms-webapp/src/webservice/controllers/create-service.ts
@@ -1,45 +1,44 @@
 import {
-  IResponseSuccessJson,
-  IResponseErrorNotFound,
-  IResponseErrorTooManyRequests,
-  IResponseErrorInternal,
-  IResponseErrorForbiddenNotAuthorized,
-  ResponseErrorInternal,
-  ResponseSuccessJson,
-} from "@pagopa/ts-commons/lib/responses";
-import { pipe } from "fp-ts/lib/function";
-import { RequiredBodyPayloadMiddleware } from "@pagopa/io-functions-commons/dist/src/utils/middlewares/required_body_payload";
+  ApiManagementClient,
+  SubscriptionContract,
+} from "@azure/arm-apimanagement";
+import { ServiceLifecycle } from "@io-services-cms/models";
+import { EmailAddress } from "@pagopa/io-functions-commons/dist/generated/definitions/EmailAddress";
 import {
   AzureApiAuthMiddleware,
   IAzureApiAuthorization,
   UserGroup,
 } from "@pagopa/io-functions-commons/dist/src/utils/middlewares/azure_api_auth";
-import { stores, ServiceLifecycle } from "@io-services-cms/models";
+import { RequiredBodyPayloadMiddleware } from "@pagopa/io-functions-commons/dist/src/utils/middlewares/required_body_payload";
 import { withRequestMiddlewares } from "@pagopa/io-functions-commons/dist/src/utils/request_middleware";
 import { ulidGenerator } from "@pagopa/io-functions-commons/dist/src/utils/strings";
-import {
-  ApiManagementClient,
-  SubscriptionContract,
-} from "@azure/arm-apimanagement";
-import { EmailAddress } from "@pagopa/io-functions-commons/dist/generated/definitions/EmailAddress";
-import { EmailString, NonEmptyString } from "@pagopa/ts-commons/lib/strings";
-import * as TE from "fp-ts/lib/TaskEither";
-import * as RTE from "fp-ts/lib/ReaderTaskEither";
-import { sequenceS } from "fp-ts/lib/Apply";
-import * as t from "io-ts";
 import { readableReport } from "@pagopa/ts-commons/lib/reporters";
-import { UserEmailMiddleware } from "../../lib/middlewares/user-email-middleware";
+import {
+  IResponseErrorForbiddenNotAuthorized,
+  IResponseErrorInternal,
+  IResponseErrorNotFound,
+  IResponseErrorTooManyRequests,
+  IResponseSuccessJson,
+  ResponseErrorInternal,
+  ResponseSuccessJson,
+} from "@pagopa/ts-commons/lib/responses";
+import { EmailString, NonEmptyString } from "@pagopa/ts-commons/lib/strings";
+import { sequenceS } from "fp-ts/lib/Apply";
+import * as TE from "fp-ts/lib/TaskEither";
+import { pipe } from "fp-ts/lib/function";
+import * as t from "io-ts";
+import { ApimConfig } from "../../config";
 import { ServiceLifecycle as ServiceResponsePayload } from "../../generated/api/ServiceLifecycle";
 import { ServicePayload as ServiceRequestPayload } from "../../generated/api/ServicePayload";
-import { ApimConfig } from "../../config";
 import {
   getProductByName,
   getUserByEmail,
   upsertSubscription,
 } from "../../lib/clients/apim-client";
+import { UserEmailMiddleware } from "../../lib/middlewares/user-email-middleware";
 import {
-  payloadToItem,
   itemToResponse,
+  payloadToItem,
 } from "../../utils/converters/service-lifecycle-converters";
 
 type HandlerResponseTypes =
@@ -56,8 +55,8 @@ type ICreateServiceHandler = (
 ) => Promise<HandlerResponseTypes>;
 
 type Dependencies = {
-  // A store od ServiceLifecycle objects
-  store: ReturnType<typeof stores.createCosmosStore<ServiceLifecycle.ItemType>>;
+  // An instance of ServiceLifecycle client
+  fsmLifecycleClient: ServiceLifecycle.FsmClient;
   // An instance of APIM Client
   apimClient: ApiManagementClient;
   // The APIM configuration
@@ -149,30 +148,33 @@ const createSubscriptionTask = (
 };
 
 export const makeCreateServiceHandler =
-  ({ store, apimClient, apimConfig }: Dependencies): ICreateServiceHandler =>
+  ({
+    fsmLifecycleClient,
+    apimClient,
+    apimConfig,
+  }: Dependencies): ICreateServiceHandler =>
   (_auth, userEmail, servicePayload) => {
     const serviceId = ulidGenerator();
 
     const createSubscriptionStep = pipe(
       createSubscriptionTask(apimClient, userEmail, serviceId, apimConfig),
-      RTE.fromTaskEither<Error, unknown, Dependencies["store"]>,
-      RTE.mapLeft((err) => ResponseErrorInternal(err.message))
+      TE.mapLeft((err) => ResponseErrorInternal(err.message))
     );
 
     const createServiceStep = pipe(
-      ServiceLifecycle.apply("create", serviceId, {
+      fsmLifecycleClient.create(serviceId, {
         data: payloadToItem(serviceId, servicePayload),
       }),
-      RTE.map(itemToResponse),
-      RTE.map(ResponseSuccessJson),
-      RTE.mapLeft((err) => ResponseErrorInternal(err.message))
+      TE.map(itemToResponse),
+      TE.map(ResponseSuccessJson),
+      TE.mapLeft((err) => ResponseErrorInternal(err.message))
     );
 
     return pipe(
       createSubscriptionStep,
-      RTE.chain((_) => createServiceStep),
-      RTE.toUnion
-    )(store)();
+      TE.chain((_) => createServiceStep),
+      TE.toUnion
+    )();
   };
 
 export const applyRequestMiddelwares = (handler: ICreateServiceHandler) =>
