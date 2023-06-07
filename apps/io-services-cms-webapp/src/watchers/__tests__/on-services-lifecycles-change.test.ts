2182fd3624f148c663c087faf3a67e7dc7fbd406
Stefano.D'aversa <stefano.daversa@pagopa.it>
2023-06-07 13:10:04 +0200

    update unit test

--- apps/io-services-cms-webapp/src/watchers/__tests__/on-services-lifecycles-change.test.ts
+++ apps/io-services-cms-webapp/src/watchers/__tests__/on-services-lifecycles-change.test.ts
@@ -1,4 +1,4 @@
-import { ServiceLifecycle } from "@io-services-cms/models";
+import { ServiceLifecycle, ServicePublication } from "@io-services-cms/models";
 import * as E from "fp-ts/lib/Either";
 import { describe, expect, it } from "vitest";
 import { handler } from "../on-services-lifecycles-change";
@@ -24,12 +24,33 @@ const aService = {
   },
 } as unknown as ServiceLifecycle.ItemType;
 
+const aPublicationService = {
+  id: "aServiceId",
+  data: {
+    name: "aServiceName",
+    description: "aServiceDescription",
+    authorized_recipients: [],
+    max_allowed_payment_amount: 1000000,
+    metadata: {
+      address: "via tal dei tali 123",
+      email: "service@email.it",
+      pec: "service@pec.it",
+      scope: "LOCAL",
+    },
+    organization: {
+      name: "anOrganizationName",
+      fiscal_code: "12345678901",
+    },
+    require_secure_channel: false,
+  },
+} as unknown as ServicePublication.ItemType;
+
 describe("On Service Lifecycle Change Handler", () => {
   it.each`
     scenario                  | item                                                                 | expected
     ${"request-review"}       | ${{ ...aService, version: "aVersion", fsm: { state: "submitted" } }} | ${{ requestReview: { ...aService, version: "aVersion" } }}
     ${"no-op (empty object)"} | ${{ ...aService, fsm: { state: "draft" } }}                          | ${{}}
-    ${"request-publication"}  | ${{ ...aService, fsm: { state: "approved" } }}                       | ${{ requestPublication: aService }}
+    ${"request-publication"}  | ${{ ...aService, fsm: { state: "approved" } }}                       | ${{ requestPublication: aPublicationService }}
     ${"no-op (empty object)"} | ${{ ...aService, fsm: { state: "rejected" } }}                       | ${{}}
     ${"no-op (empty object)"} | ${{ ...aService, fsm: { state: "deleted" } }}                        | ${{}}
   `("should map an item to a $scenario action", async ({ item, expected }) => {
