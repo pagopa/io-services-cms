8716d1771a3a80be4bbd0951e97c888f7f4e8df4
Gianmaria Scorza <gianmaria@scorza.dev>
2023-06-13 16:06:34 +0200

    add payloadToItem authorized_recipients test

--- apps/io-services-cms-webapp/src/utils/converters/__tests__/service-lifecycle-converters.test.ts
+++ apps/io-services-cms-webapp/src/utils/converters/__tests__/service-lifecycle-converters.test.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "vitest";
+import { test, expect, describe } from "vitest";
 import { ServiceLifecycle } from "@io-services-cms/models";
 import {
   itemToResponse,
@@ -13,29 +13,34 @@ const fsm: ServiceLifecycle.ItemType["fsm"] = {
   state: "rejected",
 };
 
-test("test service-lifecycle-converters", () => {
-  const result = toServiceStatus(fsm);
-  expect(result.reason).not.toBeDefined();
-});
+describe("test service-lifecycle-converters", () => {
+  test("test service-lifecycle-converters", () => {
+    const result = toServiceStatus(fsm);
+    expect(result.reason).not.toBeDefined();
+  });
 
-test("payload to item", () => {
-  const aNewService = {
-    name: "a service",
-    description: "a description",
-    organization: {
-      name: "org",
-      fiscal_code: "00000000000",
-    },
-    metadata: {
-      scope: "LOCAL",
-    },
-    authorized_recipients: ["BBBBBB99C88D555I"],
-  } as unknown as ServicePayload;
+  test("Authorized User should contains the default fiscal code and the one on request", () => {
+    const aNewService = {
+      name: "a service",
+      description: "a description",
+      organization: {
+        name: "org",
+        fiscal_code: "00000000000",
+      },
+      metadata: {
+        scope: "LOCAL",
+      },
+      authorized_recipients: ["BBBBBB99C88D555I"],
+    } as unknown as ServicePayload;
 
-  const result = payloadToItem(
-    "ffefefe" as NonEmptyString,
-    aNewService,
-    "AAAAAA00A00A000A" as FiscalCode
-  );
-  expect(result.data.authorized_recipients).toBeDefined();
+    const result = payloadToItem(
+      "ffefefe" as NonEmptyString,
+      aNewService,
+      "AAAAAA00A00A000A" as FiscalCode
+    );
+    expect(result.data.authorized_recipients).toBeDefined();
+    expect(result.data.authorized_recipients).toContain("AAAAAA00A00A000A");
+    expect(result.data.authorized_recipients).toContain("BBBBBB99C88D555I");
+    expect(result.data.authorized_recipients).toHaveLength(2);
+  });
 });
