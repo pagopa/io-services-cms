7fb6800974b4d01e0b4666ae795305768a861de2
Rodolfo Viti <rodolfo.viti@pagopa.it>
2023-06-14 19:11:40 +0200

    add runner subnet to fn allowed list

--- infra/src/_shared_vnet.tf
+++ infra/src/_shared_vnet.tf
@@ -27,6 +27,14 @@ data "azurerm_subnet" "private_endpoints_subnet" {
   resource_group_name  = var.vnet_common_rg
 }
 
+data "azurerm_subnet" "github_runner_subnet" {
+  count = local.is_prod ? 1 : 0
+
+  name                 = "io-p-github-runner-snet"
+  virtual_network_name = var.vnet_name
+  resource_group_name  = var.vnet_common_rg
+}
+
 data "azurerm_private_dns_zone" "privatelink_documents_azure_com" {
   count = local.is_prod ? 1 : 0
 
