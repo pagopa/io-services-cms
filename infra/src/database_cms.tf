3e50ac762f89a0b97dd8cf677c30121fe3b13de4
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2023-06-15 09:55:53 +0200

    fix cosmos config (#130)
    * fix cosmos
    
    * fix autoscale
    
    * fix
    
    * fix
    
    * fix max value
    
    * fix autoscale
    
    * fix doc
    
    * fix
    
    ---------
    
    Co-authored-by: pasqualedevita <>

--- infra/src/database_cms.tf
+++ infra/src/database_cms.tf
@@ -9,7 +9,7 @@ variable "cosmos_public_network_access_enabled" {
 }
 
 module "cosmosdb_account" {
-  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//cosmosdb_account?ref=v6.3.0"
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//cosmosdb_account?ref=v6.19.0"
 
   name                = "${local.project}-cosmos-${local.application_basename}"
   location            = azurerm_resource_group.rg.location
@@ -18,7 +18,6 @@ module "cosmosdb_account" {
   enable_free_tier    = false
   kind                = "GlobalDocumentDB"
 
-  # TODO: verify it's really needed
   domain = "${local.project}-${local.application_basename}"
 
   public_network_access_enabled     = var.cosmos_public_network_access_enabled
@@ -28,16 +27,20 @@ module "cosmosdb_account" {
   is_virtual_network_filter_enabled = false
 
   main_geo_location_location       = azurerm_resource_group.rg.location
-  main_geo_location_zone_redundant = false
+  main_geo_location_zone_redundant = true
+  enable_automatic_failover        = true
+
+  additional_geo_locations = [{
+    location          = "northeurope"
+    failover_priority = 1
+    zone_redundant    = false
+  }]
+
   consistency_policy = {
     consistency_level       = "Session"
     max_interval_in_seconds = null
     max_staleness_prefix    = null
   }
-
-  capabilities = [
-    "EnableServerless"
-  ]
 
   tags = var.tags
 }
@@ -54,20 +57,23 @@ locals {
     {
       name               = local.cosmos_containers.services_lifecycle
       partition_key_path = "/id"
+      max_throughput     = 1000
     },
     {
       name               = local.cosmos_containers.services_publication
       partition_key_path = "/id"
+      max_throughput     = 1000
     },
     {
       name               = local.cosmos_containers.services_history
       partition_key_path = "/serviceId"
+      max_throughput     = 1000
     },
   ]
 }
 
 module "db_cms_containers" {
-  source   = "git::https://github.com/pagopa/terraform-azurerm-v3.git//cosmosdb_sql_container?ref=v6.3.0"
+  source   = "git::https://github.com/pagopa/terraform-azurerm-v3.git//cosmosdb_sql_container?ref=v6.19.0"
   for_each = { for c in local.database_containers : c.name => c }
 
   name                = each.value.name
@@ -75,4 +81,5 @@ module "db_cms_containers" {
   account_name        = module.cosmosdb_account.name
   database_name       = azurerm_cosmosdb_sql_database.db_cms.name
   partition_key_path  = each.value.partition_key_path
+  autoscale_settings  = { max_throughput = each.value.max_throughput }
 }
