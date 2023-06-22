903d9440cf2aa7125929b4e75c172adfcb261c52
Gianmaria Scorza <gianmaria@scorza.dev>
2023-06-22 17:10:48 +0200

    refactor LEGACY COSMOSDB configuration

--- apps/io-services-cms-webapp/src/utils/cosmos-legacy.ts
+++ apps/io-services-cms-webapp/src/utils/cosmos-legacy.ts
@@ -8,9 +8,9 @@ import { getConfigOrThrow } from "../config";
 const config = getConfigOrThrow();
 
 // Setup DocumentDB
-export const cosmosDbUri = config.COSMOSDB_LEGACY_URI;
-export const cosmosDbName = config.COSMOSDB_LEGACY_NAME;
-export const cosmosDbKey = config.COSMOSDB_LEGACY_KEY;
+export const cosmosDbUri = config.LEGACY_COSMOSDB_URI;
+export const cosmosDbName = config.LEGACY_COSMOSDB_NAME;
+export const cosmosDbKey = config.LEGACY_COSMOSDB_KEY;
 
 export const cosmosdbClient = new CosmosClient({
   endpoint: cosmosDbUri,
