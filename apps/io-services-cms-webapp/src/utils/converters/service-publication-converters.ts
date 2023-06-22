fc85455d3883facad3ba4f7511ddc445fb8074b4
Gianmaria Scorza <gianmaria@scorza.dev>
2023-06-22 13:03:55 +0200

    handle metadata.category and metadata.custom_special_flow param in converters

--- apps/io-services-cms-webapp/src/utils/converters/service-publication-converters.ts
+++ apps/io-services-cms-webapp/src/utils/converters/service-publication-converters.ts
@@ -1,4 +1,5 @@
 import { ServicePublication } from "@io-services-cms/models";
+import { CategoryEnum } from "../../generated/api/ServiceMetadata";
 import { ServicePublication as ServiceResponsePayload } from "../../generated/api/ServicePublication";
 import {
   ServicePublicationStatusType,
@@ -16,7 +17,11 @@ export const itemToResponse = ({
   name: data.name,
   description: data.description,
   organization: data.organization,
-  metadata: { ...data.metadata, scope: toScopeType(data.metadata.scope) },
+  metadata: {
+    ...data.metadata,
+    scope: toScopeType(data.metadata.scope),
+    category: toCategoryType(data.metadata.category),
+  },
 });
 
 export const toServiceStatusType = (
@@ -30,5 +35,17 @@ export const toServiceStatusType = (
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
       const _: never = s;
       return ServicePublicationStatusTypeEnum[s];
+  }
+};
+
+export const toCategoryType = (
+  s: ServicePublication.ItemType["data"]["metadata"]["category"]
+): CategoryEnum => {
+  switch (s) {
+    case "STANDARD":
+    case "SPECIAL":
+      return CategoryEnum[s];
+    default:
+      return CategoryEnum.STANDARD;
   }
 };
