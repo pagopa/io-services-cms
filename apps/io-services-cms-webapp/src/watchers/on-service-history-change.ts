62e4c9d38804a08b8fe5e7521764a7066684342b
Rodolfo Viti <rodolfo.viti@pagopa.it>
2023-06-23 18:25:04 +0200

    setup ServiceHistoryWatcher az fn

---
+++ apps/io-services-cms-webapp/src/watchers/on-service-history-change.ts
@@ -0,0 +1,43 @@
+import {
+  Queue,
+  ServiceHistory,
+  ServicePublication,
+} from "@io-services-cms/models";
+import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
+import * as E from "fp-ts/lib/Either";
+import * as RE from "fp-ts/lib/ReaderEither";
+import { pipe } from "fp-ts/lib/function";
+
+type Actions = "requestSyncLegacy";
+
+type NoAction = typeof noAction;
+type Action<A extends Actions, B> = Record<A, B>;
+type RequestSyncLegacyAction = Action<
+  "requestSyncLegacy",
+  Queue.RequestSyncLegacyItem
+>;
+const noAction = {};
+
+const onReleaseHandler = (
+  item: ServicePublication.ItemType
+): RequestSyncLegacyAction => ({
+  requestSyncLegacy: {
+    ...item,
+    last_update:
+      item.last_update ?? (new Date().toISOString() as NonEmptyString), // last_update fallback (value is always set by persistence layer) TODO add log
+  },
+});
+
+export const handler: RE.ReaderEither<
+  { item: ServiceHistory.ServiceHistory },
+  Error,
+  NoAction | RequestSyncLegacyAction
+> = ({ item }) => {
+  switch (item.fsm.state) {
+    case "unpublished":
+    case "published":
+      return pipe(item, onReleaseHandler, E.right);
+    default:
+      return E.right(noAction);
+  }
+};
