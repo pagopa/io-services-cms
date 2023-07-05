3e58239551576f6c6473feb96bba17de5817950d
Rodolfo Viti <rodolfo.viti@pagopa.it>
2023-07-05 15:30:07 +0200

    fix legacy-to-cms mapping

--- apps/io-services-cms-webapp/src/watchers/on-service-history-change.ts
+++ apps/io-services-cms-webapp/src/watchers/on-service-history-change.ts
@@ -24,44 +24,75 @@ const noAction = {};
 
 const cmsToLegacy = (
   serviceHistory: ServiceHistory
-): Queue.RequestSyncLegacyItem => ({
-  authorizedCIDRs: toAuthorizedCIDRs(serviceHistory.data.authorized_cidrs),
-  authorizedRecipients: toAuthorizedRecipients(
-    serviceHistory.data.authorized_recipients
-  ),
-  departmentName: serviceHistory.data.organization
-    .department_name as NonEmptyString,
-  isVisible: serviceHistory.fsm.state === "published",
-  maxAllowedPaymentAmount: serviceHistory.data.max_allowed_payment_amount,
-  organizationFiscalCode: serviceHistory.data.organization.fiscal_code,
-  organizationName: serviceHistory.data.organization.name,
-  requireSecureChannels: serviceHistory.data.require_secure_channel,
-  serviceId: serviceHistory.serviceId,
-  serviceName: serviceHistory.data.name,
-  cmsTag: true,
-  serviceMetadata: {
-    category:
-      (serviceHistory.data.metadata.category as
-        | StandardServiceCategoryEnum
-        | SpecialServiceCategoryEnum) ?? StandardServiceCategoryEnum.STANDARD,
-    scope: serviceHistory.data.metadata.scope as ServiceScopeEnum,
-    customSpecialFlow: serviceHistory.data.metadata
-      .custom_special_flow as undefined,
-    address: serviceHistory.data.metadata.address,
-    appAndroid: serviceHistory.data.metadata.app_android,
-    appIos: serviceHistory.data.metadata.app_ios,
-    cta: serviceHistory.data.metadata.cta,
-    description: serviceHistory.data.description,
-    email: serviceHistory.data.metadata.email,
-    pec: serviceHistory.data.metadata.pec,
-    phone: serviceHistory.data.metadata.phone,
-    privacyUrl: serviceHistory.data.metadata.privacy_url,
-    supportUrl: serviceHistory.data.metadata.support_url,
-    tokenName: serviceHistory.data.metadata.token_name,
-    tosUrl: serviceHistory.data.metadata.tos_url,
-    webUrl: serviceHistory.data.metadata.web_url,
-  },
-});
+): Queue.RequestSyncLegacyItem => {
+  const legacyServiceBase = {
+    authorizedCIDRs: toAuthorizedCIDRs(serviceHistory.data.authorized_cidrs),
+    authorizedRecipients: toAuthorizedRecipients(
+      serviceHistory.data.authorized_recipients
+    ),
+    departmentName: serviceHistory.data.organization
+      .department_name as NonEmptyString, // it will be validated in OnRequestSyncLegacy Azure Function
+    isVisible: serviceHistory.fsm.state === "published",
+    maxAllowedPaymentAmount: serviceHistory.data.max_allowed_payment_amount,
+    organizationFiscalCode: serviceHistory.data.organization.fiscal_code,
+    organizationName: serviceHistory.data.organization.name,
+    requireSecureChannels: serviceHistory.data.require_secure_channel,
+    serviceId: serviceHistory.serviceId,
+    serviceName: serviceHistory.data.name,
+    cmsTag: true,
+    serviceMetadata: {
+      scope: ServiceScopeEnum[serviceHistory.data.metadata.scope],
+      address: serviceHistory.data.metadata.address,
+      appAndroid: serviceHistory.data.metadata.app_android,
+      appIos: serviceHistory.data.metadata.app_ios,
+      cta: serviceHistory.data.metadata.cta,
+      description: serviceHistory.data.description,
+      email: serviceHistory.data.metadata.email,
+      pec: serviceHistory.data.metadata.pec,
+      phone: serviceHistory.data.metadata.phone,
+      privacyUrl: serviceHistory.data.metadata.privacy_url,
+      supportUrl: serviceHistory.data.metadata.support_url,
+      tokenName: serviceHistory.data.metadata.token_name,
+      tosUrl: serviceHistory.data.metadata.tos_url,
+      webUrl: serviceHistory.data.metadata.web_url,
+    },
+  };
+  return {
+    ...legacyServiceBase,
+    serviceMetadata: {
+      ...legacyServiceBase.serviceMetadata,
+      ...getSpecialFields(
+        serviceHistory.data.metadata.category,
+        serviceHistory.data.metadata.custom_special_flow
+      ),
+    },
+  };
+};
+
+const getSpecialFields = (
+  cat?: "STANDARD" | "SPECIAL",
+  customSpecialFlow?: string
+):
+  | {
+      category: SpecialServiceCategoryEnum;
+      customSpecialFlow: NonEmptyString;
+    }
+  | {
+      category: StandardServiceCategoryEnum;
+      customSpecialFlow: undefined;
+    } => {
+  if (cat && cat === SpecialServiceCategoryEnum.SPECIAL) {
+    return {
+      category: SpecialServiceCategoryEnum.SPECIAL,
+      customSpecialFlow: customSpecialFlow as NonEmptyString, // it will be validated in OnRequestSyncLegacy Azure Function
+    };
+  } else {
+    return {
+      category: StandardServiceCategoryEnum.STANDARD,
+      customSpecialFlow: customSpecialFlow as undefined, // it will be validated in OnRequestSyncLegacy Azure Function
+    };
+  }
+};
 
 const toRequestSyncLegacyAction = (
   serviceHistory: ServiceHistory
