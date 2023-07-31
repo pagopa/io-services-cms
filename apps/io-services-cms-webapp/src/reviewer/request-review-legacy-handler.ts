70405444679008aa24262a91d6348facbde48833
Gianmaria Scorza <gianmaria@scorza.dev>
2023-07-31 16:48:25 +0200

    fix handler

--- apps/io-services-cms-webapp/src/reviewer/request-review-legacy-handler.ts
+++ apps/io-services-cms-webapp/src/reviewer/request-review-legacy-handler.ts
@@ -1,15 +1,15 @@
 import { Context } from "@azure/functions";
 import { Queue, ServiceLifecycle } from "@io-services-cms/models";
 import { readableReport } from "@pagopa/ts-commons/lib/reporters";
+import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import * as E from "fp-ts/lib/Either";
-import * as TE from "fp-ts/lib/TaskEither";
 import * as O from "fp-ts/lib/Option";
+import * as TE from "fp-ts/lib/TaskEither";
 import { flow, pipe } from "fp-ts/lib/function";
 import { Json } from "io-ts-types";
-import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import { withJsonInput } from "../lib/azure/misc";
-import { SYNC_FROM_LEGACY } from "../utils/synchronizer";
 import { ServiceReviewDao } from "../utils/service-review-dao";
+import { SYNC_FROM_LEGACY } from "../utils/synchronizer";
 
 const parseIncomingMessage = (
   queueItem: Json
@@ -29,16 +29,15 @@ export const handleQueueItem = (
   pipe(
     queueItem,
     parseIncomingMessage,
-    E.mapLeft((_) => new Error("Error while parsing incoming message")), // TODO: map as _permanent_ error
     TE.fromEither,
-    TE.map((requestReviewLegacy) => {
-      // execute operations
+    TE.chain((requestReviewLegacy) =>
       pipe(
         requestReviewLegacy.serviceId,
         fsmLifecycleClient.fetch,
-        TE.chainW(
-          flow(
-            O.foldW(
+        TE.chain((service) =>
+          pipe(
+            service,
+            O.fold(
               () =>
                 TE.left(
                   new Error(
@@ -58,23 +57,21 @@ export const handleQueueItem = (
           )
         ),
         // 2. create entry in service review legacy table
-        TE.chain((_) => createLegacyReviewEntry(requestReviewLegacy, dao)),
-        TE.bimap(
-          (err) => {
-            context.log.error(
-              `[RequestReviewLegacyHandler] - Item ${requestReviewLegacy.serviceId} failed for reason ${err.message}`
-            );
-            return err;
-          },
-          (_) => {
-            context.log.info(
-              `[RequestReviewLegacyHandler] - Item ${requestReviewLegacy.serviceId} processed`
-            );
-            return void 0;
-          }
+        TE.chain((_) =>
+          pipe(
+            dao.insert({
+              service_id: requestReviewLegacy.serviceId,
+              service_version: "0.0.1" as NonEmptyString, // TODO: how to value this field?
+              ticket_id: "" as NonEmptyString, // TODO: how to value this field?
+              ticket_key: requestReviewLegacy.ticketKey,
+              status: "PENDING",
+              extra_data: {},
+            }),
+            TE.mapLeft(E.toError)
+          )
         )
-      );
-    }),
+      )
+    ),
     TE.getOrElse((e) => {
       throw e;
     })
@@ -90,22 +87,6 @@ const setToSubmitted = (
     lastTransition: SYNC_FROM_LEGACY,
   },
 });
-const createLegacyReviewEntry = (
-  requestReviewLegacy: Queue.RequestReviewLegacyItem,
-  dao: ServiceReviewDao
-) =>
-  pipe(
-    {
-      service_id: requestReviewLegacy.serviceId,
-      service_version: "0.0.1" as NonEmptyString, // TODO: how to value this field?
-      ticket_id: "" as NonEmptyString, // TODO: how to value this field?
-      ticket_key: requestReviewLegacy.ticketKey,
-      status: "PENDING",
-    },
-    dao.insert,
-    TE.map((_) => void 0),
-    TE.mapLeft((e) => new Error(`Error while inserting legacy review: ${e}`))
-  );
 
 export const createRequestReviewLegacyHandler = (
   fsmLifecycleClient: ServiceLifecycle.FsmClient,
