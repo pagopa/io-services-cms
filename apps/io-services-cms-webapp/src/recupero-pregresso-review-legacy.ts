1b482021e05702267d2bcfba432f777150c6a62d
Gianmaria Scorza <gianmaria@scorza.dev>
2023-08-07 15:54:48 +0200

    firt version

--- apps/io-services-cms-webapp/src/recupero-pregresso-review-legacy.ts
+++ apps/io-services-cms-webapp/src/recupero-pregresso-review-legacy.ts
@@ -1,54 +1,132 @@
+/* eslint-disable functional/immutable-data */
 /* eslint-disable no-console */
 /* eslint-disable functional/no-let */
+import { Queue } from "@io-services-cms/models";
+import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import * as E from "fp-ts/Either";
+import * as RA from "fp-ts/ReadonlyArray";
+import * as TE from "fp-ts/TaskEither";
+import * as O from "fp-ts/lib/Option";
+import { pipe } from "fp-ts/lib/function";
 import { getConfigOrThrow } from "./config";
+import { getApimClient, getSubscription } from "./lib/clients/apim-client";
 import {
   JiraLegacyIssue,
   jiraLegacyClient,
 } from "./lib/clients/jira-legacy-client";
-import { RequestReviewLegacyItem } from "@io-services-cms/models/queue";
+
+const SERVICE_ID_PREFIX = "devportal-service-";
 
 const config = getConfigOrThrow();
 const jClient = jiraLegacyClient(config);
+const apimClient = getApimClient(config, config.AZURE_SUBSCRIPTION_ID);
 
-const recupera = async () => {
+const start = async () => {
   let startAt = 0;
   let maxResults = 1;
-  let messages: unknown = [];
+  const messages: unknown[] = [];
+
+  console.log(`PAST RECOVERY PROCEDURE STARTING...`);
 
   while (startAt < maxResults) {
     const response = await jClient.searchJiraNewReviewRejectedIssuesPaginated(
       startAt
     )();
 
+    console.log(
+      `PAST RECOVERY PROCEDURE CHUNK JIRA RETRIEVED WITH startAt: ${startAt}, maxResults: ${maxResults}`
+    );
+
     if (E.isLeft(response)) {
       throw response.left;
     }
+
     const issues = response.right.issues;
 
-    issues.forEach((issue) => {
-      // build message
-      messages.add(buildQueueMessage(issue));
+    issues.forEach(async (issue) => {
+      // extract serviceId
+      const serviceId = extractServiceId(issue);
+
+      if (O.isNone(serviceId)) {
+        console.log(`Issue ${issue.key} has no serviceId label, ERROR...`);
+        throw new Error(`Issue ${issue.key} has no serviceId label, ERROR...`);
+      }
+
+      // retrieve apimUserId calling apim
+      const apimUserId = await retrieveApimUserId(serviceId.value)();
+
+      if (E.isLeft(apimUserId)) {
+        console.log(`Issue ${issue.key} has no apimUserId, ERROR...`);
+        throw new Error(`Issue ${issue.key} has no apimUserId, ERROR...`);
+      }
+
+      // build message and push it into messages array
+      messages.push(
+        buildQueueMessage(issue, serviceId.value, apimUserId.right)
+      );
     });
+
+    console.log(
+      `PAST RECOVERY PROCEDURE CHUNK JIRA PROCESSED WITH SUCCES startAt: ${startAt}, maxResults: ${maxResults}`
+    );
 
     maxResults = response.right.total;
     startAt += 100;
   }
-};
-const buildQueueMessage = (issue: JiraLegacyIssue): RequestReviewLegacyItem => {
-  return {
-    isNewTicket: true,
-    serviceId: extractServiceId(issue),
-    ticketId: NonEmptyString,
-    ticketKey: NonEmptyString,
-    apimUserId: NonEmptyString,
-  };
+
+  console.log(
+    `PAST RECOVERY PROCEDURE ALL JIRA ITEM RETIEVED... startAt: ${startAt}, maxResults: ${maxResults}, RETRIEVED ${messages.length} TICKETS...`
+  );
+
+  // write on queue
 };
 
-recupera()
-  .then((risultato) => {
-    console.log(risultato); // Stampa: Promise risolta!
+const extractServiceId = (issue: JiraLegacyIssue): O.Option<NonEmptyString> =>
+  pipe(
+    issue.labels,
+    RA.findFirst((label) => label.startsWith(SERVICE_ID_PREFIX)),
+    O.map(
+      (label) => label.substring(SERVICE_ID_PREFIX.length) as NonEmptyString
+    )
+  );
+
+const retrieveApimUserId = (
+  serviceId: NonEmptyString
+): TE.TaskEither<Error, NonEmptyString> =>
+  pipe(
+    getSubscription(
+      apimClient,
+      config.AZURE_APIM_RESOURCE_GROUP,
+      config.AZURE_APIM,
+      serviceId
+    ),
+    TE.mapLeft(
+      (_) =>
+        new Error(
+          `An error has occurred while retrieving service '${serviceId}'`
+        )
+    ),
+    TE.map(({ ownerId }) => ownerId as NonEmptyString)
+  );
+
+const buildQueueMessage = (
+  issue: JiraLegacyIssue,
+  serviceId: NonEmptyString,
+  apimUserId: NonEmptyString
+): Queue.RequestReviewLegacyItem => ({
+  isNewTicket: true,
+  serviceId,
+  ticketId: issue.id,
+  ticketKey: issue.key,
+  apimUserId,
+});
+
+start()
+  .then(() => {
+    console.log(`PAST RECOVERY PROCEDURE SUCCESSFULLY FINISH`);
   })
-  .catch((errore) => {
-    console.error("Si è verificato un errore:", errore);
+  .catch((error) => {
+    console.error(
+      `PAST RECOVERY PROCEDURE END UP IN ERRROR, the reason WAS: ${error}`
+    );
   });
