30eb94cdde44b43df5cb6b8d01a72f2268ad777e
Giuseppe Di Pinto <118166285+giuseppedipinto@users.noreply.github.com>
2023-09-18 10:14:18 +0200

    fix:[IOPAE-600] Fix useTranslation import (#359)
    * fix useTranslation import
    
    * changeset

--- apps/backoffice/src/components/loaders/loader-fullscreen.tsx
+++ apps/backoffice/src/components/loaders/loader-fullscreen.tsx
@@ -6,7 +6,7 @@ import {
   CircularProgress,
   Typography
 } from "@mui/material";
-import { useTranslation } from "react-i18next";
+import { useTranslation } from "next-i18next";
 
 export type LoaderFullscreenProps = {
   title: string;
