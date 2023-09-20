f0e546d196169e29ab33219985e6fad0870cb825
Giuseppe Di Pinto <118166285+giuseppedipinto@users.noreply.github.com>
2023-09-20 16:24:05 +0200

    [IOPAE-601] Add Institution selection (#366)
    * update mui-italia to latest
    
    * add string-util file with isNullUndefinedOrEmpty
    
    * add selfcare token-exchange mock page
    
    * add new env variables
    
    * add institution change implementation
    
    * changeset
    
    * update yarn.lock

--- apps/backoffice/src/config/index.ts
+++ apps/backoffice/src/config/index.ts
@@ -34,8 +34,14 @@ export function getConfiguration() {
       .NEXT_PUBLIC_API_BACKEND_BASE_PATH as string,
     API_BACKEND_MOCKING: process.env.NEXT_PUBLIC_API_BACKEND_MOCKING === "true",
 
-    // BackOffice URLs
+    // BackOffice settings
+    BACK_OFFICE_ID: process.env.NEXT_PUBLIC_BACK_OFFICE_ID as string,
+    BACK_OFFICE_TITLE: process.env.NEXT_PUBLIC_BACK_OFFICE_TITLE as string,
+
+    // URLs
     SELFCARE_URL: process.env.NEXT_PUBLIC_SELFCARE_URL as string,
+    SELFCARE_TOKEN_EXCHANGE_URL: process.env
+      .NEXT_PUBLIC_SELFCARE_TOKEN_EXCHANGE_URL as string,
 
     // NodeJS Environment mode
     IS_DEVELOPMENT: process.env.NODE_ENV === "development",
