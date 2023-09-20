ab887db7d6efb07ee4a52f2d59bf2f00c7c3d92b
Rodolfo Viti <rodolfo.viti@pagopa.it>
2023-09-20 13:19:48 +0200

    fix info handler

--- apps/backoffice/src/app/api/info/route.ts
+++ apps/backoffice/src/app/api/info/route.ts
@@ -1,11 +1,11 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import packageJson from "../../../../package.json";
 
 /**
  * `api/info` route handler
  * @returns project _name_ and _version_
  */
-export async function GET(req: NextRequest) {
+export async function GET() {
   const info = {
     name: packageJson.name,
     version: packageJson.version
