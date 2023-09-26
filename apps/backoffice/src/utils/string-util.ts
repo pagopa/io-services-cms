1cc7a372188561798bc60b214fee5fcc1dd37725
Giuseppe Di Pinto <giuseppeantonio.dipinto@pagopa.it>
2023-09-26 17:17:23 +0200

    add obscure string util

--- apps/backoffice/src/utils/string-util.ts
+++ apps/backoffice/src/utils/string-util.ts
@@ -1,3 +1,6 @@
 /** Utility to check if a string is null, undefined or empty */
 export const isNullUndefinedOrEmpty = (value: string | null | undefined) =>
   !value || value.trim().length === 0;
+
+/** Utility to obscure string */
+export const obscure = (value: string) => "•".repeat(value.length);
