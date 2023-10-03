83afbd7897a00e535f724a98b52044dd282dc8f7
Giuseppe Di Pinto <giuseppeantonio.dipinto@pagopa.it>
2023-10-03 18:05:33 +0200

    add notifications on useFetch.fetchData result

---
+++ apps/backoffice/src/components/notification/index.tsx
@@ -0,0 +1,45 @@
+import { Alert, AlertColor, Box, Typography } from "@mui/material";
+import { CustomContentProps, SnackbarContent, useSnackbar } from "notistack";
+import { forwardRef, useCallback } from "react";
+
+export interface NotificationProps extends CustomContentProps {
+  /** The severity of the alert. This defines the color and icon used.  */
+  severity: AlertColor;
+  /** Alert title */
+  title?: string;
+  /** Alert body message content */
+  message?: string;
+}
+
+/** Custom `notistack` notification content */
+export const Notification = forwardRef<HTMLDivElement, NotificationProps>(
+  ({ id, ...props }, ref) => {
+    const { closeSnackbar } = useSnackbar();
+
+    const handleDismiss = useCallback(() => {
+      closeSnackbar(id);
+    }, [id, closeSnackbar]);
+
+    return (
+      <SnackbarContent ref={ref}>
+        <Alert
+          onClose={handleDismiss}
+          severity={props.severity}
+          variant="outlined"
+          sx={{ width: "100%" }}
+        >
+          <Box width="400px">
+            {props.title ? (
+              <Typography variant="body2" fontWeight={600}>
+                {props.title}
+              </Typography>
+            ) : null}
+            <Typography variant="body2">{props.message}</Typography>
+          </Box>
+        </Alert>
+      </SnackbarContent>
+    );
+  }
+);
+
+Notification.displayName = "Notification";
