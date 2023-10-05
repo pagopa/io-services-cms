212cba85416b2b1487d45504559d139aecc5dd15
Giuseppe Di Pinto <giuseppeantonio.dipinto@pagopa.it>
2023-10-05 16:18:25 +0200

    Improve PageHeader loading status

--- apps/backoffice/src/components/headers/page-header.tsx
+++ apps/backoffice/src/components/headers/page-header.tsx
@@ -2,9 +2,10 @@ import { Box, Typography } from "@mui/material";
 import { useTranslation } from "next-i18next";
 import { PageBreadcrumbs } from "./page-breadcrumbs";
 import { PageHtmlHeadTitle } from "./page-html-head-title";
+import { LoaderSkeleton } from "../loaders";
 
 export type PageHeaderProps = {
-  title: string;
+  title?: string;
   description?: string;
 };
 
@@ -13,11 +14,16 @@ export const PageHeader = ({ title, description }: PageHeaderProps) => {
 
   return (
     <>
-      <PageHtmlHeadTitle section={title} />
+      <PageHtmlHeadTitle section={title ?? ""} />
       <PageBreadcrumbs />
       <Box marginBottom={3} id="bo-io-page-title-descr">
         <Typography marginBottom={2} variant="h4">
-          {t(title)}
+          <LoaderSkeleton
+            loading={title === undefined}
+            style={{ width: "100%" }}
+          >
+            {t(title ?? "")}
+          </LoaderSkeleton>
         </Typography>
         {description ? (
           <Typography variant="body1">{t(description)}</Typography>
