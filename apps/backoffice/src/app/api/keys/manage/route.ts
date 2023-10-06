55b3ed1f162c15813b559a5e485c326bf32d91fa
Gianmaria Scorza <gianmaria@scorza.dev>
2023-10-06 12:07:37 +0200

    cleanUp pt2

--- apps/backoffice/src/app/api/keys/manage/route.ts
+++ apps/backoffice/src/app/api/keys/manage/route.ts
@@ -1,11 +1,9 @@
-import { NextRequest, NextResponse } from "next/server";
-import { BackOfficeUser } from "../../../../../types/next-auth";
-import { ApimUtils } from "@io-services-cms/external-clients";
 import { getConfiguration } from "@/config";
-import { retrieveManageKeys } from "../../lib/keys-manage";
-import { withJWTAuthHandler } from "../../lib/handler-wrappers";
-import { pipe } from "fp-ts/lib/function";
+import { NextRequest } from "next/server";
+import { BackOfficeUser } from "../../../../../types/next-auth";
 import { buildApimService } from "../../lib/apim-helper";
+import { withJWTAuthHandler } from "../../lib/handler-wrappers";
+import { retrieveManageKeys } from "../../lib/keys-manage";
 
 /**
  * @description Retrieve Manage keys
