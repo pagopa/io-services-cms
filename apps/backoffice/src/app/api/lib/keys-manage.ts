a771ee135f96c65e81a4bfc03d3750c6f0b174fe
Gianmaria Scorza <gianmaria@scorza.dev>
2023-10-09 10:41:26 +0200

    implements PUT /keys/manage API

--- apps/backoffice/src/app/api/lib/keys-manage.ts
+++ apps/backoffice/src/app/api/lib/keys-manage.ts
@@ -2,10 +2,11 @@
  * Module for the management of the manage key, contains the business logic for the /keys/manage apis
  */
 
-import { NextResponse } from "next/server";
 import { ApimUtils } from "@io-services-cms/external-clients";
-import { pipe } from "fp-ts/lib/function";
+import * as E from "fp-ts/lib/Either";
 import * as TE from "fp-ts/lib/TaskEither";
+import { pipe } from "fp-ts/lib/function";
+import { NextResponse } from "next/server";
 import { BackOfficeUser } from "../../../../types/next-auth";
 
 export const retrieveManageKeys = (apimService: ApimUtils.ApimService) => (
@@ -31,4 +32,58 @@ export const retrieveManageKeys = (apimService: ApimUtils.ApimService) => (
       })
     ),
     TE.toUnion
+  );
+
+export const regenerateManageKeys = (apimService: ApimUtils.ApimService) => (
+  backOfficeUser: BackOfficeUser,
+  keyType: string
+) =>
+  pipe(
+    validateKeyType(keyType),
+    E.mapLeft(error =>
+      NextResponse.json(
+        {
+          title: "InvalidKeyType",
+          status: 400,
+          detail: error
+        },
+        { status: 400 }
+      )
+    ),
+    TE.fromEither,
+    TE.chainW(_ =>
+      pipe(
+        apimService.regenerateSubscriptionKey(
+          backOfficeUser.parameters.subscriptionId,
+          "primary" as any
+        ),
+        TE.mapLeft(error =>
+          NextResponse.json(
+            {
+              title: "ApimError",
+              status: error.statusCode as any,
+              detail: JSON.stringify(error)
+            },
+            { status: error.statusCode }
+          )
+        )
+      )
+    ),
+    TE.map(response =>
+      NextResponse.json({
+        primaryKey: response.primaryKey,
+        secondaryKey: response.secondaryKey
+      })
+    ),
+    TE.toUnion,
+    x => x
+  );
+
+const validateKeyType = (keyType: string) =>
+  pipe(
+    keyType,
+    E.fromPredicate(
+      _ => _ === "primary" || _ === "secondary",
+      _ => `Invalid key type: ${_}`
+    )
   );
