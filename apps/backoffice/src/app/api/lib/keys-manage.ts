1f01f908791f98697b833d646c6920778368fd63
Gianmaria Scorza <gianmaria@scorza.dev>
2023-10-09 16:54:32 +0200

    [#IOPAE-629] ADD PUT /keys/manage API handler Implementation in Backoffice (#405)
    * implements PUT /keys/manage API
    
    * decode KeyType
    
    * cleanUp
    
    * ADD regenerate manageKey API test
    
    * changeset

--- apps/backoffice/src/app/api/lib/keys-manage.ts
+++ apps/backoffice/src/app/api/lib/keys-manage.ts
@@ -2,10 +2,13 @@
  * Module for the management of the manage key, contains the business logic for the /keys/manage apis
  */
 
-import { NextResponse } from "next/server";
+import { SubscriptionKeyType } from "@/generated/api/SubscriptionKeyType";
 import { ApimUtils } from "@io-services-cms/external-clients";
-import { pipe } from "fp-ts/lib/function";
+import { readableReport } from "@pagopa/ts-commons/lib/reporters";
+import * as E from "fp-ts/lib/Either";
 import * as TE from "fp-ts/lib/TaskEither";
+import { pipe } from "fp-ts/lib/function";
+import { NextResponse } from "next/server";
 import { BackOfficeUser } from "../../../../types/next-auth";
 
 export const retrieveManageKeys = (apimService: ApimUtils.ApimService) => (
@@ -22,6 +25,51 @@ export const retrieveManageKeys = (apimService: ApimUtils.ApimService) => (
           detail: JSON.stringify(error)
         },
         { status: error.statusCode }
+      )
+    ),
+    TE.map(response =>
+      NextResponse.json({
+        primaryKey: response.primaryKey,
+        secondaryKey: response.secondaryKey
+      })
+    ),
+    TE.toUnion
+  );
+
+export const regenerateManageKeys = (apimService: ApimUtils.ApimService) => (
+  backOfficeUser: BackOfficeUser,
+  keyType: string
+) =>
+  pipe(
+    keyType,
+    SubscriptionKeyType.decode,
+    E.mapLeft(error =>
+      NextResponse.json(
+        {
+          title: "InvalidKeyType",
+          status: 400,
+          detail: readableReport(error)
+        },
+        { status: 400 }
+      )
+    ),
+    TE.fromEither,
+    TE.chainW(decodedKeyType =>
+      pipe(
+        apimService.regenerateSubscriptionKey(
+          backOfficeUser.parameters.subscriptionId,
+          decodedKeyType
+        ),
+        TE.mapLeft(error =>
+          NextResponse.json(
+            {
+              title: "ApimError",
+              status: error.statusCode as any,
+              detail: JSON.stringify(error)
+            },
+            { status: error.statusCode }
+          )
+        )
       )
     ),
     TE.map(response =>
