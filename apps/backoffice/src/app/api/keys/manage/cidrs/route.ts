d5c9493de2651f990412c668414348bbc0264c50
Gianmaria Scorza <gianmaria@scorza.dev>
2023-10-12 11:19:20 +0200

    [#IOPAE-637] Implements PUT /keys/manage/cidrs endpoint (#408)
    * implements GET /keys/manage/cidrs endpoint
    
    * statically declare dependencies on /keys/manage/cidrs
    
    * cleanUp
    
    * add cosmos uri
    
    * test subscriptionCIDRsModel Singleton
    
    * Add GET /keys/manage/cidrs unit test
    
    * changeset
    
    * implements PUT /keys/manage/cidrs API
    
    * add unit tests for PUT /keys/manage/cidrs API
    
    * changeset

--- apps/backoffice/src/app/api/keys/manage/cidrs/route.ts
+++ apps/backoffice/src/app/api/keys/manage/cidrs/route.ts
@@ -1,7 +1,10 @@
-import { retrieveManageKeyCIDRs } from "@/app/api/lib/authorized-cidrs";
+import {
+  retrieveManageKeyCIDRs,
+  updateManageKeyCIDRs
+} from "@/app/api/lib/authorized-cidrs";
 import { withJWTAuthHandler } from "@/app/api/lib/handler-wrappers";
 import { getSubscriptionCIDRsModelInstance } from "@/app/api/lib/subscription-cidrs-singleton";
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 import { BackOfficeUser } from "../../../../../../types/next-auth";
 
 /**
@@ -18,11 +21,18 @@ const getAuthorizedManageKeyCIDRs = (
 /**
  * @description Update manage key authorized CIDRs
  */
-export async function PUT(
+const updateAuthorizedManageKeyCIDRs = (
   request: NextRequest,
   { backofficeUser }: { backofficeUser: BackOfficeUser }
-) {
-  return NextResponse.json({ message: "Not Implemented" });
-}
+) => {
+  const subscriptionCIDRsModel = getSubscriptionCIDRsModelInstance();
+  return updateManageKeyCIDRs(subscriptionCIDRsModel)(
+    backofficeUser,
+    request
+  )();
+};
 
-export const { GET = withJWTAuthHandler(getAuthorizedManageKeyCIDRs) } = {};
+export const {
+  GET = withJWTAuthHandler(getAuthorizedManageKeyCIDRs),
+  PUT = withJWTAuthHandler(updateAuthorizedManageKeyCIDRs)
+} = {};
--- apps/backoffice/src/app/api/lib/__tests__/authorized-cidrs.test.ts
+++ apps/backoffice/src/app/api/lib/__tests__/authorized-cidrs.test.ts
@@ -1,14 +1,19 @@
 import { SubscriptionCIDRsModel } from "@pagopa/io-functions-commons/dist/src/models/subscription_cidrs";
 import * as O from "fp-ts/lib/Option";
 import * as TE from "fp-ts/lib/TaskEither";
+import { NextRequest } from "next/server";
 import { describe, expect, it, vi } from "vitest";
 import { BackOfficeUser } from "../../../../../types/next-auth";
-import { retrieveManageKeyCIDRs } from "../../lib/authorized-cidrs";
+import {
+  retrieveManageKeyCIDRs,
+  updateManageKeyCIDRs
+} from "../../lib/authorized-cidrs";
 
 const anUserEmail = "anEmail@email.it";
 const anUserId = "anUserId";
 const aSubscriptionId = "aSubscriptionId";
 const aUserPermissions = ["permission1", "permission2"];
+const aCidrsList = ["127.0.0.1", "127.0.0.2"];
 const jwtMock = ({
   permissions: aUserPermissions,
   parameters: {
@@ -56,13 +61,13 @@ describe("Authorized CIDRs Subscription Manage", () => {
 
       //extract jsonBody from NextResponse
       const jsonResponse = await new Response(result.body).json();
-      console.log("jsonResponse", jsonResponse);
+
       expect(
         mockSubscriptionCIDRsModel.findLastVersionByModelId
       ).toHaveBeenCalledWith([aSubscriptionId]);
       expect(result.status).toBe(404);
       expect(jsonResponse).not.toBe(null);
-      expect(jsonResponse.title).toBe("ManageKeyCIDRsNotFound");
+      expect(jsonResponse.title).toBe("ManageKeyRetrieveCIDRsNotFound");
       expect(jsonResponse.status).toBe(404);
     });
 
@@ -94,7 +99,150 @@ describe("Authorized CIDRs Subscription Manage", () => {
       ).toHaveBeenCalledWith([aSubscriptionId]);
       expect(result.status).toBe(500);
       expect(jsonResponse).not.toBe(null);
-      expect(jsonResponse.title).toBe("ManageKeyCIDRsError");
+      expect(jsonResponse.title).toBe("ManageKeyRetrieveCIDRsError");
+      expect(jsonResponse.status).toBe(500);
+    });
+  });
+
+  describe("Update", () => {
+    it("should return 200 when authorized cidrs are updated correctly", async () => {
+      const mockSubscriptionCIDRsModel = ({
+        upsert: vi.fn(request =>
+          TE.right({
+            cidrs: request.cidrs.values()
+          })
+        )
+      } as unknown) as SubscriptionCIDRsModel;
+
+      const mockNextRequest = ({
+        json: vi.fn(() =>
+          Promise.resolve({
+            cidrs: aCidrsList
+          })
+        )
+      } as unknown) as NextRequest;
+
+      const result = await updateManageKeyCIDRs(mockSubscriptionCIDRsModel)(
+        jwtMock,
+        mockNextRequest
+      )();
+
+      //extract jsonBody from NextResponse
+      const jsonResponse = await new Response(result.body).json();
+
+      expect(mockSubscriptionCIDRsModel.upsert).toHaveBeenCalledWith({
+        cidrs: new Set(aCidrsList),
+        kind: "INewSubscriptionCIDRs",
+        subscriptionId: aSubscriptionId
+      });
+      expect(result.status).toBe(200);
+      expect(jsonResponse).toStrictEqual({ cidrs: aCidrsList });
+    });
+
+    it("should return 400 when no payload is provided on request", async () => {
+      const mockSubscriptionCIDRsModel = ({
+        upsert: vi.fn(request =>
+          TE.right({
+            cidrs: request.cidrs.values()
+          })
+        )
+      } as unknown) as SubscriptionCIDRsModel;
+
+      const mockNextRequest = ({
+        json: vi.fn(() =>
+          Promise.reject({
+            error: "no payload provided error"
+          })
+        )
+      } as unknown) as NextRequest;
+
+      const result = await updateManageKeyCIDRs(mockSubscriptionCIDRsModel)(
+        jwtMock,
+        mockNextRequest
+      )();
+
+      //extract jsonBody from NextResponse
+      const jsonResponse = await new Response(result.body).json();
+
+      expect(mockSubscriptionCIDRsModel.upsert).not.toHaveBeenCalled();
+      expect(result.status).toBe(400);
+      expect(jsonResponse).not.toBe(null);
+      expect(jsonResponse.title).toBe("ManageKeyUpdateCIDRsError");
+      expect(jsonResponse.status).toBe(400);
+    });
+
+    it("should return 400 when a wrong payload is provided", async () => {
+      const mockSubscriptionCIDRsModel = ({
+        upsert: vi.fn(request =>
+          TE.right({
+            cidrs: request.cidrs.values()
+          })
+        )
+      } as unknown) as SubscriptionCIDRsModel;
+
+      const mockNextRequest = ({
+        json: vi.fn(() =>
+          Promise.resolve({
+            anInvalidProperty: "anInvalidPropertyValue"
+          })
+        )
+      } as unknown) as NextRequest;
+
+      const result = await updateManageKeyCIDRs(mockSubscriptionCIDRsModel)(
+        jwtMock,
+        mockNextRequest
+      )();
+
+      //extract jsonBody from NextResponse
+      const jsonResponse = await new Response(result.body).json();
+
+      expect(mockSubscriptionCIDRsModel.upsert).not.toHaveBeenCalled();
+      expect(result.status).toBe(400);
+      expect(jsonResponse).not.toBe(null);
+      expect(jsonResponse.title).toBe("ManageKeyUpdateCIDRsError");
+      expect(jsonResponse.status).toBe(400);
+    });
+
+    it("should return 500 when an error is returned from cosmos", async () => {
+      const mockSubscriptionCIDRsModel = ({
+        upsert: vi.fn(() =>
+          TE.left({
+            kind: "COSMOS_ERROR_RESPONSE",
+            error: {
+              code: 500,
+              body: {
+                code: "Error",
+                message: "Cosmos error"
+              }
+            }
+          })
+        )
+      } as unknown) as SubscriptionCIDRsModel;
+
+      const mockNextRequest = ({
+        json: vi.fn(() =>
+          Promise.resolve({
+            cidrs: aCidrsList
+          })
+        )
+      } as unknown) as NextRequest;
+
+      const result = await updateManageKeyCIDRs(mockSubscriptionCIDRsModel)(
+        jwtMock,
+        mockNextRequest
+      )();
+
+      //extract jsonBody from NextResponse
+      const jsonResponse = await new Response(result.body).json();
+
+      expect(mockSubscriptionCIDRsModel.upsert).toHaveBeenCalledWith({
+        cidrs: new Set(aCidrsList),
+        kind: "INewSubscriptionCIDRs",
+        subscriptionId: aSubscriptionId
+      });
+      expect(result.status).toBe(500);
+      expect(jsonResponse).not.toBe(null);
+      expect(jsonResponse.title).toBe("ManageKeyUpdateCIDRsError");
       expect(jsonResponse.status).toBe(500);
     });
   });
