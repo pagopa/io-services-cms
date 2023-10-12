50173c1bc9cd419958a84b5a25791575ec741596
Gianmaria Scorza <gianmaria@scorza.dev>
2023-10-12 17:42:35 +0200

    module pattern for api /keys/manage/cidrs

--- apps/backoffice/src/app/api/keys/manage/cidrs/route.ts
+++ apps/backoffice/src/app/api/keys/manage/cidrs/route.ts
@@ -1,38 +1,94 @@
 import {
-  retrieveManageKeyCIDRs,
-  updateManageKeyCIDRs
-} from "@/lib/be/authorized-cidrs";
+  HTTP_STATUS_BAD_REQUEST,
+  HTTP_STATUS_INTERNAL_SERVER_ERROR
+} from "@/config/constants";
+import { ManageKeyCIDRs } from "@/generated/api/ManageKeyCIDRs";
 import { withJWTAuthHandler } from "@/lib/be/handler-wrappers";
-import { getSubscriptionCIDRsModelInstance } from "@/lib/be/subscription-cidrs-singleton";
-import { NextRequest } from "next/server";
+import {
+  retrieveAuthorizedCIDRs,
+  updateAuthorizedCIDRs
+} from "@/lib/be/keys/business";
+import { readableReport } from "@pagopa/ts-commons/lib/reporters";
+import * as E from "fp-ts/lib/Either";
+import { NextRequest, NextResponse } from "next/server";
 import { BackOfficeUser } from "../../../../../../types/next-auth";
 
 /**
  * @description Retrieve manage key authorized CIDRs
  */
-const getAuthorizedManageKeyCIDRs = (
-  request: NextRequest,
-  { backofficeUser }: { backofficeUser: BackOfficeUser }
-) => {
-  const subscriptionCIDRsModel = getSubscriptionCIDRsModelInstance();
-  return retrieveManageKeyCIDRs(subscriptionCIDRsModel)(backofficeUser)();
-};
+export const GET = withJWTAuthHandler(
+  async (
+    request: NextRequest,
+    { backofficeUser }: { backofficeUser: BackOfficeUser }
+  ) => {
+    try {
+      const authorizedCIDRsResponse = await retrieveAuthorizedCIDRs(
+        backofficeUser.parameters.subscriptionId
+      );
+
+      return NextResponse.json({
+        cidrs: authorizedCIDRsResponse
+      });
+    } catch (error) {
+      console.log(
+        `An Error has occurred while retrieving Manage Key CIDRs for subscriptionId: ${backofficeUser.parameters.subscriptionId}, caused by: ${error}`
+      );
+      return NextResponse.json(
+        {
+          title: "ManageKeyRetrieveCIDRsError",
+          status: HTTP_STATUS_INTERNAL_SERVER_ERROR,
+          detail: "Error retrieving Manage Key CIDRs"
+        },
+        { status: HTTP_STATUS_INTERNAL_SERVER_ERROR }
+      );
+    }
+  }
+);
 
 /**
  * @description Update manage key authorized CIDRs
  */
-const updateAuthorizedManageKeyCIDRs = (
-  request: NextRequest,
-  { backofficeUser }: { backofficeUser: BackOfficeUser }
-) => {
-  const subscriptionCIDRsModel = getSubscriptionCIDRsModelInstance();
-  return updateManageKeyCIDRs(subscriptionCIDRsModel)(
-    backofficeUser,
-    request
-  )();
-};
+export const PUT = withJWTAuthHandler(
+  async (
+    request: NextRequest,
+    { backofficeUser }: { backofficeUser: BackOfficeUser }
+  ) => {
+    try {
+      const body = await request.json();
+      const decodedBody = ManageKeyCIDRs.decode(body);
 
-export const {
-  GET = withJWTAuthHandler(getAuthorizedManageKeyCIDRs),
-  PUT = withJWTAuthHandler(updateAuthorizedManageKeyCIDRs)
-} = {};
+      if (E.isLeft(decodedBody)) {
+        return NextResponse.json(
+          {
+            title: "ManageKeyUpdateCIDRsError",
+            status: HTTP_STATUS_BAD_REQUEST,
+            detail: readableReport(decodedBody.left)
+          },
+          { status: HTTP_STATUS_BAD_REQUEST }
+        );
+      }
+
+      const authorizedCIDRsResponse = await updateAuthorizedCIDRs(
+        backofficeUser.parameters.subscriptionId,
+        decodedBody.right.cidrs
+      );
+
+      return NextResponse.json({
+        cidrs: authorizedCIDRsResponse
+      });
+
+    } catch (error) {
+      console.log(
+        `An Error has occurred while retrieving Manage Key CIDRs for subscriptionId: ${backofficeUser.parameters.subscriptionId}, caused by: ${error}`
+      );
+      return NextResponse.json(
+        {
+          title: "ManageKeyRetrieveCIDRsError",
+          status: HTTP_STATUS_INTERNAL_SERVER_ERROR,
+          detail: "Error retrieving Manage Key CIDRs"
+        },
+        { status: HTTP_STATUS_INTERNAL_SERVER_ERROR }
+      );
+    }
+  }
+);
