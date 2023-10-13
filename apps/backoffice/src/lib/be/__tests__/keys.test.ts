1af6ce748e4b2178cdffdfbb52ee12dea929b905
Gianmaria Scorza <gianmaria@scorza.dev>
2023-10-13 10:54:31 +0200

    added unit test for updateAuthorizedCIDRs

--- apps/backoffice/src/lib/be/__tests__/keys.test.ts
+++ apps/backoffice/src/lib/be/__tests__/keys.test.ts
@@ -3,7 +3,10 @@ import { Cidr } from "../../../generated/api/Cidr";
 
 import * as O from "fp-ts/lib/Option";
 import * as TE from "fp-ts/lib/TaskEither";
-import { retrieveAuthorizedCIDRs } from "../keys/business";
+import {
+  retrieveAuthorizedCIDRs,
+  updateAuthorizedCIDRs
+} from "../keys/business";
 
 const mocks: { cidrs: Set<Cidr>; aSubscriptionId: string } = vi.hoisted(() => ({
   cidrs: new Set(["127.0.0.1/8", "127.0.0.2/8"]) as Set<Cidr>,
@@ -34,60 +37,120 @@ vi.mock("@/lib/be/legacy-cosmos", () => ({
 describe("Authorized CIDRs Subscription Manage", () => {
   describe("Retrieve", () => {
     it("should return 200 when authorized cidrs are found", async () => {
-      getSubscriptionCIDRsModel.mockReturnValueOnce({
-        findLastVersionByModelId: vi.fn(() =>
-          TE.right(
-            O.some({
-              cidrs: mocks.cidrs
-            })
-          )
+      const findLastVersionByModelId = vi.fn(() =>
+        TE.right(
+          O.some({
+            cidrs: mocks.cidrs
+          })
         )
+      );
+
+      getSubscriptionCIDRsModel.mockReturnValueOnce({
+        findLastVersionByModelId
       });
 
       const result = await retrieveAuthorizedCIDRs(mocks.aSubscriptionId);
 
-      expect(
-        getSubscriptionCIDRsModel.mock.results[0].value.findLastVersionByModelId
-      ).toHaveBeenCalledWith([mocks.aSubscriptionId]);
+      expect(findLastVersionByModelId).toHaveBeenCalledWith([
+        mocks.aSubscriptionId
+      ]);
       expect(result).toStrictEqual(Array.from(mocks.cidrs));
     });
 
     it("should return 200 when authorized cidrs are not found", async () => {
+      const findLastVersionByModelId = vi.fn(() => TE.right(O.none));
+
       getSubscriptionCIDRsModel.mockReturnValueOnce({
-        findLastVersionByModelId: vi.fn(() => TE.right(O.none))
+        findLastVersionByModelId
       });
 
       const result = await retrieveAuthorizedCIDRs(mocks.aSubscriptionId);
 
-      expect(
-        getSubscriptionCIDRsModel.mock.results[0].value.findLastVersionByModelId
-      ).toHaveBeenCalledWith([mocks.aSubscriptionId]);
+      expect(findLastVersionByModelId).toHaveBeenCalledWith([
+        mocks.aSubscriptionId
+      ]);
       expect(result).not.toBe(null);
       expect(result).toStrictEqual(Array<Cidr>());
     });
 
     it("should return 500 when an error is returned from cosmos", async () => {
-      getSubscriptionCIDRsModel.mockReturnValueOnce({
-        findLastVersionByModelId: vi.fn(() =>
-          TE.left({
-            kind: "COSMOS_ERROR_RESPONSE",
-            error: {
-              code: 500,
-              body: {
-                code: "Error",
-                message: "Cosmos error"
-              }
+      const findLastVersionByModelId = vi.fn(() =>
+        TE.left({
+          kind: "COSMOS_ERROR_RESPONSE",
+          error: {
+            code: 500,
+            body: {
+              code: "Error",
+              message: "Cosmos error"
             }
-          })
-        )
+          }
+        })
+      );
+
+      getSubscriptionCIDRsModel.mockReturnValueOnce({
+        findLastVersionByModelId
       });
 
       expect(
         retrieveAuthorizedCIDRs(mocks.aSubscriptionId)
       ).rejects.toThrowError();
+      expect(findLastVersionByModelId).toHaveBeenCalledWith([
+        mocks.aSubscriptionId
+      ]);
+    });
+  });
+
+  describe("Update", () => {
+    it("should return 200 when authorized cidrs are updated correctly", async () => {
+      const upsert = vi.fn(() =>
+        TE.right({
+          cidrs: mocks.cidrs.values()
+        })
+      );
+
+      getSubscriptionCIDRsModel.mockReturnValueOnce({
+        upsert
+      });
+
+      const result = await updateAuthorizedCIDRs(
+        mocks.aSubscriptionId,
+        Array.from(mocks.cidrs)
+      );
+
+      expect(upsert).toHaveBeenCalledWith({
+        cidrs: mocks.cidrs,
+        kind: "INewSubscriptionCIDRs",
+        subscriptionId: mocks.aSubscriptionId
+      });
+      expect(result).toStrictEqual(Array.from(mocks.cidrs));
+    });
+
+    it("should return 500 when an error is returned from cosmos", async () => {
+      const upsert = vi.fn(() =>
+        TE.left({
+          kind: "COSMOS_ERROR_RESPONSE",
+          error: {
+            code: 500,
+            body: {
+              code: "Error",
+              message: "Cosmos error"
+            }
+          }
+        })
+      );
+
+      getSubscriptionCIDRsModel.mockReturnValueOnce({
+        upsert
+      });
+
       expect(
-        getSubscriptionCIDRsModel.mock.results[0].value.findLastVersionByModelId
-      ).toHaveBeenCalledWith([mocks.aSubscriptionId]);
+        updateAuthorizedCIDRs(mocks.aSubscriptionId, Array.from(mocks.cidrs))
+      ).rejects.toThrowError();
+      expect(upsert).toHaveBeenCalledWith({
+        cidrs: mocks.cidrs,
+        kind: "INewSubscriptionCIDRs",
+        subscriptionId: mocks.aSubscriptionId
+      });
     });
   });
 });
