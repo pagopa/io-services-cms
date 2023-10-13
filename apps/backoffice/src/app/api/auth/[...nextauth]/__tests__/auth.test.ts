b7fbdc04e5cb5ee68a6822a31134af4dfd21ff81
Rodolfo Viti <rodolfo.viti@pagopa.it>
2023-10-13 12:00:11 +0200

    add nextauth authorize unit tests

---
+++ apps/backoffice/src/app/api/auth/[...nextauth]/__tests__/auth.test.ts
@@ -0,0 +1,362 @@
+import { faker } from "@faker-js/faker/locale/it";
+import { ApimUtils } from "@io-services-cms/external-clients";
+import axios from "axios";
+import http, { IncomingMessage } from "http";
+import * as O from "fp-ts/lib/Option";
+import * as TE from "fp-ts/lib/TaskEither";
+import * as jose from "jose";
+import { afterEach, describe, expect, it, vi } from "vitest";
+import { Configuration } from "../../../../../config";
+import { authorize } from "../auth";
+import { once } from "events";
+
+// const JWA = "RS256";
+// const { publicKey, privateKey } = await jose.generateKeyPair(JWA);
+// const publicJwk = {
+//   ...(await jose.exportJWK(publicKey)),
+//   kid: faker.internet.mac()
+// };
+// const JWKS = {
+//   keys: [publicJwk]
+// };
+const aValidJwtPayload = {
+  exp: faker.number.int(),
+  iat: faker.number.int(),
+  aud: faker.string.alphanumeric(),
+  iss: faker.string.alphanumeric(),
+  jti: faker.string.alphanumeric(),
+  family_name: faker.person.lastName(),
+  name: faker.person.firstName(),
+  email: faker.internet.email(),
+  uid: faker.string.uuid(),
+  fiscal_number: faker.string.alphanumeric(),
+  desired_exp: faker.number.int(),
+  organization: {
+    id: faker.string.uuid(),
+    fiscal_code: faker.string.numeric(),
+    name: faker.company.name(),
+    roles: [
+      {
+        partyRole: faker.string.alpha(),
+        role: faker.string.alpha()
+      }
+    ],
+    groups: [faker.string.alpha()]
+  }
+};
+// const aValidJws = await new jose.SignJWT(aValidJwtPayload)
+//   .setProtectedHeader({
+//     typ: "JWT",
+//     alg: JWA,
+//     kid: publicJwk.kid
+//   })
+//   .setIssuer(faker.internet.url())
+//   .setSubject(faker.internet.userName())
+//   .setAudience(faker.internet.domainName())
+//   .setExpirationTime("6h")
+//   .setIssuedAt()
+//   .setJti(faker.string.uuid())
+//   .sign(privateKey);
+
+const mockConfig = ({
+  SELFCARE_BASE_URL: "http://localhost:7075",
+  SELFCARE_JWKS_PATH: "/.well-known/jwks.json",
+  AZURE_SUBSCRIPTION_ID: faker.string.uuid(),
+  AZURE_CLIENT_SECRET_CREDENTIAL_CLIENT_ID: faker.string.uuid(),
+  AZURE_CLIENT_SECRET_CREDENTIAL_SECRET: faker.internet.password(),
+  AZURE_CLIENT_SECRET_CREDENTIAL_TENANT_ID: faker.string.uuid(),
+  AZURE_APIM_RESOURCE_GROUP: faker.string.alpha(),
+  AZURE_APIM: faker.string.alpha()
+} as unknown) as Configuration;
+
+const mockApimClient = {};
+const mockApimUtils = ({
+  getApimClient: vi.fn(() => mockApimClient),
+  getApimService: vi.fn(() => ({
+    getUserByEmail: vi.fn(() => TE.right(O.none))
+  }))
+} as unknown) as typeof ApimUtils;
+
+// const mswServer = setupServer(
+//   // Describe the requests to mock.
+//   rest.get(
+//     mockConfig.SELFCARE_BASE_URL + mockConfig.SELFCARE_JWKS_PATH,
+//     (req, res, ctx) => {
+//       return res(ctx.json(JWKS));
+//     }
+//   )
+// );
+
+// beforeAll(() => {
+//   // Establish requests interception layer before all tests.
+//   mswServer.listen({ onUnhandledRequest: "warn" });
+//   mswServer.printHandlers();
+// });
+
+// afterAll(() => {
+//   // Clean up after all tests are done, preventing this
+//   // interception layer from affecting irrelevant tests.
+//   mswServer.close();
+// });
+
+const { getUserByEmail } = vi.hoisted(() => ({
+  getUserByEmail: vi.fn()
+}));
+
+const { getApimClient, getApimService } = vi.hoisted(() => ({
+  getApimClient: vi.fn().mockReturnValue({}),
+  getApimService: vi.fn().mockReturnValue({
+    getUserByEmail
+  })
+}));
+
+vi.mock("@io-services-cms/external-clients", () => ({
+  ApimUtils: { getApimClient, getApimService }
+}));
+
+afterEach(() => {
+  vi.clearAllMocks();
+});
+
+describe("Authorize", () => {
+  it("should fail when credentials is not valid", async () => {
+    await expect(() =>
+      authorize(mockConfig, jose)({ invalidParam: "identity_token" }, {})
+    ).rejects.toThrowError(/is not a valid/);
+
+    expect(getApimClient).not.toHaveBeenCalled();
+    expect(getApimService).not.toHaveBeenCalled();
+  });
+
+  // it("mytest", async () => {
+  //   const { data, status } = await axios.get<any>(
+  //     `${mockConfig.SELFCARE_BASE_URL}${mockConfig.SELFCARE_JWKS_PATH}`
+  //   );
+
+  //   console.log(data);
+  //   expect(status).toBe(200);
+  // });
+
+  // function concat(...buffers: Uint8Array[]): Uint8Array {
+  //   const size = buffers.reduce((acc, { length }) => acc + length, 0);
+  //   const buf = new Uint8Array(size);
+  //   let i = 0;
+  //   buffers.forEach(buffer => {
+  //     buf.set(buffer, i);
+  //     i += buffer.length;
+  //   });
+  //   return buf;
+  // }
+
+  // it("mytest2", async () => {
+  //   const url = new URL(
+  //     `${mockConfig.SELFCARE_BASE_URL}${mockConfig.SELFCARE_JWKS_PATH}`
+  //   );
+  //   const req = http.get(url);
+  //   // const res = http.request(url);
+  //   const [response] = <[IncomingMessage]>(
+  //     await Promise.race([once(req, "response"), once(req, "timeout")])
+  //   );
+  //   if (response.statusCode !== 200) {
+  //     throw new Error(
+  //       "Expected 200 OK from the JSON Web Key Set HTTP response"
+  //     );
+  //   }
+
+  //   const parts = [] as any;
+  //   for await (const part of response) {
+  //     parts.push(part);
+  //   }
+
+  //   const decoder = new TextDecoder();
+
+  //   try {
+  //     console.log(JSON.parse(decoder.decode(concat(...parts))));
+  //   } catch {
+  //     throw new Error(
+  //       "Failed to parse the JSON Web Key Set HTTP response as JSON"
+  //     );
+  //   }
+  // });
+
+  it("should fail when token is not a JWT", async () => {
+    await expect(() =>
+      authorize(mockConfig, jose)({ identity_token: "identity_token" }, {})
+    ).rejects.toThrowError("Invalid Compact JWS");
+
+    expect(getApimClient).not.toHaveBeenCalled();
+    expect(getApimService).not.toHaveBeenCalled();
+  });
+
+  // it("should fail when JWT payload is not a valid IdentityToken", async () => {
+  //   const mpckApimUtils = (vi.fn() as unknown) as typeof ApimUtils;
+  //   await expect(() =>
+  //     authorize(
+  //       mockConfig,
+  //       jose,
+  //       mpckApimUtils
+  //     )({ identity_token: aValidJws }, {})
+  //   ).rejects.toThrowError();
+
+  //   expect(mpckApimUtils).not.toHaveBeenCalled();
+  // });
+
+  it("should fail when JWT payload is not a valid IdentityToken", async () => {
+    const { uid, ...anInvalidJwtPayload } = aValidJwtPayload;
+    const mockJose = ({
+      createRemoteJWKSet: jose.createRemoteJWKSet,
+      jwtVerify: vi.fn(() =>
+        Promise.resolve(({
+          payload: anInvalidJwtPayload,
+          protectedHeader: {}
+        } as unknown) as jose.JWTVerifyResult)
+      )
+    } as unknown) as typeof jose;
+
+    await expect(() =>
+      authorize(mockConfig, mockJose)(
+        {
+          identity_token: "identity_token"
+        },
+        {}
+      )
+    ).rejects.toThrowError(
+      "value [undefined] at [root.uid] is not a valid [string]"
+    );
+
+    expect(getApimClient).not.toHaveBeenCalled();
+    expect(getApimService).not.toHaveBeenCalled();
+  });
+
+  it("should fail when Apim user is not found", async () => {
+    const mockJose = ({
+      createRemoteJWKSet: jose.createRemoteJWKSet,
+      jwtVerify: vi.fn(() =>
+        Promise.resolve(({
+          payload: aValidJwtPayload,
+          protectedHeader: {}
+        } as unknown) as jose.JWTVerifyResult)
+      )
+    } as unknown) as typeof jose;
+    getUserByEmail.mockReturnValueOnce(TE.right(O.none));
+
+    await expect(() =>
+      authorize(mockConfig, mockJose)({ identity_token: "identity_token" }, {})
+    ).rejects.toThrowError("Cannot find user");
+
+    expect(getApimClient).toHaveBeenCalledOnce();
+    expect(getApimClient).toHaveBeenCalledWith(
+      mockConfig,
+      mockConfig.AZURE_SUBSCRIPTION_ID
+    );
+    expect(getApimService).toHaveBeenCalledOnce();
+    expect(getApimService).toHaveBeenCalledWith(
+      mockApimClient,
+      mockConfig.AZURE_APIM_RESOURCE_GROUP,
+      mockConfig.AZURE_APIM
+    );
+    expect(getUserByEmail).toHaveBeenCalledOnce();
+    expect(getUserByEmail).toHaveBeenCalledWith(
+      `org.${aValidJwtPayload.organization.id}@selfcare.io.pagopa.it`,
+      true
+    );
+  });
+
+  it("should fail when retrieved Apim user is not valid", async () => {
+    const mockJose = ({
+      createRemoteJWKSet: jose.createRemoteJWKSet,
+      jwtVerify: vi.fn(() =>
+        Promise.resolve(({
+          payload: aValidJwtPayload,
+          protectedHeader: {}
+        } as unknown) as jose.JWTVerifyResult)
+      )
+    } as unknown) as typeof jose;
+    getUserByEmail.mockReturnValueOnce(TE.right(O.some({})));
+
+    await expect(() =>
+      authorize(mockConfig, mockJose)({ identity_token: "identity_token" }, {})
+    ).rejects.toThrowError(/is not a valid/);
+
+    expect(getApimClient).toHaveBeenCalledOnce();
+    expect(getApimClient).toHaveBeenCalledWith(
+      mockConfig,
+      mockConfig.AZURE_SUBSCRIPTION_ID
+    );
+    expect(getApimService).toHaveBeenCalledOnce();
+    expect(getApimService).toHaveBeenCalledWith(
+      mockApimClient,
+      mockConfig.AZURE_APIM_RESOURCE_GROUP,
+      mockConfig.AZURE_APIM
+    );
+    expect(getUserByEmail).toHaveBeenCalledOnce();
+    expect(getUserByEmail).toHaveBeenCalledWith(
+      `org.${aValidJwtPayload.organization.id}@selfcare.io.pagopa.it`,
+      true
+    );
+  });
+
+  it("should fail when retrieved Apim user is valid", async () => {
+    const mockJose = ({
+      createRemoteJWKSet: jose.createRemoteJWKSet,
+      jwtVerify: vi.fn(() =>
+        Promise.resolve(({
+          payload: aValidJwtPayload,
+          protectedHeader: {}
+        } as unknown) as jose.JWTVerifyResult)
+      )
+    } as unknown) as typeof jose;
+    const aValidApimUser = {
+      id: faker.string.uuid(),
+      email: `org.${aValidJwtPayload.organization.id}@selfcare.io.pagopa.it`,
+      groups: [
+        {
+          type: "custom",
+          name: faker.string.alpha()
+        }
+      ]
+    };
+    getUserByEmail.mockImplementation(() => TE.right(O.some(aValidApimUser)));
+
+    const user = await authorize(mockConfig, mockJose)(
+      { identity_token: "identity_token" },
+      {}
+    );
+
+    expect(user).toEqual({
+      id: aValidJwtPayload.uid,
+      name: `${aValidJwtPayload.name} ${aValidJwtPayload.family_name}`,
+      email: aValidJwtPayload.email,
+      institution: {
+        id: aValidJwtPayload.organization.id,
+        name: aValidJwtPayload.organization.name,
+        role: aValidJwtPayload.organization.roles[0].role,
+        logo_url: "url"
+      },
+      authorizedInstitutions: [
+        { id: "id_2", name: "name_2", role: "operator" }
+      ],
+      permissions: aValidApimUser.groups
+        .filter(group => group.type === "custom")
+        .map(group => group.name),
+      parameters: {
+        userId: aValidApimUser.id,
+        userEmail: aValidApimUser.email,
+        subscriptionId: mockConfig.AZURE_SUBSCRIPTION_ID
+      }
+    });
+    expect(getApimClient).toHaveBeenCalledOnce();
+    expect(getApimClient).toHaveBeenCalledWith(
+      mockConfig,
+      mockConfig.AZURE_SUBSCRIPTION_ID
+    );
+    expect(getApimService).toHaveBeenCalledOnce();
+    expect(getApimService).toHaveBeenCalledWith(
+      mockApimClient,
+      mockConfig.AZURE_APIM_RESOURCE_GROUP,
+      mockConfig.AZURE_APIM
+    );
+    expect(getUserByEmail).toHaveBeenCalledOnce();
+    expect(getUserByEmail).toHaveBeenCalledWith(aValidApimUser.email, true);
+  });
+});
--- apps/backoffice/src/app/api/auth/[...nextauth]/route.ts
+++ apps/backoffice/src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,8 @@
-import { Configuration, getConfiguration } from "@/config";
-import { SelfCareIdentity } from "@/generated/api/SelfCareIdentity";
-import { ApimUtils } from "@io-services-cms/external-clients";
-import { readableReport } from "@pagopa/ts-commons/lib/reporters";
-import { EmailString } from "@pagopa/ts-commons/lib/strings";
-import * as E from "fp-ts/lib/Either";
-import * as TE from "fp-ts/lib/TaskEither";
-import { flow, pipe } from "fp-ts/lib/function";
-import { createRemoteJWKSet, jwtVerify } from "jose";
+import { getConfiguration } from "@/config";
+import * as jose from "jose";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { ApimUser, IdentityTokenPayload } from "../types";
+import { authorize } from "./auth";
 
 if (
   getConfiguration().SELFCARE_API_MOCKING ||
@@ -24,27 +17,7 @@ const authOptions: NextAuthOptions = {
     CredentialsProvider({
       id: "access-control",
       credentials: {},
-      authorize(credentials) {
-        const {
-          identity_token: identity_token
-        } = credentials as SelfCareIdentity;
-
-        const config = getConfiguration();
-
-        return pipe(
-          identity_token,
-          verifyToken(config),
-          TE.bindTo("identityTokenPayload"),
-          TE.bind("apimUser", ({ identityTokenPayload }) =>
-            retrieveApimUser(config)(identityTokenPayload)
-          ),
-          TE.map(toUser(config)),
-          TE.getOrElse(e => {
-            console.error(e); //TODO: use "proper" log
-            throw e;
-          })
-        )();
-      }
+      authorize: authorize(getConfiguration(), jose)
     })
   ],
   pages: {
@@ -80,94 +53,3 @@ const authOptions: NextAuthOptions = {
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
-
-const verifyToken = (config: Configuration) => (
-  identity_token: string
-): TE.TaskEither<Error, IdentityTokenPayload> =>
-  pipe(
-    TE.tryCatch(
-      () =>
-        jwtVerify(
-          identity_token,
-          createRemoteJWKSet(
-            new URL(`${config.SELFCARE_BASE_URL}${config.SELFCARE_JWKS_PATH}`)
-          ),
-          {
-            issuer: config.SELFCARE_BASE_URL,
-            audience: config.BACKOFFICE_DOMAIN
-          }
-        ),
-      E.toError
-    ),
-    TE.chain(({ payload }) =>
-      pipe(
-        payload,
-        IdentityTokenPayload.decode,
-        E.mapLeft(flow(readableReport, E.toError)),
-        TE.fromEither
-      )
-    )
-  );
-
-const retrieveApimUser = (config: Configuration) => (
-  identityTokenPayload: IdentityTokenPayload
-): TE.TaskEither<Error, ApimUser> =>
-  pipe(
-    ApimUtils.getApimClient(config, config.AZURE_SUBSCRIPTION_ID),
-    apimClient =>
-      ApimUtils.getApimService(
-        apimClient,
-        config.AZURE_APIM_RESOURCE_GROUP,
-        config.AZURE_APIM
-      ),
-    apimService =>
-      apimService.getUserByEmail(
-        `org.${identityTokenPayload.organization.id}@selfcare.io.pagopa.it` as EmailString,
-        true
-      ),
-    TE.mapLeft(
-      err =>
-        new Error(`Failed to fetch user by its email, code: ${err.statusCode}`)
-    ),
-    TE.chain(TE.fromOption(() => new Error(`Cannot find user`))),
-    TE.chain(
-      flow(
-        ApimUser.decode,
-        E.mapLeft(flow(readableReport, E.toError)),
-        TE.fromEither
-      )
-    )
-  );
-
-const toUser = (config: Configuration) => ({
-  identityTokenPayload,
-  apimUser
-}: {
-  identityTokenPayload: IdentityTokenPayload;
-  apimUser: ApimUser;
-}) => ({
-  id: identityTokenPayload.uid,
-  name: `${identityTokenPayload.name} ${identityTokenPayload.family_name}`,
-  email: identityTokenPayload.email,
-  institution: {
-    id: identityTokenPayload.organization.id,
-    name: identityTokenPayload.organization.name,
-    role: identityTokenPayload.organization.roles[0]?.role,
-    logo_url: "url"
-  },
-  authorizedInstitutions: [
-    {
-      id: "id_2",
-      name: "name_2",
-      role: "operator"
-    }
-  ],
-  permissions: apimUser.groups
-    .filter(group => group.type === "custom")
-    .map(group => group.name),
-  parameters: {
-    userId: apimUser.id,
-    userEmail: apimUser.email,
-    subscriptionId: config.AZURE_SUBSCRIPTION_ID
-  }
-});
