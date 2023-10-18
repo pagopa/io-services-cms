82da76ae0597471a44525320647acb62316b681d
Gianmaria Scorza <gianmaria@scorza.dev>
2023-10-18 17:17:10 +0200

    [#IOPAE-678] Refactor module pattern (#417)
    * moved src/app/api/lib -> src/lib/be
    
    * moved src/app/api/lib -> src/lib/be
    
    * moved src/app/api/lib -> src/lib/be
    
    * add legacy-cosmos.ts, module to connect to Legacy Cosmos, add lib/be/keys which contains all necessary to /keys/manage APIs
    
    * module pattern for api /keys/manage/cidrs
    
    * cleanUp unnecessary files
    
    * rename handler-wrapper in wrappers
    
    * retrieve authorized cidrs test
    
    * added unit test for updateAuthorizedCIDRs
    
    * mangeKeys api refactored
    
    * cleanUp
    
    * add manage keys business unit tests
    
    * enable msw mocking for selfcare
    
    * wrappers module pattern
    
    * add GET Authorized CIDRs unit test
    
    * add Upsert Authorized CIDRs tests
    
    * add 500 error test Upsert managed CIDRs
    
    * Add retrieve manage subscription keys API unit tests
    
    * Add regenerate manage subscription keys API unit tests
    
    * test cleanUp
    
    * test cleanUp
    
    * fix sonar bug
    
    * fix io-services-cms test
    
    * changeset
    
    * cleanUp
    
    * retrieve manageKey test cleanUp
    
    * cleanUp
    
    * change return title on error for manageKey retriee and regenerate
    
    * remove cache on getIoServicesCmsClientConfig
    
    * remove cache from getApimConfig and getApimClient
    
    * refactor apim-service
    
    * cleanUp

---
+++ apps/backoffice/src/app/api/keys/manage/cidrs/__tests__/route.test.ts
@@ -0,0 +1,152 @@
+import { afterEach, describe, expect, it, vi } from "vitest";
+
+import { NextRequest } from "next/server";
+import { BackOfficeUser } from "../../../../../../../types/next-auth";
+import { Cidr } from "../../../../../../generated/api/Cidr";
+import { GET, PUT } from "../route";
+
+const mocks: {
+  authrizedCIDRs: Cidr[];
+  jwtMock: BackOfficeUser;
+} = vi.hoisted(() => ({
+  authrizedCIDRs: ["127.0.0.1" as Cidr],
+  jwtMock: ({
+    permissions: ["permission1", "permission2"],
+    parameters: {
+      userEmail: "anEmail@email.it",
+      userId: "anUserId",
+      subscriptionId: "aSubscriptionId"
+    }
+  } as unknown) as BackOfficeUser
+}));
+
+const { getToken } = vi.hoisted(() => ({
+  getToken: vi.fn().mockReturnValue(Promise.resolve(mocks.jwtMock))
+}));
+
+const {
+  retrieveManageSubscriptionAuthorizedCIDRs,
+  upsertManageSubscriptionAuthorizedCIDRs
+} = vi.hoisted(() => ({
+  retrieveManageSubscriptionAuthorizedCIDRs: vi
+    .fn()
+    .mockReturnValue(Promise.resolve(mocks.authrizedCIDRs)),
+  upsertManageSubscriptionAuthorizedCIDRs: vi
+    .fn()
+    .mockReturnValue(Promise.resolve(mocks.authrizedCIDRs))
+}));
+
+vi.mock("@/lib/be/keys/business", () => ({
+  retrieveManageSubscriptionAuthorizedCIDRs,
+  upsertManageSubscriptionAuthorizedCIDRs
+}));
+
+vi.mock("next-auth/jwt", async () => {
+  const actual = await vi.importActual("next-auth/jwt");
+  return {
+    ...(actual as any),
+    getToken
+  };
+});
+
+afterEach(() => {
+  vi.resetAllMocks();
+  vi.restoreAllMocks();
+});
+
+describe("Authorized CIDRs API", () => {
+  describe("Retrieve", () => {
+    it("should return 200", async () => {
+      retrieveManageSubscriptionAuthorizedCIDRs.mockReturnValueOnce(
+        Promise.resolve(mocks.authrizedCIDRs)
+      );
+      getToken.mockReturnValueOnce(Promise.resolve(mocks.jwtMock));
+
+      // Mock NextRequest
+      const request = ({
+        bodyUsed: false
+      } as any) as NextRequest;
+
+      const result = await GET(request, {});
+
+      //extract jsonBody from NextResponse
+      const jsonResponse = await new Response(result.body).json();
+
+      expect(result.status).toBe(200);
+      expect(jsonResponse.cidrs).toEqual(mocks.authrizedCIDRs);
+    });
+
+    it("should return 500", async () => {
+      retrieveManageSubscriptionAuthorizedCIDRs.mockRejectedValueOnce(
+        "an error"
+      );
+      getToken.mockReturnValueOnce(Promise.resolve(mocks.jwtMock));
+
+      // Mock NextRequest
+      const request = ({
+        bodyUsed: false
+      } as any) as NextRequest;
+
+      const result = await GET(request, {});
+
+      expect(result.status).toBe(500);
+    });
+  });
+
+  describe("Update", () => {
+    it("should return 200", async () => {
+      upsertManageSubscriptionAuthorizedCIDRs.mockReturnValueOnce(
+        Promise.resolve(mocks.authrizedCIDRs)
+      );
+      getToken.mockReturnValueOnce(Promise.resolve(mocks.jwtMock));
+
+      // Mock NextRequest
+      const request = ({
+        bodyUsed: false,
+        json: () => Promise.resolve({ cidrs: mocks.authrizedCIDRs })
+      } as any) as NextRequest;
+
+      const result = await PUT(request, {});
+
+      //extract jsonBody from NextResponse
+      const jsonResponse = await new Response(result.body).json();
+
+      expect(result.status).toBe(200);
+      expect(jsonResponse.cidrs).toEqual(mocks.authrizedCIDRs);
+    });
+
+    it("should return 400", async () => {
+      upsertManageSubscriptionAuthorizedCIDRs.mockReturnValueOnce(
+        Promise.resolve(mocks.authrizedCIDRs)
+      );
+      getToken.mockReturnValueOnce(Promise.resolve(mocks.jwtMock));
+
+      // Mock NextRequest
+      const request = ({
+        bodyUsed: false,
+        json: () => Promise.resolve({ aNotValidProp: "aNotValidProp" })
+      } as any) as NextRequest;
+
+      const result = await PUT(request, {});
+
+      expect(result.status).toBe(400);
+    });
+
+    //mockRejectedValueOnce
+
+    it("should return 500", async () => {
+      upsertManageSubscriptionAuthorizedCIDRs.mockRejectedValueOnce("error");
+      getToken.mockReturnValueOnce(Promise.resolve(mocks.jwtMock));
+
+      // Mock NextRequest
+      const request = ({
+        bodyUsed: false,
+        json: () => Promise.resolve({ cidrs: mocks.authrizedCIDRs })
+      } as any) as NextRequest;
+
+      const result = await PUT(request, {});
+
+      expect(result.status).toBe(500);
+    });
+  });
+});
--- apps/backoffice/src/app/api/services/route.ts
+++ apps/backoffice/src/app/api/services/route.ts
@@ -1,45 +1,32 @@
-import {
-  buildClient,
-  forwardIoServicesCmsRequest
-} from "@/app/api/lib/io-services-cms-proxy";
-import { getConfiguration } from "@/config";
-import { withJWTAuthHandler } from "@/app/api/lib/handler-wrappers";
+import { forwardIoServicesCmsRequest } from "@/lib/be/cms/business";
+import { withJWTAuthHandler } from "@/lib/be/wrappers";
 import { NextRequest } from "next/server";
 import { BackOfficeUser } from "../../../../types/next-auth";
 
-const configuration = getConfiguration();
-const client = buildClient(configuration);
 /**
  * @description Create a new Service with the attributes provided in the request payload
  */
-const createService = (
-  request: NextRequest,
-  { backofficeUser }: { backofficeUser: BackOfficeUser }
-) =>
-  forwardIoServicesCmsRequest(client)("createService", request, backofficeUser);
+export const POST = withJWTAuthHandler(
+  (
+    request: NextRequest,
+    { backofficeUser }: { backofficeUser: BackOfficeUser }
+  ) => forwardIoServicesCmsRequest("createService", request, backofficeUser)
+);
 
 /**
  * @description Retrieve all services owned by the calling user
  */
-const getServices = (
-  request: NextRequest,
-  { backofficeUser }: { backofficeUser: BackOfficeUser }
-) => {
-  const limit = request.nextUrl.searchParams.get("limit");
-  const offset = request.nextUrl.searchParams.get("offset");
+export const GET = withJWTAuthHandler(
+  (
+    request: NextRequest,
+    { backofficeUser }: { backofficeUser: BackOfficeUser }
+  ) => {
+    const limit = request.nextUrl.searchParams.get("limit");
+    const offset = request.nextUrl.searchParams.get("offset");
 
-  return forwardIoServicesCmsRequest(client)(
-    "getServices",
-    request,
-    backofficeUser,
-    {
+    return forwardIoServicesCmsRequest("getServices", request, backofficeUser, {
       limit: limit ?? undefined,
       offset: offset ?? undefined
-    }
-  );
-};
-
-export const {
-  GET = withJWTAuthHandler(getServices),
-  POST = withJWTAuthHandler(createService)
-} = {};
+    });
+  }
+);
