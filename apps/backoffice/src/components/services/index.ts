971e256f7464a0a20df95fff8823bb7388944260
Giuseppe Di Pinto <giuseppeantonio.dipinto@pagopa.it>
2023-10-25 11:37:48 +0200

    add service description preview

--- apps/backoffice/src/components/services/index.ts
+++ apps/backoffice/src/components/services/index.ts
@@ -3,4 +3,5 @@ export * from "./service-alerts";
 export * from "./service-context-menu";
 export * from "./service-info";
 export * from "./service-logo";
+export * from "./service-preview";
 export * from "./service-status";
