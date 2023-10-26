a44053d0c9da12ded61f409b3718d3af6b7a2a32
Gianmaria Scorza <gianmaria@scorza.dev>
2023-10-26 12:12:20 +0200

    add Apim health check

--- apps/backoffice/src/lib/be/apim-service.ts
+++ apps/backoffice/src/lib/be/apim-service.ts
@@ -4,17 +4,19 @@ import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import * as E from "fp-ts/lib/Either";
 import * as t from "io-ts";
 import { cache } from "react";
+import { HealthChecksError } from "./errors";
 
 const Config = t.type({
   AZURE_CLIENT_SECRET_CREDENTIAL_CLIENT_ID: NonEmptyString,
   AZURE_CLIENT_SECRET_CREDENTIAL_SECRET: NonEmptyString,
   AZURE_CLIENT_SECRET_CREDENTIAL_TENANT_ID: NonEmptyString,
+  AZURE_APIM_SUBSCRIPTION_PRODUCT_NAME: NonEmptyString,
   AZURE_SUBSCRIPTION_ID: NonEmptyString,
   AZURE_APIM_RESOURCE_GROUP: NonEmptyString,
   AZURE_APIM: NonEmptyString
 });
 
-const getApimConfig = () => {
+const getApimConfig = cache(() => {
   const result = Config.decode(process.env);
 
   if (E.isLeft(result)) {
@@ -23,7 +25,7 @@ const getApimConfig = () => {
     );
   }
   return result.right;
-};
+});
 
 export const getApimService = cache(() => {
   // Apim Service, used to operates on Apim resources
@@ -38,3 +40,18 @@ export const getApimService = cache(() => {
     apimConfig.AZURE_APIM
   );
 });
+
+export const getApimHealth = async () => {
+  try {
+    const { AZURE_APIM_SUBSCRIPTION_PRODUCT_NAME } = getApimConfig();
+    const apimService = getApimService();
+    const res = await apimService.getProductByName(
+      AZURE_APIM_SUBSCRIPTION_PRODUCT_NAME
+    )();
+    if (!res) {
+      throw new Error();
+    }
+  } catch (e) {
+    throw new HealthChecksError("APIM", e);
+  }
+};
