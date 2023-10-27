fd239259569de739a17653d3727a0b9c5520c2f9
Gianmaria Scorza <gianmaria@scorza.dev>
2023-10-27 12:53:23 +0200

    [#IOPAE-737] ADD GET health api in B4F (#450)
    * add health API on B4F openapi.yaml
    
    * add GET /api/health API
    
    * changeset

---
+++ apps/backoffice/src/app/api/health/route.ts
@@ -0,0 +1,31 @@
+import {
+  HTTP_STATUS_INTERNAL_SERVER_ERROR,
+  HTTP_STATUS_OK
+} from "@/config/constants";
+import { getApimHealth } from "@/lib/be/apim-service";
+import { getIoServicesCmsHealth } from "@/lib/be/cms-client";
+import healthcheck from "@/lib/be/healthcheck";
+import { getLegacyCosmosHealth } from "@/lib/be/legacy-cosmos";
+import { getSelfcareHealth } from "@/lib/be/selfcare-client";
+import { NextResponse } from "next/server";
+
+/**
+ * `api/healt` route handler
+ * @returns project _name_ and _version_
+ */
+export async function GET() {
+  const health = await healthcheck([
+    getLegacyCosmosHealth(),
+    getApimHealth(),
+    getSelfcareHealth(),
+    getIoServicesCmsHealth()
+  ]);
+  const status =
+    health.status === "ok" ? HTTP_STATUS_OK : HTTP_STATUS_INTERNAL_SERVER_ERROR;
+
+  const response = {
+    status: health.status
+  };
+
+  return NextResponse.json(response, { status });
+}
