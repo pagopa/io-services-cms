67381a6bdcc557625f10474176c7c8624018486f
Gianmaria Scorza <gianmaria@scorza.dev>
2023-10-30 16:48:33 +0100

    Revert "operationId serviceList renamed to getServiceList"
    This reverts commit 7e2004ea5e34e5f7e77d0a13bf1e9852b139fa4a.

--- apps/backoffice/src/lib/be/apim-service.ts
+++ apps/backoffice/src/lib/be/apim-service.ts
@@ -1,13 +1,6 @@
-import { SubscriptionCollection } from "@azure/arm-apimanagement";
-import {
-  AzureAuthorityHosts,
-  ClientSecretCredential,
-  ErrorResponse
-} from "@azure/identity";
 import { ApimUtils } from "@io-services-cms/external-clients";
 import { readableReport } from "@pagopa/ts-commons/lib/reporters";
 import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
-import axios from "axios";
 import * as E from "fp-ts/lib/Either";
 import * as t from "io-ts";
 import { cache } from "react";
@@ -47,57 +40,6 @@ export const getApimService: () => ApimUtils.ApimService = cache(() => {
     apimConfig.AZURE_APIM_RESOURCE_GROUP,
     apimConfig.AZURE_APIM
   );
-});
-
-const getAxiosInstance = cache(async () => {
-  const apimConfig = getApimConfig();
-
-  const credential = new ClientSecretCredential(
-    apimConfig.AZURE_CLIENT_SECRET_CREDENTIAL_TENANT_ID,
-    apimConfig.AZURE_CLIENT_SECRET_CREDENTIAL_CLIENT_ID,
-    apimConfig.AZURE_CLIENT_SECRET_CREDENTIAL_SECRET,
-    {
-      authorityHost: AzureAuthorityHosts.AzurePublicCloud
-    }
-  );
-  const tokenResponse = await credential.getToken(
-    "https://management.azure.com/.default"
-  );
-
-  const accessToken = tokenResponse?.token || null;
-
-  if (!accessToken) {
-    throw new Error("invalid or null accessToken");
-  }
-
-  return axios.create({
-    baseURL: "https://management.azure.com/subscriptions",
-    timeout: 5000,
-    headers: { Authorization: `Bearer ${accessToken}` }
-  });
-});
-
-export const getApimRestClient = cache(async () => {
-  const apimConfig = getApimConfig();
-  const axiosInstance = await getAxiosInstance();
-
-  const getServiceList = async (
-    userId: string,
-    limit: number,
-    offset: number
-  ) => {
-    const subscriptionListUrl = `${apimConfig.AZURE_SUBSCRIPTION_ID}/resourceGroups/${apimConfig.AZURE_APIM_RESOURCE_GROUP}/providers/Microsoft.ApiManagement/service/${apimConfig.AZURE_APIM}/users/${userId}/subscriptions?api-version=2022-08-01&%24skip=${offset}&%24top=${limit}`;
-
-    const { data, status } = await axiosInstance.get<
-      SubscriptionCollection | ErrorResponse
-    >(subscriptionListUrl);
-
-    return { data, status };
-  };
-
-  return {
-    getServiceList
-  };
 });
 
 export const getApimHealth: () => Promise<void> = async () => {
