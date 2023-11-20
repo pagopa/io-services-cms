cecb2e9e72cd43f731d39f17d21fbf115169f9d5
Giuseppe Di Pinto <118166285+giuseppedipinto@users.noreply.github.com>
2023-11-20 14:19:03 +0100

    [IOPAE-809] Add product switch on BackOffice IO (#562)
    * add selfcare product Id and Title
    
    * add product switch on header component
    
    * add selfcare mocks for dashboard, users and groups
    
    * changeset

--- apps/backoffice/src/components/headers/header.tsx
+++ apps/backoffice/src/components/headers/header.tsx
@@ -5,23 +5,46 @@ import { signOut, useSession } from "next-auth/react";
 import { useTranslation } from "next-i18next";
 import { useEffect, useState } from "react";
 
-const products: ProductSwitchItem[] = [
-  {
-    id: getConfiguration().BACK_OFFICE_ID,
-    title: getConfiguration().BACK_OFFICE_TITLE,
-    productUrl: "",
-    linkType: "internal"
-  }
-];
-
 export const Header = () => {
   const { t } = useTranslation();
   const { data: session } = useSession();
+
+  const getSelfcareInstitutionDashboardUrl = () =>
+    `${getConfiguration().SELFCARE_URL}/dashboard/${
+      session?.user?.institution.id
+    }`;
+
+  const products: ProductSwitchItem[] = [
+    {
+      id: getConfiguration().BACK_OFFICE_ID,
+      title: getConfiguration().BACK_OFFICE_TITLE,
+      productUrl: "",
+      linkType: "internal"
+    },
+    {
+      id: getConfiguration().SELFCARE_ID,
+      title: getConfiguration().SELFCARE_TITLE,
+      productUrl: getSelfcareInstitutionDashboardUrl(),
+      linkType: "internal"
+    }
+  ];
+
   const [parties, setParties] = useState(Array<PartySwitchItem>());
   const [selectedPartyId, setSelectedPartyId] = useState("");
+  const [selectedProductId, setSelectedProductId] = useState(products[0].id);
+
+  const selectedProductChange = (product: ProductSwitchItem) => {
+    // no action if user click on current product (i.e.: BackOffice IO)
+    if (selectedProductId === product.id) return;
+    // otherwise navigate to product url (i.e.: SelfCare)
+    setSelectedProductId(product.id);
+    window.location.href = product.productUrl;
+  };
 
   const selectedPartyChange = (party: PartySwitchItem) => {
+    // no action if user click on current institution
     if (selectedPartyId === party.id) return;
+    // otherwise perform next/auth logout and navigate to callbackUrl
     setSelectedPartyId(party.id);
     signOut({
       callbackUrl:
@@ -57,9 +80,9 @@ export const Header = () => {
 
   return (
     <HeaderProduct
-      productId={products[0].id}
+      productId={selectedProductId}
       productsList={products}
-      onSelectedProduct={p => console.log("Selected Product:", p.title)}
+      onSelectedProduct={selectedProductChange}
       onSelectedParty={selectedPartyChange}
       partyList={parties}
       partyId={selectedPartyId}
