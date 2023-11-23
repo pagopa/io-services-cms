b2bfc1637c740fbd6d7a2c23ee7de34d1db1fa4f
Gianmaria Scorza <gianmaria@scorza.dev>
2023-11-23 18:13:56 +0100

    reintroduced GET /api/institutions API, which returns all the user's authorized institutions

---
+++ apps/backoffice/src/app/api/institutions/route.ts
@@ -0,0 +1,29 @@
+import { handleInternalErrorResponse } from "@/lib/be/errors";
+import { retrieveUserAuthorizedInstitutions } from "@/lib/be/institutions/business";
+import { withJWTAuthHandler } from "@/lib/be/wrappers";
+import { NextRequest, NextResponse } from "next/server";
+import { BackOfficeUser } from "../../../../types/next-auth";
+
+/**
+ * @description Retrieves all the onboarded institutions related
+ * to the provided user and the product retrieved from Subscription Key
+ */
+export const GET = withJWTAuthHandler(
+  async (
+    request: NextRequest,
+    { backofficeUser }: { backofficeUser: BackOfficeUser }
+  ) => {
+    try {
+      const institutionResponse = await retrieveUserAuthorizedInstitutions(
+        backofficeUser.id
+      );
+      return NextResponse.json(institutionResponse);
+    } catch (error) {
+      console.error(
+        `An Error has occurred while retrieving authorized institution for user having selfcareUserId: ${backofficeUser.id}, apimManageSubscriptionId: ${backofficeUser.parameters.subscriptionId}, caused by: `,
+        error
+      );
+      return handleInternalErrorResponse("InstitutionsRetrieveError", error);
+    }
+  }
+);
--- apps/backoffice/src/lib/be/institutions/business.ts
+++ apps/backoffice/src/lib/be/institutions/business.ts
@@ -1,8 +1,55 @@
 import { Institution as BackofficeInstitution } from "@/generated/api/Institution";
-import { getInstitutionById } from "./selfcare";
+import {
+  InstitutionResource,
+  InstitutionResources
+} from "@/types/selfcare/InstitutionResource";
+import { EmailString } from "@pagopa/ts-commons/lib/strings";
+import * as O from "fp-ts/lib/Option";
+import { pipe } from "fp-ts/lib/function";
+import { getInstitutionById, getUserAuthorizedInstitutions } from "./selfcare";
+
+export const retrieveUserAuthorizedInstitutions = async (
+  selfCareUserId: string
+): Promise<InstitutionResources> => {
+  const apiResult = await getUserAuthorizedInstitutions(selfCareUserId);
+  return apiResult.map(toInstitution);
+};
 
 export const retrieveInstitution = async (
   institutionId: string
 ): Promise<BackofficeInstitution> => {
   return await getInstitutionById(institutionId);
 };
+
+const toInstitution = (
+  institutionResource: InstitutionResource
+): BackofficeInstitution => ({
+  id: institutionResource.id,
+  externalId: institutionResource.externalId,
+  originId: institutionResource.originId,
+  description: institutionResource.description,
+  digitalAddress: pipe(
+    institutionResource.digitalAddress,
+    EmailString.decode,
+    O.fromEither,
+    O.toUndefined
+  ),
+  address: institutionResource.address,
+  zipCode: institutionResource.zipCode,
+  taxCode: institutionResource.taxCode,
+  origin: institutionResource.origin,
+  institutionType: institutionResource.institutionType,
+  paymentServiceProvider: institutionResource.pspData,
+  dataProtectionOfficer: institutionResource.dpoData,
+  rea: institutionResource.companyInformations?.rea,
+  shareCapital: institutionResource.companyInformations?.shareCapital,
+  businessRegisterPlace:
+    institutionResource.companyInformations?.businessRegisterPlace,
+  supportEmail: institutionResource.assistanceContacts?.supportEmail,
+  supportPhone: institutionResource.assistanceContacts?.supportPhone,
+  subunitCode: institutionResource.subunitCode,
+  subunitType: institutionResource.subunitType,
+  aooParentCode: institutionResource.aooParentCode,
+  rootParent: institutionResource.rootParent,
+  logo: institutionResource.logo
+});
