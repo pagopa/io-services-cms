7716df0fb53fcc3674abdc409b4f64af5eb37586
Stefano.D'aversa <stefano.daversa@pagopa.it>
2023-12-04 16:17:26 +0100

    add max_allowed_payment_amount in api response

--- apps/io-services-cms-webapp/src/utils/converters/service-lifecycle-converters.ts
+++ apps/io-services-cms-webapp/src/utils/converters/service-lifecycle-converters.ts
@@ -52,6 +52,7 @@ export const itemToResponse = ({
   require_secure_channel: data.require_secure_channel,
   authorized_recipients: data.authorized_recipients,
   authorized_cidrs: data.authorized_cidrs,
+  max_allowed_payment_amount: data.max_allowed_payment_amount,
 });
 
 export const toServiceStatus = (
