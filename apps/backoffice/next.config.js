9c3089f26f630bd9a4b1106c28ee3ff71aebbb8e
Giuseppe Di Pinto <giuseppeantonio.dipinto@pagopa.it>
2023-12-14 11:35:57 +0100

    add webpack config to avoid build time msw config resolution error

--- apps/backoffice/next.config.js
+++ apps/backoffice/next.config.js
@@ -8,6 +8,24 @@ const nextConfig = {
   output: "standalone",
   experimental: {
     instrumentationHook: true
+  },
+  // https://github.com/mswjs/msw/issues/1801
+  // Setting `resolve.alias` to `false` will tell webpack to ignore a module.
+  // `msw/node` is a server-only module that exports methods not available in the `browser`.
+  // `msw/browser` is a client-only module that exports methods not available on the `server`.
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "msw/browser": false
+      };
+    } else {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "msw/node": false
+      };
+    }
+    return config;
   }
 };
 
