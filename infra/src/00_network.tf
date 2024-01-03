2628848276cbc8f6545d0027925ce747a7c62acc
Stefano.D'aversa <stefano.daversa@pagopa.it>
2024-01-03 15:25:05 +0100

    add devportal subnet

--- infra/src/00_network.tf
+++ infra/src/00_network.tf
@@ -43,3 +43,9 @@ data "azurerm_subnet" "appgateway_snet" {
   virtual_network_name = var.io_common.vnet_name
   resource_group_name  = var.io_common.resource_group_name
 }
+
+data "azurerm_subnet" "devportal_snet" {
+  name                 = "${var.prefix}-selfcare-be-common-snet"
+  virtual_network_name = var.io_common.vnet_name
+  resource_group_name  = var.io_common.resource_group_name
+}
