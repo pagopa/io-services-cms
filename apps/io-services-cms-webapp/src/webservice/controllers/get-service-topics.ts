8bfe879173fc0cd32cd9c7c1a70dd4e34ca2bb47
Gianmaria Scorza <gianmaria@scorza.dev>
2024-01-08 16:38:14 +0100

    clean question comment

--- apps/io-services-cms-webapp/src/webservice/controllers/get-service-topics.ts
+++ apps/io-services-cms-webapp/src/webservice/controllers/get-service-topics.ts
@@ -55,7 +55,6 @@ export const makeGetServiceTopicsHandler =
       TE.map(
         flow(
           O.fold(
-            // QUESTION: when no topics are found on DB should we return an empty list or an error 404?
             () => ResponseSuccessJson({ topics: [] }),
             (topics) => ResponseSuccessJson({ topics })
           )
