7d59438d9328fb2d2b9eb6c0be312d9f88bee73a
Giuseppe Di Pinto <giuseppeantonio.dipinto@pagopa.it>
2024-01-18 15:14:16 +0100

    fix merge error

--- apps/backoffice/next.config.js
+++ apps/backoffice/next.config.js
@@ -2,7 +2,7 @@ const { i18n } = require("./next-i18next.config");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
   transpilePackages: ["@pagopa/mui-italia"],
   i18n,
   output: "standalone",
