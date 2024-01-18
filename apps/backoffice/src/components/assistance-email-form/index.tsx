45afed5815d7a21396c2a0f0b5e2512b78ee16a4
Giuseppe Di Pinto <giuseppeantonio.dipinto@pagopa.it>
2024-01-18 13:27:50 +0100

    draft assistance page and email form

---
+++ apps/backoffice/src/components/assistance-email-form/index.tsx
@@ -0,0 +1,94 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Grid, Typography } from "@mui/material";
+import { TFunction } from "i18next";
+import { useTranslation } from "next-i18next";
+import { FormProvider, useForm } from "react-hook-form";
+import * as z from "zod";
+import { ButtonBack, ButtonNext } from "../buttons";
+import { CardBaseContainer } from "../cards/card-base-container";
+import { TextFieldController } from "../forms/controllers";
+
+export type AssistanceEmailFormProps = {
+  onBack: () => void;
+  onComplete: (email: string) => void;
+};
+
+const defaultFormValues = {
+  email: "",
+  confirmEmail: ""
+};
+
+const getValidationSchema = (t: TFunction<"translation", undefined>) =>
+  z
+    .object({
+      email: z.string().email(t("forms.errors.field.email")),
+      confirmEmail: z.string().email(t("forms.errors.field.email"))
+    })
+    .refine(
+      schema =>
+        schema.confirmEmail !== "" && schema.email === schema.confirmEmail,
+      {
+        message: t("forms.errors.field.confirmEmail"),
+        path: ["confirmEmail"]
+      }
+    );
+
+/** Wrapper for content rendering based on user permissions/role match */
+export const AssistanceEmailForm = ({
+  onBack,
+  onComplete
+}: AssistanceEmailFormProps) => {
+  const { t } = useTranslation();
+
+  const methods = useForm({
+    defaultValues: defaultFormValues,
+    resolver: zodResolver(getValidationSchema(t)),
+    mode: "onChange"
+  });
+
+  const {
+    getValues,
+    formState: { isValid }
+  } = methods;
+
+  return (
+    <>
+      <CardBaseContainer>
+        <FormProvider {...methods}>
+          <TextFieldController
+            name="email"
+            label={t("forms.assistance.email.label")}
+            placeholder={t("forms.assistance.email.placeholder")}
+            size="small"
+          />
+          <TextFieldController
+            name="confirmEmail"
+            label={t("forms.assistance.confirmEmail.label")}
+            placeholder={t("forms.assistance.confirmEmail.placeholder")}
+            size="small"
+          />
+        </FormProvider>
+      </CardBaseContainer>
+      <Typography
+        variant="body2"
+        color="text.secondary"
+        marginTop={2}
+        dangerouslySetInnerHTML={{
+          __html: t("forms.assistance.disclaimer")
+        }}
+      ></Typography>
+      <Grid container spacing={0} marginY={4}>
+        <Grid item xs={6}>
+          <ButtonBack onClick={onBack} />
+        </Grid>
+        <Grid item xs={6} textAlign="right">
+          <ButtonNext
+            label="buttons.forward"
+            onClick={() => onComplete(getValues("email"))}
+            disabled={!isValid}
+          />
+        </Grid>
+      </Grid>
+    </>
+  );
+};
---
+++ apps/backoffice/src/pages/assistance.tsx
@@ -0,0 +1,46 @@
+import { AssistanceEmailForm } from "@/components/assistance-email-form";
+import { PageHeader } from "@/components/headers";
+import { AppLayout, PageLayout } from "@/layouts";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useRouter } from "next/router";
+import { ReactElement } from "react";
+
+const pageTitleLocaleKey = "routes.assistance.title";
+const pageDescriptionLocaleKey = "routes.assistance.description";
+
+export default function Assistance() {
+  const { t } = useTranslation();
+  const router = useRouter();
+  const { callbackUrl } = router.query;
+
+  return (
+    <>
+      <PageHeader
+        title={pageTitleLocaleKey}
+        description={pageDescriptionLocaleKey}
+      />
+      <AssistanceEmailForm
+        onBack={() => router.push((callbackUrl as string) ?? "/")}
+        onComplete={email => console.log("onComplete", email)}
+      />
+    </>
+  );
+}
+
+export async function getStaticProps({ locale }: any) {
+  return {
+    props: {
+      // pass the translation props to the page component
+      ...(await serverSideTranslations(locale))
+    }
+  };
+}
+
+Assistance.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <AppLayout hideHeader hideSidenav>
+      <PageLayout isFullWidth={false}>{page}</PageLayout>
+    </AppLayout>
+  );
+};
