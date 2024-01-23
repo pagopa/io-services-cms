b0f9a387ba37fec5c3ef0ae413e9fc72f715c31b
Giuseppe Di Pinto <118166285+giuseppedipinto@users.noreply.github.com>
2024-01-23 15:49:49 +0100

    [IOPAE-881] MUI Chip custom styleOverrides (#681)
    * add custom chip colors
    
    * changeset

---
+++ apps/backoffice/src/config/app-theme.ts
@@ -0,0 +1,30 @@
+import { createTheme } from "@mui/material/styles";
+import { theme } from "@pagopa/mui-italia";
+
+/** App custom theme
+ *
+ * Based on `mui-italia` theme with some minor customizations. */
+export const appTheme = createTheme({
+  ...theme,
+  components: {
+    ...theme.components,
+    MuiChip: {
+      ...theme.components?.MuiChip,
+      styleOverrides: {
+        ...theme.components?.MuiChip?.styleOverrides,
+        colorSuccess: {
+          backgroundColor: "#E1F4E1",
+          color: "#224021"
+        },
+        colorWarning: {
+          backgroundColor: "#FFF5DA",
+          color: "#614C15"
+        },
+        colorError: {
+          backgroundColor: "#FFE0E0",
+          color: "#761F1F"
+        }
+      }
+    }
+  }
+});
