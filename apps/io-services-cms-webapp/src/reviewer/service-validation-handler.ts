0fb14c8f80d618589f0b140d82b3ba489b2b5ff2
Gianmaria Scorza <gianmaria@scorza.dev>
2024-03-11 15:44:50 +0100

    using STRINGEQUALS instead of UPPER...

--- apps/io-services-cms-webapp/src/reviewer/service-validation-handler.ts
+++ apps/io-services-cms-webapp/src/reviewer/service-validation-handler.ts
@@ -106,11 +106,11 @@ const getDuplicatesOnServicePublication =
   (item: Queue.RequestReviewItemStrict) =>
     servicePublicationCosmosHelper.fetchItems(
       {
-        query: `SELECT VALUE c.id FROM c WHERE UPPER(c.data.name) = @serviceName AND c.data.organization.fiscal_code = @organizationFiscalCode AND c.id != @currentServiceId`,
+        query: `SELECT VALUE c.id FROM c WHERE STRINGEQUALS(c.data.name, @serviceName, true) AND c.data.organization.fiscal_code = @organizationFiscalCode AND c.id != @currentServiceId`,
         parameters: [
           {
             name: "@serviceName",
-            value: item.data.name.toUpperCase(),
+            value: item.data.name,
           },
           {
             name: "@organizationFiscalCode",
