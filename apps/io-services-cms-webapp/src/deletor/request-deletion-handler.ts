eb540d89fa8441bc4c2613ed8974d77c374b259f
Gianmaria Scorza <gianmaria@scorza.dev>
2024-03-19 12:38:57 +0100

    add new OnRequestDeletion Queue Trigger Azure Function

---
+++ apps/io-services-cms-webapp/src/deletor/request-deletion-handler.ts
@@ -0,0 +1,56 @@
+import { Context } from "@azure/functions";
+import {
+  FsmItemNotFoundError,
+  Queue,
+  ServicePublication,
+} from "@io-services-cms/models";
+import { readableReport } from "@pagopa/ts-commons/lib/reporters";
+import * as E from "fp-ts/lib/Either";
+import * as T from "fp-ts/lib/Task";
+import * as TE from "fp-ts/lib/TaskEither";
+import { flow, pipe } from "fp-ts/lib/function";
+import { Json } from "io-ts-types";
+import { withJsonInput } from "../lib/azure/misc";
+
+const parseIncomingMessage = (
+  queueItem: Json
+): E.Either<Error, Queue.RequestDeletionItem> =>
+  pipe(
+    queueItem,
+    Queue.RequestDeletionItem.decode,
+    E.mapLeft(flow(readableReport, (_) => new Error(_)))
+  );
+
+export const handleQueueItem = (
+  context: Context,
+  queueItem: Json,
+  fsmPublicationClient: ServicePublication.FsmClient
+) =>
+  pipe(
+    queueItem,
+    parseIncomingMessage,
+    E.mapLeft((_) => new Error("Error while parsing incoming message")), // TODO: map as _permanent_ error
+    TE.fromEither,
+    TE.chainW(({ id }) => fsmPublicationClient.getStore().delete(id)),
+    TE.fold(
+      (e) => {
+        if (e instanceof FsmItemNotFoundError) {
+          context.log.info(
+            `Operation Completed no more action needed => ${e.message}`,
+            e
+          );
+          return T.of(void 0);
+        } else {
+          throw e;
+        }
+      },
+      (_) => T.of(void 0)
+    )
+  );
+
+export const createRequestDeletionHandler = (
+  fsmPublicationClient: ServicePublication.FsmClient
+): ReturnType<typeof withJsonInput> =>
+  withJsonInput((context, queueItem) =>
+    handleQueueItem(context, queueItem, fsmPublicationClient)()
+  );
