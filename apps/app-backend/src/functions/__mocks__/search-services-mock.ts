c7d25a941d09726a54209f4cecc29c80465ce517
Stefano.D'aversa <stefano.daversa@pagopa.it>
2024-04-22 18:21:44 +0200

    search services api

---
+++ apps/app-backend/src/functions/__mocks__/search-services-mock.ts
@@ -0,0 +1,18 @@
+import { ServiceMinified } from "../../generated/definitions/internal/ServiceMinified";
+import { SearchMappedResult } from "../../utils/azure-search/client";
+
+export const mockSearchServicesResult: SearchMappedResult<ServiceMinified> = {
+  resources: [
+    {
+      id: "01HKCDP7542ABQTNXGSFDXTG24",
+      name: "Service 1",
+      version: 1,
+    },
+    {
+      id: "01HKCDP7542ABQTNXGSFDXTG25",
+      name: "Service 2",
+      version: 2,
+    },
+  ],
+  count: 2,
+};
