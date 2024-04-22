3a8419583d9b50c079e47467ad8fe2baa4c7cc61
christian-calabrese <christian.calabrese@pagopa.it>
2024-04-22 16:40:41 +0200

    fix: networking and rbac resource groups

--- infra/_modules/ai_search/main.tf
+++ infra/_modules/ai_search/main.tf
@@ -31,6 +31,7 @@ resource "azurerm_private_endpoint" "srch" {
     name                           = azurerm_search_service.srch.name
     private_connection_resource_id = azurerm_search_service.srch.id
     is_manual_connection           = false
+    subresource_names              = ["searchService"]
   }
 
   private_dns_zone_group {
@@ -48,7 +49,7 @@ resource "azurerm_role_assignment" "search_to_cosmos_account_reader" {
 }
 
 resource "azurerm_cosmosdb_sql_role_assignment" "search_to_cosmos_data_reader" {
-  resource_group_name = var.resource_group_name
+  resource_group_name = "${var.prefix}-${var.env_short}-services-cms-rg"
   account_name        = data.azurerm_cosmosdb_account.cosmos.name
   role_definition_id  = "${data.azurerm_cosmosdb_account.cosmos.id}/sqlRoleDefinitions/00000000-0000-0000-0000-000000000001"
   principal_id        = azurerm_search_service.srch.identity[0].principal_id
--- infra/_modules/networking/main.tf
+++ infra/_modules/networking/main.tf
@@ -5,7 +5,7 @@
 module "snet" {
   source               = "github.com/pagopa/terraform-azurerm-v3//subnet?ref=v8.3.0"
   name                 = "${var.project}-${var.application_basename}-srch-snet-001"
-  resource_group_name  = var.resource_group_name
+  resource_group_name  = "${var.project}-common-rg-001"
   virtual_network_name = var.vnet_name
   address_prefixes     = var.snet_cidrs
 
