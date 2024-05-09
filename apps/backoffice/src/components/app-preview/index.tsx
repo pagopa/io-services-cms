e340671779ad7b34ee8a8a274c66dd68eebcb187
Carlo Bussi <carlo.bussi@itpartneritalia.com>
2024-05-09 17:02:15 +0200

    Added scroll infobox

--- apps/backoffice/src/components/app-preview/index.tsx
+++ apps/backoffice/src/components/app-preview/index.tsx
@@ -1,7 +1,9 @@
 import { Close, InfoOutlined } from "@mui/icons-material";
+import MouseIcon from "@mui/icons-material/Mouse";
 import { Box, Button, Dialog, Stack, Tooltip, Typography } from "@mui/material";
 import { useTranslation } from "next-i18next";
 
+import { useState } from "react";
 import styles from "./app-preview.module.css";
 
 type AppPreviewProps = {
@@ -9,8 +11,56 @@ type AppPreviewProps = {
   onClose: () => void;
 };
 
+const scrollInfoBox = (toggleInfobBox: () => void) => (
+  <Stack
+    width={140}
+    height={122}
+    padding={1}
+    borderRadius={2}
+    sx={{
+      zIndex: 99,
+      position: "absolute",
+      right: 100,
+      backgroundColor: "white",
+      boxShadow: "-5px 5px 30px gray"
+    }}
+  >
+    <Box display="flex" justifyContent="flex-end">
+      <Button
+        onClick={toggleInfobBox}
+        variant="text"
+        style={{
+          height: 24,
+          minWidth: 24,
+          padding: 0,
+          backgroundColor: "transparent"
+        }}
+      >
+        <Close fontSize="small" />
+      </Button>
+    </Box>
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      gap={1}
+      paddingBottom={1}
+    >
+      <MouseIcon />
+      <Typography fontSize="10px" textAlign="center" fontWeight="600">
+        Lorem ipsum dolor sit amet, qui minim labore adipisicing minim.
+      </Typography>{" "}
+    </Box>
+  </Stack>
+);
+
 export const AppPreview = ({ showPreview, onClose }: AppPreviewProps) => {
   const { t } = useTranslation();
+  const [isInfoOpen, setisInfoOpen] = useState(true);
+
+  const toggleInfoBox = () => {
+    setisInfoOpen(!isInfoOpen);
+  };
 
   return (
     <Dialog open={showPreview} onClose={onClose} disableScrollLock>
@@ -42,6 +92,7 @@ export const AppPreview = ({ showPreview, onClose }: AppPreviewProps) => {
             backgroundSize: "cover"
           }}
         >
+          {isInfoOpen && scrollInfoBox(toggleInfoBox)}
           <Box
             display="flex"
             padding={1}
@@ -96,7 +147,7 @@ export const AppPreview = ({ showPreview, onClose }: AppPreviewProps) => {
             </Box>
           </Box>
         </Box>
-        <Box textAlign={"center"}>
+        <Box textAlign="center">
           <Button
             size="medium"
             startIcon={<Close />}
