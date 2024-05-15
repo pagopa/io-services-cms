dbad5f78ce3fdeca080318946b052fdc0394410d
Carlo Bussi <carlo.bussi@itpartneritalia.com>
2024-05-15 14:12:06 +0200

    small fixes

--- apps/backoffice/src/components/app-preview/index.tsx
+++ apps/backoffice/src/components/app-preview/index.tsx
@@ -11,48 +11,52 @@ type AppPreviewProps = {
   onClose: () => void;
 };
 
-const scrollInfoBox = (toggleInfobBox: () => void) => (
-  <Stack
-    width={140}
-    height={122}
-    padding={1}
-    borderRadius={2}
-    sx={{
-      zIndex: 99,
-      position: "absolute",
-      right: 100,
-      backgroundColor: "white",
-      boxShadow: "-5px 5px 30px gray"
-    }}
-  >
-    <Box display="flex" justifyContent="flex-end">
-      <Button
-        onClick={toggleInfobBox}
-        variant="text"
-        style={{
-          height: 24,
-          minWidth: 24,
-          padding: 0,
-          backgroundColor: "transparent"
-        }}
-      >
-        <Close fontSize="small" />
-      </Button>
-    </Box>
-    <Box
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      gap={1}
-      paddingBottom={1}
+const ScrollInfoBox = (toggleInfobBox: () => void) => {
+  const { t } = useTranslation();
+
+  return (
+    <Stack
+      width={140}
+      height={122}
+      padding={1}
+      borderRadius={2}
+      sx={{
+        zIndex: 99,
+        position: "absolute",
+        right: 100,
+        backgroundColor: "white",
+        boxShadow: "-5px 5px 30px gray"
+      }}
     >
-      <MouseIcon />
-      <Typography fontSize="10px" textAlign="center" fontWeight="600">
-        Lorem ipsum dolor sit amet, qui minim labore adipisicing minim.
-      </Typography>{" "}
-    </Box>
-  </Stack>
-);
+      <Box display="flex" justifyContent="flex-end">
+        <Button
+          onClick={toggleInfobBox}
+          variant="text"
+          style={{
+            height: 24,
+            minWidth: 24,
+            padding: 0,
+            backgroundColor: "transparent"
+          }}
+        >
+          <Close fontSize="small" />
+        </Button>
+      </Box>
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        gap={1}
+        paddingBottom={1}
+      >
+        <MouseIcon color="action" />
+        <Typography fontSize="10px" textAlign="center" fontWeight="600">
+          {t("service.inAppPreview.scrollInfo")}
+        </Typography>
+      </Box>
+    </Stack>
+  );
+};
 
 export const AppPreview = ({ showPreview, onClose }: AppPreviewProps) => {
   const { t } = useTranslation();
@@ -92,7 +96,7 @@ export const AppPreview = ({ showPreview, onClose }: AppPreviewProps) => {
             backgroundSize: "cover"
           }}
         >
-          {isInfoOpen && scrollInfoBox(toggleInfoBox)}
+          {isInfoOpen && ScrollInfoBox(toggleInfoBox)}
           <Box
             display="flex"
             padding={1}
