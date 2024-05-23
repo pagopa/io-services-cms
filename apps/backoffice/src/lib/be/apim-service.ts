3a5cb2042955527845a46f1c4436c0ff3c54e07a
Gianmaria Scorza <gianmaria@scorza.dev>
2024-05-23 17:56:27 +0200

    clean

--- apps/backoffice/src/lib/be/apim-service.ts
+++ apps/backoffice/src/lib/be/apim-service.ts
@@ -179,7 +179,11 @@ export const getApimHealth: () => Promise<void> = async () => {
     const apimService = getApimService();
     const res = await apimService.getProductByName(AZURE_APIM_PRODUCT_NAME)();
     if (E.isLeft(res)) {
-      throw new HealthChecksError("apim", minifyApimError(res.left));
+      throw new Error(
+        `error getting apim product, ${JSON.stringify(
+          minifyApimError(res.left)
+        )}`
+      );
     }
   } catch (e) {
     throw new HealthChecksError("apim", e);
