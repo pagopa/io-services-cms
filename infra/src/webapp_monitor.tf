436839fe72c96f018e2fbf9c60957e3c2237bee5
Gianmaria Scorza <gianmaria@scorza.dev>
2024-05-27 15:07:34 +0200

    Add poison queue element presency alert

--- infra/src/webapp_monitor.tf
+++ infra/src/webapp_monitor.tf
@@ -1,3 +1,8 @@
+data "azurerm_monitor_action_group" "iopquarantineerror-action-group" {
+  resource_group_name = var.io_common.resource_group_name
+  name                = "iopquarantineerror"
+}
+
 resource "azurerm_monitor_metric_alert" "webapp_functions_app_health_check" {
   name                = "${module.webapp_functions_app.name}-health-check-failed"
   resource_group_name = azurerm_resource_group.rg.name
@@ -18,5 +23,34 @@ resource "azurerm_monitor_metric_alert" "webapp_functions_app_health_check" {
 
   action {
     action_group_id = data.azurerm_monitor_action_group.error_action_group.id
+  }
+}
+
+resource "azurerm_monitor_scheduled_query_rules_alert" "poison-queue-alert" {
+  name                = "io-services-cms-poison-queue"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  action {
+    action_group = [data.azurerm_monitor_action_group.iopquarantineerror-action-group.id]
+  }
+
+  data_source_id = module.storage_account.id
+
+  description = "Poison queue contains elements, this indicates failure in messages process"
+
+  query = <<-QUERY
+    StorageQueueLogs  
+    | where OperationName contains "PutMessage" 
+    | where Uri contains "-poison"
+    | count
+  QUERY
+
+  frequency   = "5" // minutes
+  time_window = "5" // minutes
+
+  trigger {
+    operator  = "GreaterThan"
+    threshold = 0
   }
 }
