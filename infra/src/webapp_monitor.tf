ca05ba15fba7b2cd1bc9f66d47fca1edd24148a5
Gianmaria Scorza <gianmaria@scorza.dev>
2024-05-27 15:34:06 +0200

    cleanUp pt2

--- infra/src/webapp_monitor.tf
+++ infra/src/webapp_monitor.tf
@@ -1,8 +1,3 @@
-data "azurerm_monitor_action_group" "iopquarantineerror-action-group" {
-  resource_group_name = var.io_common.resource_group_name
-  name                = "iopquarantineerror"
-}
-
 resource "azurerm_monitor_metric_alert" "webapp_functions_app_health_check" {
   name                = "${module.webapp_functions_app.name}-health-check-failed"
   resource_group_name = azurerm_resource_group.rg.name
@@ -23,34 +18,5 @@ resource "azurerm_monitor_metric_alert" "webapp_functions_app_health_check" {
 
   action {
     action_group_id = data.azurerm_monitor_action_group.error_action_group.id
-  }
-}
-
-resource "azurerm_monitor_scheduled_query_rules_alert" "poison-queue-alert" {
-  name                = "[iopservicescmsst] Messages In Poison Queue"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  severity            = 1
-  action {
-    action_group = [data.azurerm_monitor_action_group.iopquarantineerror-action-group.id]
-  }
-
-  data_source_id = module.storage_account.id
-
-  description = "Storage Account [iopservicescmsst] poison queue contains messages, this happen on multiple failures in message process by the QueueTrigger Azure Functions"
-
-  query = <<-QUERY
-    StorageQueueLogs  
-    | where OperationName contains "PutMessage" 
-    | where Uri contains "-poison"
-    | count
-  QUERY
-
-  frequency   = "5" // minutes
-  time_window = "5" // minutes
-
-  trigger {
-    operator  = "GreaterThan"
-    threshold = 0
   }
 }
