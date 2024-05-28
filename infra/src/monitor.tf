3a23d8498ee7dd00de1875483d2e9086cb2a637e
Rodolfo Viti <rodolfo.viti@pagopa.it>
2024-05-28 12:38:04 +0200

    use azurerm_monitor_scheduled_query_rules_alert_v2

--- infra/src/monitor.tf
+++ infra/src/monitor.tf
@@ -8,32 +8,36 @@ resource "azurerm_monitor_diagnostic_setting" "queue_diagnostic_setting" {
   }
 }
 
-resource "azurerm_monitor_scheduled_query_rules_alert" "poison-queue-alert" {
+resource "azurerm_monitor_scheduled_query_rules_alert_v2" "poison-queue-alert" {
+  enabled             = true
   name                = "[${upper(local.application_basename)}] Messages In Poison Queue"
-  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  scopes              = [module.storage_account.id]
+  description         = "Storage Account [${module.storage_account.name}] poison queue contains messages, this happen on multiple failures in message process by the QueueTrigger Azure Functions"
   severity            = 0
-  action {
-    action_group = [data.azurerm_monitor_action_group.iopquarantineerror_action_group.id]
+
+  // check once every 5 minutes(evaluation_frequency)
+  // on the last 10 minutes of data(window_duration)
+  evaluation_frequency = "PT5M"
+  window_duration      = "PT10M"
+
+  mute_actions_after_alert_duration = "PT5M"
+
+  criteria {
+    query                   = <<-QUERY
+      StorageQueueLogs  
+      | where OperationName contains "PutMessage" 
+      | where Uri contains "-poison"
+      | distinct Uri
+      | project QueueName = split(Uri, "/")[3]
+    QUERY
+    operator                = "GreaterThan"
+    time_aggregation_method = "Count"
+    threshold               = 0
   }
 
-  data_source_id = module.storage_account.id
-
-  description = "Storage Account [${module.storage_account.name}] poison queue contains messages, this happen on multiple failures in message process by the QueueTrigger Azure Functions"
-
-  query = <<-QUERY
-    StorageQueueLogs  
-    | where OperationName contains "PutMessage" 
-    | where Uri contains "-poison"
-    | distinct Uri
-    | project QueueName = split(Uri, "/", 3)
-  QUERY
-
-  frequency   = "5" // minutes
-  time_window = "5" // minutes
-
-  trigger {
-    operator  = "GreaterThan"
-    threshold = 0
+  action {
+    action_groups = [data.azurerm_monitor_action_group.iopquarantineerror_action_group.id]
   }
 }
