13a5e95f697216dccc3a4e3049533ec5dff07276
Rodolfo Viti <rodolfo.viti@pagopa.it>
2024-06-03 17:06:26 +0200

    fix app_settings

--- infra/_modules/function_app/locals.tf
+++ infra/_modules/function_app/locals.tf
@@ -3,17 +3,7 @@ locals {
     snet_cidr     = "10.20.5.0/24" # Picked as the first available non-allocated CIDR from the io-p-itn-common-vnet-01
     cosmosdb_name = "app-backend"
     app_settings = {
-      FUNCTIONS_WORKER_RUNTIME       = "node"
-      FUNCTIONS_WORKER_PROCESS_COUNT = 4
-      NODE_ENV                       = "production"
-
-      // Keepalive fields are all optionals
-      FETCH_KEEPALIVE_ENABLED             = "true"
-      FETCH_KEEPALIVE_SOCKET_ACTIVE_TTL   = "110000"
-      FETCH_KEEPALIVE_MAX_SOCKETS         = "40"
-      FETCH_KEEPALIVE_MAX_FREE_SOCKETS    = "10"
-      FETCH_KEEPALIVE_FREE_SOCKET_TIMEOUT = "30000"
-      FETCH_KEEPALIVE_TIMEOUT             = "60000"
+      NODE_ENV = "production"
 
       FEATURED_ITEMS_CONTAINER_NAME      = "static-content"
       FEATURED_SERVICES_FILE_NAME        = "featured-services.json"
