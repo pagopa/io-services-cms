7d26a98370ca7c4f340a6f72ea342b256aaf0cab
Gianmaria Scorza <gianmaria@scorza.dev>
2024-06-04 15:29:23 +0200

    Add new Featured Functions which use the new Azure Blob Storage Client

--- apps/app-backend/src/main.ts
+++ apps/app-backend/src/main.ts
@@ -1,8 +1,11 @@
 import { app } from "@azure/functions";
-import { createBlobService } from "azure-storage";
+import { DefaultAzureCredential } from "@azure/identity";
+import { BlobServiceClient } from "@azure/storage-blob";
 import * as E from "fp-ts/Either";
 import { pipe } from "fp-ts/lib/function";
 import { getConfigOrError } from "./config";
+import { GetFeaturedInstitutionsFn } from "./functions/featured-institutions";
+import { GetFeaturedServicesFn } from "./functions/featured-services";
 import { GetServiceByIdFn } from "./functions/get-service-by-id";
 import { InfoFn } from "./functions/info";
 import { SearchInstitutionsFn } from "./functions/search-institutions";
@@ -11,8 +14,9 @@ import { Institution } from "./generated/definitions/internal/Institution";
 import { ServiceMinified } from "./generated/definitions/internal/ServiceMinified";
 import { makeAzureSearchClient } from "./utils/azure-search/client";
 import { buildServiceDetailsContainerDependency } from "./utils/cosmos-db/helper";
-import { GetFeaturedInstitutionsFn } from "./functions/featured-institutions";
-import { GetFeaturedServicesFn } from "./functions/featured-services";
+import { createBlobService } from "azure-storage";
+import { GetFeaturedInstitutionsNewFn } from "./functions/featured-institutions-new";
+import { GetFeaturedServicesNewFn } from "./functions/featured-services-new";
 
 const config = pipe(
   getConfigOrError(),
@@ -21,6 +25,12 @@ const config = pipe(
   })
 );
 // Handlers Depedencies
+//newOne
+const blobServiceClient = new BlobServiceClient(
+  `https://iopitnsvcappbest01.blob.core.windows.net`,
+  new DefaultAzureCredential()
+);
+
 const blobService = createBlobService(
   config.FEATURED_ITEMS_BLOB_CONNECTION_STRING
 );
@@ -66,6 +76,16 @@ app.http("GetFeaturedServices", {
   handler: GetFeaturedServices,
 });
 
+const GetFeaturedServicesNew = GetFeaturedServicesNewFn(config)({
+  blobServiceClient,
+});
+app.http("GetFeaturedServicesNew", {
+  methods: ["GET"],
+  route: "services/featurednew",
+  authLevel: "anonymous",
+  handler: GetFeaturedServicesNew,
+});
+
 const GetFeaturedInstitutions = GetFeaturedInstitutionsFn(config)({
   blobService,
 });
@@ -74,6 +94,16 @@ app.http("GetFeaturedInstitutions", {
   route: "institutions/featured",
   authLevel: "anonymous",
   handler: GetFeaturedInstitutions,
+});
+
+const GetFeaturedInstitutionsNew = GetFeaturedInstitutionsNewFn(config)({
+  blobServiceClient,
+});
+app.http("GetFeaturedInstitutionsNew", {
+  methods: ["GET"],
+  route: "institutions/featurednew",
+  authLevel: "anonymous",
+  handler: GetFeaturedInstitutionsNew,
 });
 
 const SearchInstitutions = SearchInstitutionsFn(config)({
