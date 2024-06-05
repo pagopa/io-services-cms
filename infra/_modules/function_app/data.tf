1f4ea4eaaa274b175ba4cb04572c52532730e658
Rodolfo Viti <62432865+rodoviti@users.noreply.github.com>
2024-06-05 12:04:46 +0200

    chore: add services be fn autoscalers and basic alerts (#885)

--- infra/_modules/function_app/data.tf
+++ infra/_modules/function_app/data.tf
@@ -7,3 +7,8 @@ data "azurerm_cosmosdb_account" "cosmos" {
   name                = "${var.prefix}-${var.env_short}-cosmos-services-cms"
   resource_group_name = "${var.prefix}-${var.env_short}-services-cms-rg"
 }
+
+data "azurerm_monitor_action_group" "error_action_group" {
+  name                = "${var.prefix}${var.env_short}error"
+  resource_group_name = "${var.prefix}-${var.env_short}-rg-common"
+}
