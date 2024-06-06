d9d5f54bc54cfce96f4c0fa570ef3e99a0952dc2
Rodolfo Viti <62432865+rodoviti@users.noreply.github.com>
2024-06-06 16:40:24 +0200

    Patch indexes sortable field (#895)

--- infra/_modules/ai_search/organizations.tf
+++ infra/_modules/ai_search/organizations.tf
@@ -201,6 +201,186 @@ resource "restapi_object" "organizations_indexer" {
   depends_on   = [azurerm_role_assignment.search_to_cosmos_account_reader, azurerm_cosmosdb_sql_role_assignment.search_to_cosmos_data_reader, restapi_object.organizations_datasource]
 }
 
+resource "restapi_object" "organizations_index_02" {
+  path         = "/indexes"
+  query_string = "api-version=2023-11-01"
+  data = jsonencode({
+    name                  = "idx-organization-02"
+    defaultScoringProfile = null
+    fields = [
+      {
+        name                = "id"
+        type                = "Edm.String"
+        searchable          = false
+        filterable          = true
+        retrievable         = true
+        sortable            = false
+        facetable           = false
+        key                 = true
+        indexAnalyzer       = null
+        searchAnalyzer      = null
+        dimensions          = null
+        vectorSearchProfile = null
+        synonymMaps         = []
+      },
+      {
+        name                = "name"
+        type                = "Edm.String"
+        searchable          = true
+        filterable          = false
+        retrievable         = true
+        sortable            = true
+        facetable           = false
+        key                 = false
+        analyzer            = "organizationCustomAnalyzer"
+        dimensions          = null
+        vectorSearchProfile = null
+        synonymMaps         = []
+      },
+      {
+        name                = "scope"
+        type                = "Edm.String"
+        searchable          = false
+        filterable          = true
+        retrievable         = false
+        sortable            = false
+        facetable           = false
+        key                 = false
+        dimensions          = null
+        vectorSearchProfile = null
+        synonymMaps         = []
+      },
+      {
+        name                = "fiscal_code"
+        type                = "Edm.String"
+        searchable          = false
+        filterable          = false
+        retrievable         = true
+        sortable            = false
+        facetable           = false
+        key                 = false
+        dimensions          = null
+        vectorSearchProfile = null
+        synonymMaps         = []
+      }
+    ]
+    scoringProfiles = [
+      {
+        name                = "BoostScope"
+        functionAggregation = "sum"
+        text                = null
+        functions = [
+          {
+            fieldName     = "scope"
+            interpolation = "constant"
+            type          = "tag"
+            boost         = 3
+            freshness     = null
+            magnitude     = null
+            distance      = null
+            tag = {
+              tagsParameter = "boostScope"
+            }
+          }
+        ]
+      }
+    ]
+    corsOptions = null
+    suggesters  = []
+    analyzers = [
+      {
+        "@odata.type" = "#Microsoft.Azure.Search.CustomAnalyzer"
+        name          = "organizationCustomAnalyzer"
+        tokenizer     = "customNGramTokenizer"
+        tokenFilters = [
+          "italianStopWord",
+          "asciifolding",
+          "lowercase"
+        ]
+        charFilters = [
+          "removeWhitespace"
+        ]
+      }
+    ]
+    tokenizers = [
+      {
+        "@odata.type" = "#Microsoft.Azure.Search.NGramTokenizer"
+        name          = "customNGramTokenizer"
+        minGram       = 3
+        maxGram       = 5
+        tokenChars    = []
+      }
+    ]
+    tokenFilters = [
+      {
+        "@odata.type"  = "#Microsoft.Azure.Search.StopwordsTokenFilter"
+        name           = "italianStopWord"
+        stopwords      = []
+        stopwordsList  = "italian"
+        ignoreCase     = false
+        removeTrailing = true
+      }
+    ]
+    charFilters = [
+      {
+        "@odata.type" = "#Microsoft.Azure.Search.MappingCharFilter"
+        name          = "removeWhitespace"
+        mappings = [
+          "\\u0020=>"
+        ]
+      }
+    ]
+    encryptionKey = null
+    similarity = {
+      "@odata.type" = "#Microsoft.Azure.Search.BM25Similarity"
+      k1            = null
+      b             = null
+    }
+    semantic     = null
+    vectorSearch = null
+  })
+  id_attribute = "name" # The ID field on the response
+  depends_on   = [azurerm_role_assignment.search_to_cosmos_account_reader, azurerm_cosmosdb_sql_role_assignment.search_to_cosmos_data_reader]
+}
+
+resource "restapi_object" "organizations_indexer_02" {
+  path         = "/indexers"
+  query_string = "api-version=2023-11-01"
+  data = jsonencode(
+    {
+      name            = "idxr-organization-02"
+      dataSourceName  = restapi_object.organizations_datasource.id
+      targetIndexName = restapi_object.organizations_index_02.id
+      description     = null
+      skillsetName    = null
+      disabled        = null
+      schedule        = { interval = local.indexers_scheduling_interval.organizations }
+      parameters = {
+        batchSize              = null
+        maxFailedItems         = null
+        maxFailedItemsPerBatch = null
+        base64EncodeKeys       = null
+        configuration          = {}
+      }
+      fieldMappings = [
+        {
+          sourceFieldName = "fiscal_code"
+          targetFieldName = "fiscal_code"
+          mappingFunction = null
+        },
+        {
+          sourceFieldName = "fiscal_code"
+          targetFieldName = "id"
+          mappingFunction = null
+        }
+      ]
+      outputFieldMappings = []
+      encryptionKey       = null
+  })
+  id_attribute = "name" # The ID field on the response
+  depends_on   = [azurerm_role_assignment.search_to_cosmos_account_reader, azurerm_cosmosdb_sql_role_assignment.search_to_cosmos_data_reader, restapi_object.organizations_datasource]
+}
+
 resource "restapi_object" "organizations_alias" {
   path         = "/aliases"
   query_string = "api-version=2024-03-01-Preview"
