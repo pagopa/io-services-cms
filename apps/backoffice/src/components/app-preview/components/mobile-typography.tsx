40d7bd65c9b33f1f774982e86022ee6df3b000af
Giuseppe Di Pinto <giuseppeantonio.dipinto@pagopa.it>
2024-07-08 17:19:08 +0200

    fix

--- apps/backoffice/src/components/app-preview/components/mobile-typography.tsx
+++ apps/backoffice/src/components/app-preview/components/mobile-typography.tsx
@@ -1,13 +1,7 @@
 import { Typography, TypographyProps } from "@mui/material";
-import { styled } from "@mui/system";
 
 interface MobileTypographyProps extends TypographyProps {}
 
-const MobileTypographyRoot = styled(Typography)(({ theme }) => ({
-  fontFamily: "TitilliumSansPro, sans-serif"
-}));
-
-/** Render IO App font **Readex Pro** */
 export const MobileTypography = (props: MobileTypographyProps) => {
-  return <MobileTypographyRoot {...props} />;
+  return <Typography {...props} fontFamily="TitilliumSansPro, sans-serif" />;
 };
