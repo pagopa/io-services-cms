2de54ae5f8b2dc374e1d2c90130d9adb0190c498
Carlo Bussi <carlo.bussi@itpartneritalia.com>
2024-07-16 09:22:02 +0200

    Header tests

---
+++ apps/backoffice/src/components/headers/__tests__/page-html-head-title..test.tsx
@@ -0,0 +1,37 @@
+import { cleanup, render, screen } from "@testing-library/react";
+import React from "react";
+import { afterEach, describe, expect, it, vi } from "vitest";
+import { PageHtmlHeadTitle } from "../page-html-head-title";
+
+let section = "";
+
+vi.mock("next/head", () => {
+  return {
+    __esModule: true,
+    default: ({ children }: { children: Array<React.ReactElement> }) => {
+      return <>{children}</>;
+    }
+  };
+});
+
+const getHeadTitle = () => {
+  return <PageHtmlHeadTitle section={section} />;
+};
+
+const resetProps = () => {};
+
+// needed to clean document (react dom)
+afterEach(() => {
+  cleanup();
+  resetProps();
+});
+
+describe("[HeadTitle] Component", () => {
+  it("should render the component", async () => {
+    render(getHeadTitle());
+
+    const title = screen.getByText("app.title |");
+
+    expect(title).toBeDefined();
+  });
+});
