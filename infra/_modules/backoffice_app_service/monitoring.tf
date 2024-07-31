dd98fa48ebe2a428c47019dc7ab9b93d460f1f28
Gianmaria Scorza <gianmaria@scorza.dev>
2024-07-31 16:38:32 +0200

    add traditional autoscale

--- infra/_modules/backoffice_app_service/monitoring.tf
+++ infra/_modules/backoffice_app_service/monitoring.tf
@@ -1,37 +1,148 @@
-module "backoffice_app_service_autoscaler" {
-  source = "github.com/pagopa/dx//infra/modules/azure_app_service_plan_autoscaler?ref=main"
+###############
+# Autoscalers #
+###############
 
-  resource_group_name = var.resource_group_name
-  target_service = {
-    app_service_name = module.backoffice_app_service.app_service.app_service.name
-  }
+resource "azurerm_monitor_autoscale_setting" "backoffice_app_service" {
+  name                = format("%s-autoscale", module.backoffice_app_service.app_service.app_service.name)
+  resource_group_name = module.backoffice_app_service.app_service.resource_group_name
+  location            = var.location
+  target_resource_id  = module.backoffice_app_service.app_service.plan.id
 
-  scheduler = {
-    normal_load = {
+  profile {
+    name = "default"
+
+    capacity {
       default = local.backoffice.autoscale_settings.default
       minimum = local.backoffice.autoscale_settings.min
+      maximum = local.backoffice.autoscale_settings.max
     }
-    maximum = local.backoffice.autoscale_settings.max
-  }
 
-  scale_metrics = {
-    requests = {
-      upper_threshold = 3000
-      lower_threshold = 2000
-      increase_by     = 2
-      decrease_by     = 1
-    },
-    cpu = {
-      upper_threshold = 60
-      lower_threshold = 30
-      increase_by     = 2
-      decrease_by     = 1
-    },
-    memory = {
-      upper_threshold = 80
-      lower_threshold = 30
-      increase_by     = 2
-      decrease_by     = 1
+    rule {
+      metric_trigger {
+        metric_name              = "Requests"
+        metric_resource_id       = module.backoffice_app_service.app_service.app_service.id
+        metric_namespace         = "microsoft.web/sites"
+        time_grain               = "PT1M"
+        statistic                = "Average"
+        time_window              = "PT1M"
+        time_aggregation         = "Average"
+        operator                 = "GreaterThan"
+        threshold                = 3000
+        divide_by_instance_count = false
+      }
+
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "2"
+        cooldown  = "PT1M"
+      }
+    }
+
+    rule {
+      metric_trigger {
+        metric_name              = "CpuPercentage"
+        metric_resource_id       = module.backoffice_app_service.app_service.plan.id
+        metric_namespace         = "microsoft.web/serverfarms"
+        time_grain               = "PT1M"
+        statistic                = "Average"
+        time_window              = "PT5M"
+        time_aggregation         = "Average"
+        operator                 = "GreaterThan"
+        threshold                = 60
+        divide_by_instance_count = false
+      }
+
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "2"
+        cooldown  = "PT5M"
+      }
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "MemoryPercentage"
+        metric_resource_id = module.backoffice_app_service.app_service.plan.id
+        metric_namespace   = "microsoft.web/serverfarms"
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 80
+      }
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "2"
+        cooldown  = "PT5M"
+      }
+    }
+
+    rule {
+      metric_trigger {
+        metric_name              = "Requests"
+        metric_resource_id       = module.backoffice_app_service.app_service.app_service.id
+        metric_namespace         = "microsoft.web/sites"
+        time_grain               = "PT1M"
+        statistic                = "Average"
+        time_window              = "PT7M"
+        time_aggregation         = "Average"
+        operator                 = "LessThan"
+        threshold                = 2000
+        divide_by_instance_count = false
+      }
+
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT5M"
+      }
+    }
+
+    rule {
+      metric_trigger {
+        metric_name              = "CpuPercentage"
+        metric_resource_id       = module.backoffice_app_service.app_service.plan.id
+        metric_namespace         = "microsoft.web/serverfarms"
+        time_grain               = "PT1M"
+        statistic                = "Average"
+        time_window              = "PT7M"
+        time_aggregation         = "Average"
+        operator                 = "LessThan"
+        threshold                = 30
+        divide_by_instance_count = false
+      }
+
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT5M"
+      }
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "MemoryPercentage"
+        metric_resource_id = module.backoffice_app_service.app_service.plan.id
+        metric_namespace   = "microsoft.web/serverfarms"
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT7M"
+        time_aggregation   = "Average"
+        operator           = "LessThan"
+        threshold          = 30
+      }
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT5M"
+      }
     }
   }
 
