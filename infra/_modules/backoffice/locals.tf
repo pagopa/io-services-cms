aa6539cd0131ef910d37dd464c86a905ca3d12d2
Gianmaria Scorza <gianmaria@scorza.dev>
2024-08-01 10:06:37 +0200

    add missing configs

--- infra/_modules/backoffice/locals.tf
+++ infra/_modules/backoffice/locals.tf
@@ -18,6 +18,7 @@ locals {
       AZURE_APIM_RESOURCE_GROUP = data.azurerm_api_management.apim_v2.resource_group_name
       AZURE_APIM_PRODUCT_NAME   = data.azurerm_api_management_product.apim_v2_product_services.product_id
       APIM_USER_GROUPS          = "apimessagewrite,apiinforead,apimessageread,apilimitedprofileread,apiservicewrite"
+      API_APIM_MOCKING          = false
 
       # Logs
       AI_SDK_CONNECTION_STRING = data.azurerm_application_insights.ai_common.connection_string
@@ -26,9 +27,10 @@ locals {
       NEXTAUTH_SECRET = azurerm_key_vault_secret.bo_auth_session_secret.value
 
       # Legacy source data
-      LEGACY_COSMOSDB_NAME = "db"
-      LEGACY_COSMOSDB_URI  = data.azurerm_cosmosdb_account.cosmos_legacy.endpoint
-      LEGACY_COSMOSDB_KEY  = data.azurerm_key_vault_secret.legacy_cosmosdb_key.value
+      LEGACY_COSMOSDB_NAME    = "db"
+      LEGACY_COSMOSDB_URI     = data.azurerm_cosmosdb_account.cosmos_legacy.endpoint
+      LEGACY_COSMOSDB_KEY     = data.azurerm_key_vault_secret.legacy_cosmosdb_key.value
+      LEGACY_COSMOSDB_MOCKING = false
 
       # Services CMS source data
       COSMOSDB_NAME                           = "db-services-cms"
@@ -42,16 +44,19 @@ locals {
 
       API_SERVICES_CMS_URL                      = "https://${var.cms_fn_default_hostname}"
       API_SERVICES_CMS_BASE_PATH                = "/api/v1"
+      API_SERVICES_CMS_MOCKING                  = false
       API_SERIVCES_CMS_TOPICS_CACHE_TTL_MINUTES = "60"
 
       # Selfcare
       SELFCARE_EXTERNAL_API_BASE_URL = "https://api.selfcare.pagopa.it/external/v2"
       SELFCARE_JWKS_PATH             = "/.well-known/jwks.json"
       SELFCARE_API_KEY               = data.azurerm_key_vault_secret.selfcare_api_key.value
+      SELFCARE_API_MOCKING           = false
 
       # Subscriptions Migration
-      SUBSCRIPTION_MIGRATION_API_URL = "https://io-p-subsmigrations-fn.azurewebsites.net/api/v1"
-      SUBSCRIPTION_MIGRATION_API_KEY = data.azurerm_key_vault_secret.subscription_migration_api_key.value
+      SUBSCRIPTION_MIGRATION_API_URL     = "https://io-p-subsmigrations-fn.azurewebsites.net/api/v1"
+      SUBSCRIPTION_MIGRATION_API_KEY     = data.azurerm_key_vault_secret.subscription_migration_api_key.value
+      SUBSCRIPTION_MIGRATION_API_MOCKING = false
 
 
       // Fetch keepalive
