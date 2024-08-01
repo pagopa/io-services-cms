cc896d84c86d5f3c05ad7f1415675304e75a5d5e
Gianmaria Scorza <gianmaria@scorza.dev>
2024-08-01 08:58:47 +0200

    reference kv secret bo_auth_session_secret using data

--- infra/_modules/backoffice_app_service/data.tf
+++ infra/_modules/backoffice_app_service/data.tf
@@ -67,19 +67,7 @@ data "azurerm_key_vault_secret" "subscription_migration_api_key" {
   key_vault_id = data.azurerm_key_vault.backoffice_key_vault.id
 }
 
-resource "random_password" "bo_auth_session_secret" {
-  for_each    = toset([var.bo_auth_session_secret_rotation_id])
-  length      = 16
-  min_lower   = 3
-  min_numeric = 3
-  min_special = 3
-  min_upper   = 3
-}
-
-resource "azurerm_key_vault_secret" "bo_auth_session_secret" {
-  name            = "bo-auth-session-secret"
-  key_vault_id    = data.azurerm_key_vault.backoffice_key_vault.id
-  value           = random_password.bo_auth_session_secret[var.bo_auth_session_secret_rotation_id].result
-  content_type    = "string"
-  expiration_date = "2028-09-27T07:41:36Z"
+data "azurerm_key_vault_secret" "bo_auth_session_secret" {
+  name         = "bo-auth-session-secret"
+  key_vault_id = data.azurerm_key_vault.backoffice_key_vault.id
 }
