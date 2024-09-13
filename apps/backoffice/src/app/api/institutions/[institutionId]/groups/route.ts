077fac42cfe7044a6f7975eddba03252f3538906
Stefano.D'aversa <stefano.daversa@pagopa.it>
2024-09-13 11:13:22 +0200

    clean up

--- apps/backoffice/src/app/api/institutions/[institutionId]/groups/route.ts
+++ apps/backoffice/src/app/api/institutions/[institutionId]/groups/route.ts
@@ -15,40 +15,39 @@ export const GET = withJWTAuthHandler(
     request: NextRequest,
     {
       backofficeUser,
-      params,
-    }: { backofficeUser: BackOfficeUser; params: { institutionId: string } },
+      params
+    }: { backofficeUser: BackOfficeUser; params: { institutionId: string } }
   ) => {
-    if (backofficeUser.institution.role === "ADMIN") {
-      try {
-        const limit = parseStringToNumberFunction(
-          request.nextUrl.searchParams.get("size"),
-        );
-        const offset = parseStringToNumberFunction(
-          request.nextUrl.searchParams.get("number"),
-        );
-        const institutionResponse = await retrieveUserGroups(
-          params.institutionId,
-          limit,
-          offset,
-        );
-        return sanitizedNextResponseJson(institutionResponse);
-      } catch (error) {
-        console.error(
-          `An Error has occurred while searching groups for institutionId: ${params.institutionId}, caused by: `,
-          error,
-        );
-
-        return handleInternalErrorResponse("InstitutionGroupsError", error);
-      }
-    } else {
+    if (backofficeUser.institution.role !== "ADMIN") {
       return NextResponse.json(
         {
           detail: "Role not authorized",
           status: 401,
-          title: "User Unauthorized",
+          title: "User Unauthorized"
         },
-        { status: 401 },
+        { status: 401 }
       );
     }
-  },
+    try {
+      const limit = parseStringToNumberFunction(
+        request.nextUrl.searchParams.get("size")
+      );
+      const offset = parseStringToNumberFunction(
+        request.nextUrl.searchParams.get("number")
+      );
+      const institutionResponse = await retrieveUserGroups(
+        params.institutionId,
+        limit,
+        offset
+      );
+      return sanitizedNextResponseJson(institutionResponse);
+    } catch (error) {
+      console.error(
+        `An Error has occurred while searching groups for institutionId: ${params.institutionId}, caused by: `,
+        error
+      );
+
+      return handleInternalErrorResponse("InstitutionGroupsError", error);
+    }
+  }
 );
