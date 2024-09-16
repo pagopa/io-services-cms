f6cc1f0df7bee1fda32f5dc54d8621b8aa8c5413
Stefano.D'aversa <stefano.daversa@pagopa.it>
2024-09-16 10:02:04 +0200

    fix selfcare role for checking admin function and unit test

--- apps/backoffice/src/lib/be/authz.ts
+++ apps/backoffice/src/lib/be/authz.ts
@@ -1,7 +1,9 @@
+import { SelfcareRoles } from "@/types/auth";
+
 import { BackOfficeUser } from "../../../types/next-auth";
 
 export const isBackofficeUserAdmin = (user: BackOfficeUser): boolean =>
-  user.institution.role === "ADMIN";
+  user.institution.role === SelfcareRoles.admin;
 
 export const isInstitutionIdSameAsCaller = (
   user: BackOfficeUser,
