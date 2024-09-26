f7eaf79a65239b9c5cecd264ee030da9f2509e32
Mario Mupo <mario.mupo@pagopa.it>
2024-09-26 16:08:02 +0200

    fiz: naming convention

--- infra/src/network.tf
+++ infra/src/network.tf
@@ -56,19 +56,19 @@ module "postgres_flexible_snet" {
 
 data "azurerm_subnet" "private_endpoints_subnet_itn" {
   name                 = "io-p-itn-pep-snet-01"
-  virtual_network_name = "io-p-itn-common-vnet-01"
-  resource_group_name  = "io-p-itn-common-rg-01"
+  virtual_network_name = local.vnet_common_name_itn
+  resource_group_name  = local.vnet_common_resource_group_name_itn
 }
 
 
 resource "azurerm_private_endpoint" "cosmos_db" {
-  name                = "${local.project}-itn-cosmos-services-cms"
+  name                = "${local.project_itn}-svc-cosno-pep-01"
   location            = "italynorth"
   resource_group_name = azurerm_resource_group.rg.name
   subnet_id           = data.azurerm_subnet.private_endpoints_subnet_itn.id
 
   private_service_connection {
-    name                           = "${local.project}-itn-cosmos-services-cms-private-endpoint"
+    name                           = "${local.project_itn}-svc-cosno-pep-01"
     private_connection_resource_id = module.cosmosdb_account.id
     is_manual_connection           = false
     subresource_names              = ["Sql"]
