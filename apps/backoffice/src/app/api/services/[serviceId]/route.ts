2033d21a41f0459fc30e2301d4fa442a3530f911
Rodolfo Viti <rodolfo.viti@pagopa.it>
2024-10-11 16:55:38 +0200

    manage group authz on update service handler

--- apps/backoffice/src/app/api/services/[serviceId]/route.ts
+++ apps/backoffice/src/app/api/services/[serviceId]/route.ts
@@ -1,7 +1,16 @@
-import { HTTP_STATUS_BAD_REQUEST } from "@/config/constants";
+import { ServicePayload } from "@/generated/api/ServicePayload";
+import { isAdmin } from "@/lib/be/authz";
+import {
+  handleBadRequestErrorResponse,
+  handleForbiddenErrorResponse,
+  handleInternalErrorResponse,
+  handlerErrorLog,
+} from "@/lib/be/errors";
+import { groupExists } from "@/lib/be/institutions/business";
+import { parseBody } from "@/lib/be/req-res-utils";
 import { forwardIoServicesCmsRequest } from "@/lib/be/services/business";
 import { withJWTAuthHandler } from "@/lib/be/wrappers";
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
 import { BackOfficeUser } from "../../../../../types/next-auth";
 
@@ -34,27 +43,51 @@ export const PUT = withJWTAuthHandler(
       params,
     }: { backofficeUser: BackOfficeUser; params: { serviceId: string } },
   ) => {
-    let jsonBody;
+    let servicePayload;
     try {
-      jsonBody = await nextRequest.json();
-    } catch (_) {
-      return NextResponse.json(
-        {
-          detail: "invalid JSON body",
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          status: HTTP_STATUS_BAD_REQUEST as any,
-          title: "validationError",
-        },
-        { status: HTTP_STATUS_BAD_REQUEST },
+      servicePayload = await parseBody(nextRequest, ServicePayload);
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      return handleBadRequestErrorResponse(error.message);
+    }
+    if (isAdmin(backofficeUser)) {
+      try {
+        if (
+          servicePayload.metadata.group_id &&
+          !(await groupExists(
+            backofficeUser.institution.id,
+            servicePayload.metadata.group_id,
+          ))
+        ) {
+          return handleBadRequestErrorResponse(
+            "Provided group_id does not exists",
+          );
+        }
+      } catch (error) {
+        handlerErrorLog(
+          `An Error has occurred while checking group existance: institutionId=${backofficeUser.institution.id} , groupId=${servicePayload.metadata.group_id}`,
+          error,
+        );
+        return handleInternalErrorResponse(
+          "CheckInstitutionGroupsError",
+          error,
+        );
+      }
+    } else {
+      return handleForbiddenErrorResponse(
+        "Cannot set service group relationship",
       );
     }
-    jsonBody.organization = {
-      fiscal_code: backofficeUser.institution.fiscalCode,
-      name: backofficeUser.institution.name,
-    };
+
     return forwardIoServicesCmsRequest("updateService", {
       backofficeUser,
-      jsonBody,
+      jsonBody: {
+        ...servicePayload,
+        organization: {
+          fiscal_code: backofficeUser.institution.fiscalCode,
+          name: backofficeUser.institution.name,
+        },
+      },
       nextRequest,
       pathParams: params,
     });
