c01c7c128052a830989711214b5a2089e13b801c
Gianmaria Scorza <gianmaria@scorza.dev>
2024-10-14 13:00:31 +0200

    revert managed identity

--- infra/_modules/eventhub/main.tf
+++ infra/_modules/eventhub/main.tf
@@ -26,16 +26,3 @@ module "eventhub" {
 
   tags = var.tags
 }
-
-module "function_app" {
-  source       = "github.com/pagopa/dx//infra/modules/azure_role_assignments?ref=main"
-  principal_id = var.cms_fn_principal_id
-
-  event_hub = [
-    {
-      namespace_name      = module.eventhub.name
-      resource_group_name = var.resource_group_name
-      role                = "writer"
-    }
-  ]
-}
