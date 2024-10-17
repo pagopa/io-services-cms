0e81f3444e998a73108c23c20d589fcf9c5aa434
Gianmaria Scorza <gianmaria@scorza.dev>
2024-10-17 10:36:12 +0200

    clean pt3

--- apps/io-services-cms-webapp/src/utils/ingestion/formatter/service-publication-avro-formatter.ts
+++ apps/io-services-cms-webapp/src/utils/ingestion/formatter/service-publication-avro-formatter.ts
@@ -14,63 +14,54 @@ import { servicePublication as avroServicePublication } from "../../../generated
 
 export const buildAvroServicePublicationObject = (
   servicePublicationCosmosRecord: ServicePublication.CosmosResource,
-): Omit<avroServicePublication, "schema" | "subject"> =>
-  // eslint-disable-next-line no-console, no-underscore-dangle
-  ({
-    data: {
-      authorized_cidrs: servicePublicationCosmosRecord.data.authorized_cidrs,
-
-      authorized_recipients:
-        servicePublicationCosmosRecord.data.authorized_recipients,
-
-      description: servicePublicationCosmosRecord.data.description,
-
-      max_allowed_payment_amount:
-        servicePublicationCosmosRecord.data.max_allowed_payment_amount,
-      metadata: {
-        address: servicePublicationCosmosRecord.data.metadata.address,
-        app_android: servicePublicationCosmosRecord.data.metadata.app_android,
-        app_ios: servicePublicationCosmosRecord.data.metadata.app_ios,
-        category: toAvroCategory(
-          servicePublicationCosmosRecord.data.metadata.category,
-        ),
-        cta: servicePublicationCosmosRecord.data.metadata.cta,
-        custom_special_flow:
-          servicePublicationCosmosRecord.data.metadata.custom_special_flow,
-        email: servicePublicationCosmosRecord.data.metadata.email,
-        pec: servicePublicationCosmosRecord.data.metadata.pec,
-        phone: servicePublicationCosmosRecord.data.metadata.phone,
-        privacy_url: servicePublicationCosmosRecord.data.metadata.privacy_url,
-        scope: toAvroScope(servicePublicationCosmosRecord.data.metadata.scope),
-        support_url: servicePublicationCosmosRecord.data.metadata.support_url,
-        token_name: servicePublicationCosmosRecord.data.metadata.token_name,
-        topic_id: servicePublicationCosmosRecord.data.metadata.topic_id,
-        tos_url: servicePublicationCosmosRecord.data.metadata.tos_url,
-        web_url: servicePublicationCosmosRecord.data.metadata.web_url,
-      },
-
-      name: servicePublicationCosmosRecord.data.name,
-
-      organization: {
-        department_name:
-          servicePublicationCosmosRecord.data.organization.department_name,
-        fiscal_code:
-          servicePublicationCosmosRecord.data.organization.fiscal_code,
-        name: servicePublicationCosmosRecord.data.organization.name,
-      },
-
-      require_secure_channel:
-        servicePublicationCosmosRecord.data.require_secure_channel,
+): Omit<avroServicePublication, "schema" | "subject"> => ({
+  data: {
+    authorized_cidrs: servicePublicationCosmosRecord.data.authorized_cidrs,
+    authorized_recipients:
+      servicePublicationCosmosRecord.data.authorized_recipients,
+    description: servicePublicationCosmosRecord.data.description,
+    max_allowed_payment_amount:
+      servicePublicationCosmosRecord.data.max_allowed_payment_amount,
+    metadata: {
+      address: servicePublicationCosmosRecord.data.metadata.address,
+      app_android: servicePublicationCosmosRecord.data.metadata.app_android,
+      app_ios: servicePublicationCosmosRecord.data.metadata.app_ios,
+      category: toAvroCategory(
+        servicePublicationCosmosRecord.data.metadata.category,
+      ),
+      cta: servicePublicationCosmosRecord.data.metadata.cta,
+      custom_special_flow:
+        servicePublicationCosmosRecord.data.metadata.custom_special_flow,
+      email: servicePublicationCosmosRecord.data.metadata.email,
+      pec: servicePublicationCosmosRecord.data.metadata.pec,
+      phone: servicePublicationCosmosRecord.data.metadata.phone,
+      privacy_url: servicePublicationCosmosRecord.data.metadata.privacy_url,
+      scope: toAvroScope(servicePublicationCosmosRecord.data.metadata.scope),
+      support_url: servicePublicationCosmosRecord.data.metadata.support_url,
+      token_name: servicePublicationCosmosRecord.data.metadata.token_name,
+      topic_id: servicePublicationCosmosRecord.data.metadata.topic_id,
+      tos_url: servicePublicationCosmosRecord.data.metadata.tos_url,
+      web_url: servicePublicationCosmosRecord.data.metadata.web_url,
     },
-    fsm: {
-      state: toAvroFsmState(servicePublicationCosmosRecord.fsm),
+    name: servicePublicationCosmosRecord.data.name,
+    organization: {
+      department_name:
+        servicePublicationCosmosRecord.data.organization.department_name,
+      fiscal_code: servicePublicationCosmosRecord.data.organization.fiscal_code,
+      name: servicePublicationCosmosRecord.data.organization.name,
     },
-    id: servicePublicationCosmosRecord.id,
-    modified_at:
-      servicePublicationCosmosRecord.modified_at ??
-      DateUtils.unixSecondsToMillis(servicePublicationCosmosRecord._ts),
-    version: cleanEtag(servicePublicationCosmosRecord._etag),
-  });
+    require_secure_channel:
+      servicePublicationCosmosRecord.data.require_secure_channel,
+  },
+  fsm: {
+    state: toAvroFsmState(servicePublicationCosmosRecord.fsm),
+  },
+  id: servicePublicationCosmosRecord.id,
+  modified_at:
+    servicePublicationCosmosRecord.modified_at ??
+    DateUtils.unixSecondsToMillis(servicePublicationCosmosRecord._ts),
+  version: cleanEtag(servicePublicationCosmosRecord._etag),
+});
 
 const cleanEtag = (etag: NonEmptyString): string => etag.replace(/"/g, "");
 
@@ -100,19 +91,6 @@ export const toAvroCategory = (
   }
   return PublicationCategoryEnum.STANDARD;
 };
-
-export const avroServicePublicationFormatterOld = (
-  item: ServicePublication.CosmosResource,
-): EventData => ({
-  body: avro.Type.forSchema(
-    avroServicePublication.schema as avro.Schema, // cast due to tsc can not proper recognize object as avro.Schema (eg. if you use const schemaServices: avro.Type = JSON.parse(JSON.stringify(services.schema())); it will loose the object type and it will work fine)
-  ).toBuffer(
-    Object.assign(
-      new avroServicePublication(),
-      buildAvroServicePublicationObject(item),
-    ),
-  ),
-});
 
 export const avroServicePublicationFormatter = (
   item: ServicePublication.CosmosResource,
