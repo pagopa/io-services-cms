3869095a88eaf83c34ad88482308b4a43e840702
Mario Mupo <mario.mupo@pagopa.it>
2024-11-07 16:02:42 +0100

    feat: added apim itn resources

--- infra/src/apim_itn_api.tf
+++ infra/src/apim_itn_api.tf
@@ -30,60 +30,60 @@ module "api_services_cms_itn" {
 
 # API operation policy
 
-# resource "azurerm_api_management_api_operation_policy" "create_service_policy_itn" {
-#   api_name            = "io-services-cms-api"
-#   api_management_name = data.azurerm_api_management.apim_itn.name
-#   resource_group_name = data.azurerm_api_management.apim_itn.resource_group_name
-#   operation_id        = "createService"
+resource "azurerm_api_management_api_operation_policy" "create_service_policy_itn" {
+  api_name            = "io-services-cms-api"
+  api_management_name = data.azurerm_api_management.apim_itn.name
+  resource_group_name = data.azurerm_api_management.apim_itn.resource_group_name
+  operation_id        = "createService"
 
-#   xml_content = file("./api/io_services_cms/v1/createservice_policy/policy.xml")
-# }
+  xml_content = file("./api/io_services_cms/v1/createservice_policy/policy.xml")
+}
 
-# resource "azurerm_api_management_api_operation_policy" "update_service_policy_itn" {
-#   api_name            = "io-services-cms-api"
-#   api_management_name = data.azurerm_api_management.apim_itn.name
-#   resource_group_name = data.azurerm_api_management.apim_itn.resource_group_name
-#   operation_id        = "updateService"
+resource "azurerm_api_management_api_operation_policy" "update_service_policy_itn" {
+  api_name            = "io-services-cms-api"
+  api_management_name = data.azurerm_api_management.apim_itn.name
+  resource_group_name = data.azurerm_api_management.apim_itn.resource_group_name
+  operation_id        = "updateService"
 
-#   xml_content = file("./api/io_services_cms/v1/updateService_policy/policy.xml")
-# }
+  xml_content = file("./api/io_services_cms/v1/updateService_policy/policy.xml")
+}
 
-# resource "azurerm_api_management_api_operation_policy" "get_service_topics_policy_itn" {
-#   api_name            = "io-services-cms-api"
-#   api_management_name = data.azurerm_api_management.apim_itn.name
-#   resource_group_name = data.azurerm_api_management.apim_itn.resource_group_name
-#   operation_id        = "getServiceTopics"
+resource "azurerm_api_management_api_operation_policy" "get_service_topics_policy_itn" {
+  api_name            = "io-services-cms-api"
+  api_management_name = data.azurerm_api_management.apim_itn.name
+  resource_group_name = data.azurerm_api_management.apim_itn.resource_group_name
+  operation_id        = "getServiceTopics"
 
-#   xml_content = file("./api/io_services_cms/v1/getservicetopics_policy/policy.xml")
-# }
-# resource "azurerm_api_management_logger" "cache_policy_app_insights" {
-#   name                = "cache-policy-appinsight-apimlogger"
-#   api_management_name = data.azurerm_api_management.apim_itn.name
-#   resource_group_name = data.azurerm_api_management.apim_itn.resource_group_name
+  xml_content = file("./api/io_services_cms/v1/getservicetopics_policy/policy.xml")
+}
+resource "azurerm_api_management_logger" "cache_policy_app_insights_itn" {
+  name                = "cache-policy-appinsight-apimlogger"
+  api_management_name = data.azurerm_api_management.apim_itn.name
+  resource_group_name = data.azurerm_api_management.apim_itn.resource_group_name
 
-#   application_insights {
-#     instrumentation_key = data.azurerm_application_insights.application_insights.instrumentation_key
-#   }
-# }
+  application_insights {
+    instrumentation_key = data.azurerm_application_insights.application_insights.instrumentation_key
+  }
+}
 
-# resource "azurerm_api_management_api_diagnostic" "services_cms_api_app_insights" {
-#   identifier               = "applicationinsights"
-#   resource_group_name      = data.azurerm_api_management.apim_itn.resource_group_name
-#   api_management_name      = data.azurerm_api_management.apim_itn.name
-#   api_name                 = module.api_services_cms_itn.name
-#   api_management_logger_id = azurerm_api_management_logger.cache_policy_app_insights.id
+resource "azurerm_api_management_api_diagnostic" "services_cms_api_app_insights_itn" {
+  identifier               = "applicationinsights"
+  resource_group_name      = data.azurerm_api_management.apim_itn.resource_group_name
+  api_management_name      = data.azurerm_api_management.apim_itn.name
+  api_name                 = module.api_services_cms_itn.name
+  api_management_logger_id = azurerm_api_management_logger.cache_policy_app_insights.id
 
-#   sampling_percentage       = 100.0
-#   always_log_errors         = true
-#   log_client_ip             = false
-#   verbosity                 = "verbose"
-#   http_correlation_protocol = "W3C"
+  sampling_percentage       = 100.0
+  always_log_errors         = true
+  log_client_ip             = false
+  verbosity                 = "verbose"
+  http_correlation_protocol = "W3C"
 
-#   frontend_response {
-#     body_bytes = 32
-#   }
+  frontend_response {
+    body_bytes = 32
+  }
 
-#   backend_response {
-#     body_bytes = 32
-#   }
-# }
+  backend_response {
+    body_bytes = 32
+  }
+}
