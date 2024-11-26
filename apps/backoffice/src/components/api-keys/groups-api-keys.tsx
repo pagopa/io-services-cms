627c1164bcac591a15d17e0d12a51107a58676d1
Giuseppe Di Pinto <giuseppeantonio.dipinto@pagopa.it>
2024-11-26 18:34:31 +0100

    add main implementation

---
+++ apps/backoffice/src/components/api-keys/groups-api-keys.tsx
@@ -0,0 +1,375 @@
+/* eslint-disable max-lines-per-function */
+import { Cidr } from "@/generated/api/Cidr";
+import { Group } from "@/generated/api/Group";
+import { ManageKeyCIDRs } from "@/generated/api/ManageKeyCIDRs";
+import { Subscription } from "@/generated/api/Subscription";
+import { SubscriptionKeyTypeEnum } from "@/generated/api/SubscriptionKeyType";
+import { SubscriptionKeys } from "@/generated/api/SubscriptionKeys";
+import { SubscriptionPagination } from "@/generated/api/SubscriptionPagination";
+import { SubscriptionTypeEnum } from "@/generated/api/SubscriptionType";
+import useFetch, { client } from "@/hooks/use-fetch";
+import { Add, Delete, ExpandMore } from "@mui/icons-material";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Button,
+  Stack,
+} from "@mui/material";
+import * as E from "fp-ts/lib/Either";
+import { useSession } from "next-auth/react";
+import { useTranslation } from "next-i18next";
+import { useEffect, useState } from "react";
+
+import { AccessControl } from "../access-control";
+import { useDialog } from "../dialog-provider";
+import { ApiKeys } from "./api-keys";
+import { ApiKeysHeader } from "./api-keys-header";
+import { AuthorizedCidrs } from "./authorized-cidrs";
+
+export interface GroupsApiKeysProps {
+  /** Main component card description */
+  description?: string;
+  /** Selfcare groups */
+  groups?: Group[];
+  /** Event triggered when user click "Create group" on modal that warns of the non-existence of any SC group */
+  onCreateGroupClick: () => void;
+  /** Event triggered when user click "Generate API Key" */
+  onGenerateClick: () => void;
+  /** Main component card title */
+  title: string;
+}
+
+/** Single Group API Key interface */
+interface GroupApiKeyProps {
+  apiKey: RecordApiKeyValue;
+  onDelete: (event: React.MouseEvent, subscriptionId: string) => void;
+  onExpand: (expanded: boolean, subscriptionId: string) => void;
+  onRotateKey: (
+    keyType: SubscriptionKeyTypeEnum,
+    subscriptionId: string,
+  ) => void;
+  onUpdateCidrs: (cidrs: string[], subscriptionId: string) => void;
+  subscriptionId: string;
+}
+
+type GroupApiKeysRecordset = Record<string, RecordApiKeyValue>;
+
+interface RecordApiKeyValue {
+  cidrs: string[];
+  name: string;
+  primary_key: string;
+  secondary_key: string;
+}
+
+const convertArrayToRecordset = (
+  items: Subscription[],
+): GroupApiKeysRecordset =>
+  items.reduce((acc, item) => {
+    acc[item.id] = {
+      cidrs: [],
+      name: item.name,
+      primary_key: "",
+      secondary_key: "",
+    };
+    return acc;
+  }, {} as GroupApiKeysRecordset);
+
+const checkGroupsExists = async (institutionId: string) => {
+  try {
+    const maybeResponse = await client.checkInstitutionGroupsExistence({
+      institutionId,
+    });
+
+    if (E.isRight(maybeResponse)) {
+      return maybeResponse.right.status === 200;
+    }
+    return false;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+const borderStyle = "1px solid #E3E7EB";
+
+/** Group API Keys main component
+ *
+ * Used to show, copy, rotate `keys` _(primary/secondary)_ and edit optional `authorized cidrs`.
+ * */
+export const GroupsApiKeys = (props: GroupsApiKeysProps) => {
+  const { t } = useTranslation();
+  const { data: session } = useSession();
+  const showDialog = useDialog();
+
+  const { data: mspData, fetchData: mspFetchData } =
+    useFetch<SubscriptionPagination>();
+  const { data: keysData, fetchData: skFetchData } =
+    useFetch<SubscriptionKeys>();
+  const { data: cidrsData, fetchData: scFetchData } =
+    useFetch<ManageKeyCIDRs>();
+
+  const [apiKeys, setApiKeys] = useState<GroupApiKeysRecordset | undefined>(
+    undefined,
+  );
+
+  const handleOnGenerateClick = async () => {
+    const hasAtLeastOneGroup = await checkGroupsExists(
+      session?.user?.institution.id as string,
+    );
+
+    if (hasAtLeastOneGroup) {
+      props.onGenerateClick();
+    } else {
+      const noGroupAvailableCreateOne = await showDialog({
+        cancelButtonLabel: t("buttons.close"),
+        confirmButtonLabel: t("routes.keys.groups.noGroupModal.confirm"),
+        message: t("routes.keys.groups.noGroupModal.description"),
+        title: t("routes.keys.groups.title"),
+      });
+      if (noGroupAvailableCreateOne) {
+        props.onCreateGroupClick();
+      }
+    }
+  };
+
+  const updateApiKey = (key: string, newValues: Partial<RecordApiKeyValue>) => {
+    setApiKeys((prevApiKeys) => {
+      const currentRecord = prevApiKeys?.[key] || {
+        cidrs: [],
+        name: "",
+        primary_key: "",
+        secondary_key: "",
+      };
+
+      return {
+        ...prevApiKeys,
+        [key]: {
+          ...currentRecord,
+          ...newValues,
+        } as RecordApiKeyValue,
+      };
+    });
+  };
+
+  const handleOnExpandClick = async (
+    expanded: boolean,
+    subscriptionId: string,
+  ) => {
+    if (expanded && apiKeys && !apiKeys[subscriptionId].primary_key) {
+      const apiKeyObjValue: RecordApiKeyValue = {
+        cidrs: [],
+        name: apiKeys[subscriptionId].name,
+        primary_key: "",
+        secondary_key: "",
+      };
+      const maybeKeysResponse = await client.getManageSubscriptionKeys({
+        subscriptionId,
+      });
+      if (E.isRight(maybeKeysResponse)) {
+        const maybeKeys = SubscriptionKeys.decode(
+          maybeKeysResponse.right.value,
+        );
+        if (E.isRight(maybeKeys)) {
+          apiKeyObjValue.primary_key = maybeKeys.right.primary_key;
+          apiKeyObjValue.secondary_key = maybeKeys.right.secondary_key;
+        }
+      }
+
+      const maybeCidrsResponse =
+        await client.getManageSubscriptionAuthorizedCidrs({
+          subscriptionId,
+        });
+      if (E.isRight(maybeCidrsResponse)) {
+        const maybeCidrs = ManageKeyCIDRs.decode(
+          maybeCidrsResponse.right.value,
+        );
+        if (E.isRight(maybeCidrs)) {
+          apiKeyObjValue.cidrs = [...maybeCidrs.right.cidrs];
+        }
+      }
+
+      updateApiKey(subscriptionId, apiKeyObjValue);
+    }
+  };
+
+  const handleRotateKey = (
+    keyType: SubscriptionKeyTypeEnum,
+    subscriptionId: string,
+  ) => {
+    skFetchData(
+      "regenerateManageSubscriptionKey",
+      { keyType, subscriptionId },
+      SubscriptionKeys,
+      {
+        notify: "all",
+        referenceId: subscriptionId,
+      },
+    );
+  };
+
+  const handleUpdateCidrs = (cidrs: string[], subscriptionId: string) => {
+    scFetchData(
+      "updateManageSubscriptionAuthorizedCidrs",
+      {
+        body: { cidrs: Array.from(cidrs || []).filter(Cidr.is) },
+        subscriptionId,
+      },
+      ManageKeyCIDRs,
+      { notify: "all", referenceId: subscriptionId },
+    );
+  };
+
+  const handleDeleteClick = (event: React.MouseEvent) => {
+    event.stopPropagation();
+    console.log("TODO: delete");
+  };
+
+  // update GroupApiKey keys
+  useEffect(() => {
+    if (keysData && keysData._referenceId && apiKeys) {
+      const actualApiKey = apiKeys[keysData._referenceId];
+
+      actualApiKey.primary_key = keysData.primary_key;
+      actualApiKey.secondary_key = keysData.secondary_key;
+
+      updateApiKey(keysData._referenceId, actualApiKey);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [keysData]);
+
+  // update GroupApiKey cidrs
+  useEffect(() => {
+    if (cidrsData && cidrsData._referenceId && apiKeys) {
+      const actualApiKey = apiKeys[cidrsData._referenceId];
+
+      actualApiKey.cidrs = [...cidrsData.cidrs];
+
+      updateApiKey(cidrsData._referenceId, actualApiKey);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cidrsData]);
+
+  useEffect(() => {
+    if (mspData?.value) {
+      setApiKeys(convertArrayToRecordset(mspData.value as Subscription[]));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [mspData?.value]);
+
+  // Fetch all Manage Group API Keys
+  useEffect(() => {
+    mspFetchData(
+      "getManageSubscriptions",
+      { kind: SubscriptionTypeEnum.MANAGE_GROUP, limit: 10 },
+      SubscriptionPagination,
+      {
+        notify: "errors",
+      },
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <Box
+      bgcolor="background.paper"
+      borderRadius={0.5}
+      id="card-details"
+      padding={3}
+    >
+      <ApiKeysHeader description={props.description} title={props.title} />
+      <AccessControl requiredRole="admin">
+        <Button
+          onClick={handleOnGenerateClick}
+          size="medium"
+          startIcon={<Add />}
+          sx={{ marginBottom: 2 }}
+          variant="contained"
+        >
+          {t("routes.keys.groups.generate")}
+        </Button>
+      </AccessControl>
+      <Box>
+        {Object.entries(apiKeys ?? {}).map(([id, apiKey]) => (
+          <GroupsApiKeys.GroupApiKey
+            apiKey={apiKey}
+            key={id}
+            onDelete={handleDeleteClick}
+            onExpand={handleOnExpandClick}
+            onRotateKey={handleRotateKey}
+            onUpdateCidrs={handleUpdateCidrs}
+            subscriptionId={id}
+          />
+        ))}
+      </Box>
+    </Box>
+  );
+};
+
+/** GroupsApiKey internal component */
+const GroupApiKey = ({
+  apiKey,
+  onDelete,
+  onExpand,
+  onRotateKey,
+  onUpdateCidrs,
+  subscriptionId,
+}: GroupApiKeyProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <Accordion
+      disableGutters
+      key={subscriptionId}
+      onChange={(_, expanded) => onExpand(expanded, subscriptionId)}
+      square
+      sx={{
+        border: borderStyle,
+        borderRadius: 1,
+        marginY: 1,
+      }}
+    >
+      <AccordionSummary expandIcon={<ExpandMore />} id="panel1-header">
+        <Stack direction="row" flex={1} justifyContent="space-between">
+          <Box fontWeight={600}>{apiKey.name}</Box>
+          <Box marginRight={1}>
+            <Button
+              color="error"
+              onClick={(event) => onDelete(event, subscriptionId)}
+              startIcon={<Delete />}
+              variant="naked"
+            >
+              {t("buttons.delete")}
+            </Button>
+          </Box>
+        </Stack>
+      </AccordionSummary>
+      <AccordionDetails
+        sx={{
+          border: borderStyle,
+          borderRadius: 1,
+          margin: 3,
+          marginTop: 1,
+        }}
+      >
+        <ApiKeys
+          keys={{
+            primary_key: apiKey.primary_key,
+            secondary_key: apiKey.secondary_key,
+          }}
+          onRotateKey={(type) => onRotateKey(type, subscriptionId)}
+        />
+        <AuthorizedCidrs
+          cidrs={apiKey.cidrs as unknown as string[]}
+          description="routes.keys.authorizedCidrs.description"
+          editable={true}
+          onSaveClick={(cidrs) => onUpdateCidrs(cidrs, subscriptionId)}
+        />
+      </AccordionDetails>
+    </Accordion>
+  );
+};
+
+// namespaced component
+GroupsApiKeys.GroupApiKey = GroupApiKey;
--- apps/backoffice/src/pages/keys/index.tsx
+++ apps/backoffice/src/pages/keys/index.tsx
@@ -1,12 +1,15 @@
-import { ApiKeys, AuthorizedCidrs } from "@/components/api-keys";
+import { ApiKeys, AuthorizedCidrs, GroupsApiKeys } from "@/components/api-keys";
 import { PageHeader } from "@/components/headers";
+import { getConfiguration } from "@/config";
 import { Cidr } from "@/generated/api/Cidr";
 import { ManageKeyCIDRs } from "@/generated/api/ManageKeyCIDRs";
 import { SubscriptionKeyTypeEnum } from "@/generated/api/SubscriptionKeyType";
 import { SubscriptionKeys } from "@/generated/api/SubscriptionKeys";
 import useFetch from "@/hooks/use-fetch";
 import { AppLayout, PageLayout } from "@/layouts";
-import { Grid } from "@mui/material";
+import { Stack } from "@mui/material";
+import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { ReactElement, useEffect } from "react";
@@ -16,6 +19,8 @@ const pageDescriptionLocaleKey = "routes.keys.description";
 
 export default function Keys() {
   const { t } = useTranslation();
+  const router = useRouter();
+  const { data: session } = useSession();
   const { data: mkData, fetchData: mkFetchData } = useFetch<SubscriptionKeys>();
   const { data: acData, fetchData: acFetchData } = useFetch<ManageKeyCIDRs>();
 
@@ -48,24 +53,37 @@ export default function Keys() {
         description={pageDescriptionLocaleKey}
         title={pageTitleLocaleKey}
       />
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <ApiKeys
-            description={t("routes.keys.manage.description")}
-            keys={mkData}
-            onRotateKey={handleRotateKey}
-            title={t("routes.keys.manage.title")}
+      <Stack>
+        <ApiKeys
+          description={t("routes.keys.manage.description")}
+          keys={mkData}
+          onRotateKey={handleRotateKey}
+          title={t("routes.keys.manage.title")}
+        />
+        <AuthorizedCidrs
+          cidrs={acData?.cidrs as unknown as string[]}
+          description="routes.keys.authorizedCidrs.description"
+          editable={true}
+          onSaveClick={handleUpdateCidrs}
+        />
+      </Stack>
+      {getConfiguration().GROUP_APIKEY_ENABLED && (
+        <Stack marginTop={3}>
+          <GroupsApiKeys
+            description={t("routes.keys.groups.description")}
+            onCreateGroupClick={() =>
+              window.open(
+                `${getConfiguration().SELFCARE_URL}/dashboard/${
+                  session?.user?.institution.id
+                }/groups/add`,
+                "_blank",
+              )
+            }
+            onGenerateClick={() => router.push("/keys/new-group-api-key")}
+            title={t("routes.keys.groups.title")}
           />
-        </Grid>
-        <Grid item xs={12}>
-          <AuthorizedCidrs
-            cidrs={acData?.cidrs as unknown as string[]}
-            description="routes.keys.authorizedCidrs.description"
-            editable={true}
-            onSaveClick={handleUpdateCidrs}
-          />
-        </Grid>
-      </Grid>
+        </Stack>
+      )}
     </>
   );
 }
