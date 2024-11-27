d652058d465f0c58ee593ba324ed99991e3eeb2a
Rodolfo Viti <rodolfo.viti@pagopa.it>
2024-11-27 09:34:25 +0100

    add getGroupUnboundedServices to OpenAPI

--- apps/backoffice/src/app/api/services/group-unbound/route.ts
+++ apps/backoffice/src/app/api/services/group-unbound/route.ts
@@ -1,18 +1,20 @@
+import { UnboundedGroupServices } from "@/generated/api/UnboundedGroupServices";
 import { handleInternalErrorResponse } from "@/lib/be/errors";
 import { sanitizedNextResponseJson } from "@/lib/be/sanitize";
 import { retrieveUnboundedGroupServices } from "@/lib/be/services/business";
 import { withJWTAuthHandler } from "@/lib/be/wrappers";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 import { BackOfficeUser } from "../../../../../types/next-auth";
 /**
+ * @operationId getGroupUnboundedServices
  * @description Retrieve all group-unbounded services owned by the calling user
  */
 export const GET = withJWTAuthHandler(
   async (
     _: NextRequest,
     { backofficeUser }: { backofficeUser: BackOfficeUser },
-  ) => {
+  ): Promise<NextResponse<UnboundedGroupServices>> => {
     try {
       const result = await retrieveUnboundedGroupServices(backofficeUser);
 
