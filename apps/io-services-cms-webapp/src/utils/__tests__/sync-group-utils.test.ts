a27de7fcc8fc2ad92899049312d9867c6ea7136e
Rodolfo Viti <rodolfo.viti@pagopa.it>
2025-01-15 16:18:23 +0100

    unit test clean code

--- apps/io-services-cms-webapp/src/utils/__tests__/sync-group-utils.test.ts
+++ apps/io-services-cms-webapp/src/utils/__tests__/sync-group-utils.test.ts
@@ -6,26 +6,15 @@ import {
 } from "../sync-group-utils";
 import * as E from "fp-ts/lib/Either";
 import * as TE from "fp-ts/lib/TaskEither";
+import { ApimUtils } from "@io-services-cms/external-clients";
+import { ServiceLifecycle } from "@io-services-cms/models";
 
 const mocks = vi.hoisted(() => ({
-  ApimUtils: {
-    ApimService: { getSubscription: vi.fn(), updateSubscription: vi.fn() },
-    SUBSCRIPTION_MANAGE_GROUP_PREFIX: "FOO",
+  ApimService: { getSubscription: vi.fn(), updateSubscription: vi.fn() },
+  LifecycleStore: {
+    executeOnServicesFilteredByGroupId: vi.fn(),
+    bulkPatch: vi.fn(),
   },
-  ServiceLifecycle: {
-    LifecycleStore: {
-      executeOnServicesFilteredByGroupId: vi.fn(),
-      bulkPatch: vi.fn(),
-    },
-  },
-}));
-
-vi.mock("@io-services-cms/external-clients", () => ({
-  ApimUtils: mocks.ApimUtils,
-}));
-
-vi.mock("@io-services-cms/models", () => ({
-  ServiceLifecycle: mocks.ServiceLifecycle,
 }));
 
 beforeEach(() => {
@@ -34,9 +23,7 @@ beforeEach(() => {
 describe("sync group utils", () => {
   describe("syncSubscription", () => {
     const deps = {
-      apimService: mocks.ApimUtils.ApimService as unknown as Parameters<
-        typeof syncSubscription
-      >[0],
+      apimService: mocks.ApimService as unknown as ApimUtils.ApimService,
     };
 
     it.each`
@@ -49,9 +36,7 @@ describe("sync group utils", () => {
       async ({ testOutput, error, groupId, expectedResult }) => {
         // given
         const item = { productId: "prod-io", id: groupId } as GroupChangeEvent;
-        mocks.ApimUtils.ApimService.getSubscription.mockReturnValueOnce(
-          TE.left(error),
-        );
+        mocks.ApimService.getSubscription.mockReturnValueOnce(TE.left(error));
 
         // when
         const result = await syncSubscription(deps.apimService)(item)();
@@ -60,17 +45,11 @@ describe("sync group utils", () => {
         expect(result._tag === testOutput).toBeTruthy();
         const unionRes = E.toUnion(result);
         expect(unionRes).toStrictEqual(expectedResult);
-        expect(
-          mocks.ApimUtils.ApimService.getSubscription,
-        ).toHaveBeenCalledOnce();
-        expect(
-          mocks.ApimUtils.ApimService.getSubscription,
-        ).toHaveBeenCalledWith(
-          mocks.ApimUtils.SUBSCRIPTION_MANAGE_GROUP_PREFIX + item.id,
+        expect(mocks.ApimService.getSubscription).toHaveBeenCalledOnce();
+        expect(mocks.ApimService.getSubscription).toHaveBeenCalledWith(
+          ApimUtils.SUBSCRIPTION_MANAGE_GROUP_PREFIX + item.id,
         );
-        expect(
-          mocks.ApimUtils.ApimService.updateSubscription,
-        ).not.toHaveBeenCalled();
+        expect(mocks.ApimService.updateSubscription).not.toHaveBeenCalled();
       },
     );
 
@@ -96,10 +75,10 @@ describe("sync group utils", () => {
           state: "active",
           ...expSubParam,
         };
-        mocks.ApimUtils.ApimService.getSubscription.mockReturnValueOnce(
+        mocks.ApimService.getSubscription.mockReturnValueOnce(
           TE.right(expectedSubscription),
         );
-        mocks.ApimUtils.ApimService.updateSubscription.mockReturnValueOnce(
+        mocks.ApimService.updateSubscription.mockReturnValueOnce(
           TE.left(error),
         );
 
@@ -110,20 +89,12 @@ describe("sync group utils", () => {
         expect(result._tag === testOutput).toBeTruthy();
         const unionRes = E.toUnion(result);
         expect(unionRes).toStrictEqual(expectedResult);
-        expect(
-          mocks.ApimUtils.ApimService.getSubscription,
-        ).toHaveBeenCalledOnce();
-        expect(
-          mocks.ApimUtils.ApimService.getSubscription,
-        ).toHaveBeenCalledWith(
-          mocks.ApimUtils.SUBSCRIPTION_MANAGE_GROUP_PREFIX + item.id,
+        expect(mocks.ApimService.getSubscription).toHaveBeenCalledOnce();
+        expect(mocks.ApimService.getSubscription).toHaveBeenCalledWith(
+          ApimUtils.SUBSCRIPTION_MANAGE_GROUP_PREFIX + item.id,
         );
-        expect(
-          mocks.ApimUtils.ApimService.updateSubscription,
-        ).toHaveBeenCalledOnce();
-        expect(
-          mocks.ApimUtils.ApimService.updateSubscription,
-        ).toHaveBeenCalledWith(
+        expect(mocks.ApimService.updateSubscription).toHaveBeenCalledOnce();
+        expect(mocks.ApimService.updateSubscription).toHaveBeenCalledWith(
           expectedSubscription.name,
           {
             displayName: item.name,
@@ -147,10 +118,10 @@ describe("sync group utils", () => {
         eTag: "eTag",
         state: "active",
       };
-      mocks.ApimUtils.ApimService.getSubscription.mockReturnValueOnce(
+      mocks.ApimService.getSubscription.mockReturnValueOnce(
         TE.right(expectedSubscription),
       );
-      mocks.ApimUtils.ApimService.updateSubscription.mockReturnValueOnce(
+      mocks.ApimService.updateSubscription.mockReturnValueOnce(
         TE.right(expectedSubscription),
       );
       // when
@@ -161,18 +132,12 @@ describe("sync group utils", () => {
       if (E.isRight(result)) {
         expect(result.right).toBeUndefined();
       }
-      expect(
-        mocks.ApimUtils.ApimService.getSubscription,
-      ).toHaveBeenCalledOnce();
-      expect(mocks.ApimUtils.ApimService.getSubscription).toHaveBeenCalledWith(
-        mocks.ApimUtils.SUBSCRIPTION_MANAGE_GROUP_PREFIX + item.id,
+      expect(mocks.ApimService.getSubscription).toHaveBeenCalledOnce();
+      expect(mocks.ApimService.getSubscription).toHaveBeenCalledWith(
+        ApimUtils.SUBSCRIPTION_MANAGE_GROUP_PREFIX + item.id,
       );
-      expect(
-        mocks.ApimUtils.ApimService.updateSubscription,
-      ).toHaveBeenCalledOnce();
-      expect(
-        mocks.ApimUtils.ApimService.updateSubscription,
-      ).toHaveBeenCalledWith(
+      expect(mocks.ApimService.updateSubscription).toHaveBeenCalledOnce();
+      expect(mocks.ApimService.updateSubscription).toHaveBeenCalledWith(
         expectedSubscription.name,
         {
           displayName: item.name,
@@ -185,8 +150,8 @@ describe("sync group utils", () => {
 
   describe("syncServices", () => {
     const deps = {
-      serviceLifecycleStore: mocks.ServiceLifecycle
-        .LifecycleStore as unknown as Parameters<typeof syncServices>[0],
+      serviceLifecycleStore:
+        mocks.LifecycleStore as unknown as ServiceLifecycle.LifecycleStore,
     };
     it("should complete successfully but do nothing when status is different from DELETED", async () => {
       //given
@@ -196,20 +161,19 @@ describe("sync group utils", () => {
         name: "name",
         status: "ACTIVE",
       } as GroupChangeEvent;
+
       //when
       const result = await syncServices(deps.serviceLifecycleStore)(item)();
+
       //then
       expect(E.isRight(result)).toBeTruthy();
       if (E.isRight(result)) {
         expect(result.right).toBeUndefined();
       }
       expect(
-        mocks.ServiceLifecycle.LifecycleStore
-          .executeOnServicesFilteredByGroupId,
+        mocks.LifecycleStore.executeOnServicesFilteredByGroupId,
       ).not.toHaveBeenCalled();
-      expect(
-        mocks.ServiceLifecycle.LifecycleStore.bulkPatch,
-      ).not.toHaveBeenCalled();
+      expect(mocks.LifecycleStore.bulkPatch).not.toHaveBeenCalled();
     });
 
     it("should fail when status is DELETED and executeOnServicesFilteredByGroupId fails", async () => {
@@ -222,27 +186,25 @@ describe("sync group utils", () => {
         status: "DELETED",
       } as GroupChangeEvent;
       const error = new Error("error on executeOnServicesFilteredByGroupId");
-      mocks.ServiceLifecycle.LifecycleStore.executeOnServicesFilteredByGroupId.mockReturnValueOnce(
+      mocks.LifecycleStore.executeOnServicesFilteredByGroupId.mockReturnValueOnce(
         TE.left(error),
       );
+
       //when
       const result = await syncServices(deps.serviceLifecycleStore)(item)();
+
       //then
       expect(E.isLeft(result)).toBeTruthy();
       if (E.isLeft(result)) {
         expect(result.left).toStrictEqual(error);
       }
       expect(
-        mocks.ServiceLifecycle.LifecycleStore
-          .executeOnServicesFilteredByGroupId,
+        mocks.LifecycleStore.executeOnServicesFilteredByGroupId,
       ).toHaveBeenCalledOnce();
       expect(
-        mocks.ServiceLifecycle.LifecycleStore
-          .executeOnServicesFilteredByGroupId,
+        mocks.LifecycleStore.executeOnServicesFilteredByGroupId,
       ).toHaveBeenCalledWith(groupId, expect.any(Function));
-      expect(
-        mocks.ServiceLifecycle.LifecycleStore.bulkPatch,
-      ).not.toHaveBeenCalledOnce();
+      expect(mocks.LifecycleStore.bulkPatch).not.toHaveBeenCalledOnce();
     });
 
     it("should fail when status is DELETED and bulkPatch fails", async () => {
@@ -255,55 +217,41 @@ describe("sync group utils", () => {
         status: "DELETED",
       } as GroupChangeEvent;
       const serviceIds = ["service1", "service2"];
-      const expectedBulkPatchInput = [
-        {
-          data: {
-            metadata: {
-              group_id: undefined,
-            },
+      const expectedBulkPatchInput = serviceIds.map((sid) => ({
+        data: {
+          metadata: {
+            group_id: undefined,
           },
-          id: "service1",
         },
-        {
-          data: {
-            metadata: {
-              group_id: undefined,
-            },
-          },
-          id: "service2",
-        },
-      ];
+        id: sid,
+      }));
       const error = new Error("error on bulkPatch");
-      mocks.ServiceLifecycle.LifecycleStore.executeOnServicesFilteredByGroupId.mockImplementationOnce(
+      mocks.LifecycleStore.executeOnServicesFilteredByGroupId.mockImplementationOnce(
         (_groupId, callback) => {
           return callback(serviceIds);
         },
       );
 
-      mocks.ServiceLifecycle.LifecycleStore.bulkPatch.mockReturnValueOnce(
-        TE.left(error),
-      );
+      mocks.LifecycleStore.bulkPatch.mockReturnValueOnce(TE.left(error));
+
       //when
       const result = await syncServices(deps.serviceLifecycleStore)(item)();
+
       //then
       expect(E.isLeft(result)).toBeTruthy();
       if (E.isLeft(result)) {
         expect(result.left).toStrictEqual(error);
       }
       expect(
-        mocks.ServiceLifecycle.LifecycleStore
-          .executeOnServicesFilteredByGroupId,
+        mocks.LifecycleStore.executeOnServicesFilteredByGroupId,
       ).toHaveBeenCalledOnce();
       expect(
-        mocks.ServiceLifecycle.LifecycleStore
-          .executeOnServicesFilteredByGroupId,
+        mocks.LifecycleStore.executeOnServicesFilteredByGroupId,
       ).toHaveBeenCalledWith(groupId, expect.any(Function));
-      expect(
-        mocks.ServiceLifecycle.LifecycleStore.bulkPatch,
-      ).toHaveBeenCalledOnce();
-      expect(
-        mocks.ServiceLifecycle.LifecycleStore.bulkPatch,
-      ).toHaveBeenCalledWith(expectedBulkPatchInput);
+      expect(mocks.LifecycleStore.bulkPatch).toHaveBeenCalledOnce();
+      expect(mocks.LifecycleStore.bulkPatch).toHaveBeenCalledWith(
+        expectedBulkPatchInput,
+      );
     });
 
     it("should fail when status is DELETED and at least one of the bulkPatch operation has status different from 200", async () => {
@@ -316,41 +264,26 @@ describe("sync group utils", () => {
         status: "DELETED",
       } as GroupChangeEvent;
       const serviceIds = ["service1", "service2"];
-      const expectedBulkPatchInput = [
-        {
-          data: {
-            metadata: {
-              group_id: undefined,
-            },
+      const expectedBulkPatchInput = serviceIds.map((sid) => ({
+        data: {
+          metadata: {
+            group_id: undefined,
           },
-          id: "service1",
         },
-        {
-          data: {
-            metadata: {
-              group_id: undefined,
-            },
-          },
-          id: "service2",
-        },
-      ];
-      const bulkPatchResult = [
-        {
-          statusCode: 200,
-          requestCharge: 1,
-        },
-        {
-          statusCode: 200,
-          requestCharge: 1,
-        },
-      ];
-      mocks.ServiceLifecycle.LifecycleStore.executeOnServicesFilteredByGroupId.mockImplementationOnce(
+        id: sid,
+      }));
+      mocks.LifecycleStore.executeOnServicesFilteredByGroupId.mockImplementationOnce(
         (_groupId, callback) => {
           return callback(serviceIds);
         },
       );
-      mocks.ServiceLifecycle.LifecycleStore.bulkPatch.mockReturnValueOnce(
-        TE.right(bulkPatchResult),
+      mocks.LifecycleStore.bulkPatch.mockReturnValueOnce(
+        TE.right(
+          expectedBulkPatchInput.map((_) => ({
+            statusCode: 200,
+            requestCharge: 1,
+          })),
+        ),
       );
 
       //when
@@ -362,19 +295,15 @@ describe("sync group utils", () => {
         expect(result.right).toBeUndefined();
       }
       expect(
-        mocks.ServiceLifecycle.LifecycleStore
-          .executeOnServicesFilteredByGroupId,
+        mocks.LifecycleStore.executeOnServicesFilteredByGroupId,
       ).toHaveBeenCalledOnce();
       expect(
-        mocks.ServiceLifecycle.LifecycleStore
-          .executeOnServicesFilteredByGroupId,
+        mocks.LifecycleStore.executeOnServicesFilteredByGroupId,
       ).toHaveBeenCalledWith(groupId, expect.any(Function));
-      expect(
-        mocks.ServiceLifecycle.LifecycleStore.bulkPatch,
-      ).toHaveBeenCalledOnce();
-      expect(
-        mocks.ServiceLifecycle.LifecycleStore.bulkPatch,
-      ).toHaveBeenCalledWith(expectedBulkPatchInput);
+      expect(mocks.LifecycleStore.bulkPatch).toHaveBeenCalledOnce();
+      expect(mocks.LifecycleStore.bulkPatch).toHaveBeenCalledWith(
+        expectedBulkPatchInput,
+      );
     });
 
     it("should complete successfully", async () => {
@@ -387,42 +316,27 @@ describe("sync group utils", () => {
         status: "DELETED",
       } as GroupChangeEvent;
       const serviceIds = ["service1", "service2"];
-      const expectedBulkPatchInput = [
-        {
-          data: {
-            metadata: {
-              group_id: undefined,
-            },
+      const expectedBulkPatchInput = serviceIds.map((sid) => ({
+        data: {
+          metadata: {
+            group_id: undefined,
           },
-          id: "service1",
         },
-        {
-          data: {
-            metadata: {
-              group_id: undefined,
-            },
-          },
-          id: "service2",
-        },
-      ];
-      const bulkPatchResult = [
-        {
-          statusCode: 200,
-          requestCharge: 1,
-        },
-        {
-          statusCode: 500,
-          requestCharge: 1,
-        },
-      ];
+        id: sid,
+      }));
       const error = new Error("At least one patch operation failed");
-      mocks.ServiceLifecycle.LifecycleStore.executeOnServicesFilteredByGroupId.mockImplementationOnce(
+      mocks.LifecycleStore.executeOnServicesFilteredByGroupId.mockImplementationOnce(
         (_groupId, callback) => {
           return callback(serviceIds);
         },
       );
-      mocks.ServiceLifecycle.LifecycleStore.bulkPatch.mockReturnValueOnce(
-        TE.right(bulkPatchResult),
+      mocks.LifecycleStore.bulkPatch.mockReturnValueOnce(
+        TE.right(
+          expectedBulkPatchInput.map((_, i) => ({
+            statusCode: i === 0 ? 200 : 500,
+            requestCharge: 1,
+          })),
+        ),
       );
 
       //when
@@ -434,19 +348,15 @@ describe("sync group utils", () => {
         expect(result.left).toStrictEqual(error);
       }
       expect(
-        mocks.ServiceLifecycle.LifecycleStore
-          .executeOnServicesFilteredByGroupId,
+        mocks.LifecycleStore.executeOnServicesFilteredByGroupId,
       ).toHaveBeenCalledOnce();
       expect(
-        mocks.ServiceLifecycle.LifecycleStore
-          .executeOnServicesFilteredByGroupId,
+        mocks.LifecycleStore.executeOnServicesFilteredByGroupId,
       ).toHaveBeenCalledWith(groupId, expect.any(Function));
-      expect(
-        mocks.ServiceLifecycle.LifecycleStore.bulkPatch,
-      ).toHaveBeenCalledOnce();
-      expect(
-        mocks.ServiceLifecycle.LifecycleStore.bulkPatch,
-      ).toHaveBeenCalledWith(expectedBulkPatchInput);
+      expect(mocks.LifecycleStore.bulkPatch).toHaveBeenCalledOnce();
+      expect(mocks.LifecycleStore.bulkPatch).toHaveBeenCalledWith(
+        expectedBulkPatchInput,
+      );
     });
   });
 });
