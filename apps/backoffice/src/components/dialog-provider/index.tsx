48d1e8fd6fe7c76ea59cbd0f3994e7355fec1656
Giuseppe Di Pinto <giuseppeantonio.dipinto@pagopa.it>
2025-02-07 10:59:01 +0100

    refactor useDialog with exported DialogBaseView

--- apps/backoffice/src/components/dialog-provider/index.tsx
+++ apps/backoffice/src/components/dialog-provider/index.tsx
@@ -10,6 +10,8 @@ import { useTranslation } from "next-i18next";
 import { ReactNode, createContext, useContext, useState } from "react";
 
 interface DialogOptions {
+  /** Optional custom content rendered below `message` */
+  body?: ReactNode;
   /** label for cancel button: if specified, overwrite default value */
   cancelButtonLabel?: string;
   /** label for confirm button: if specified, overwrite default value */
@@ -37,7 +39,6 @@ const DialogContext = createContext<ShowDialogHandler>(() => {
 const DialogProvider: React.FC<{
   children: ReactNode;
 }> = ({ children }) => {
-  const { t } = useTranslation();
   const [open, setOpen] = useState(false);
   const [options, setOptions] = useState<DialogOptions>({ title: "" });
   const [promiseInfo, setPromiseInfo] = useState<PromiseInfo>();
@@ -65,49 +66,69 @@ const DialogProvider: React.FC<{
 
   return (
     <>
-      <Dialog
-        data-testid="bo-io-dialog-provider"
-        disableScrollLock
+      <DialogBaseView
+        {...options}
+        isOpen={open}
         onClose={handleCancel}
-        open={open}
-      >
-        <DialogTitle>{options.title}</DialogTitle>
-        <DialogContent sx={{ minWidth: "600px" }}>
-          <Typography
-            dangerouslySetInnerHTML={{
-              __html: options.message ?? "",
-            }}
-          ></Typography>
-        </DialogContent>
-        <DialogActions sx={{ padding: "16px 24px" }}>
-          {!options.hideCancelButton && (
-            <Button
-              data-testid="bo-io-dialog-provider-cancel-button"
-              onClick={handleCancel}
-              variant="outlined"
-            >
-              {options.cancelButtonLabel
-                ? t(options.cancelButtonLabel)
-                : t("buttons.cancel")}
-            </Button>
-          )}
-          {!options.hideConfirmButton && (
-            <Button
-              data-testid="bo-io-dialog-provider-confirm-button"
-              onClick={handleConfirm}
-              variant="contained"
-            >
-              {options.confirmButtonLabel
-                ? t(options.confirmButtonLabel)
-                : t("buttons.confirm")}
-            </Button>
-          )}
-        </DialogActions>
-      </Dialog>
+        onConfirm={handleConfirm}
+      />
       <DialogContext.Provider value={showDialog}>
         {children}
       </DialogContext.Provider>
     </>
+  );
+};
+
+export interface DialogBaseViewProps extends DialogOptions {
+  isOpen: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+}
+
+export const DialogBaseView = (props: DialogBaseViewProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <Dialog
+      data-testid="bo-io-dialog-provider"
+      disableScrollLock
+      onClose={props.onClose}
+      open={props.isOpen}
+    >
+      <DialogTitle>{props.title}</DialogTitle>
+      <DialogContent sx={{ minWidth: "600px" }}>
+        <Typography
+          dangerouslySetInnerHTML={{
+            __html: props.message ?? "",
+          }}
+        ></Typography>
+        {props.body}
+      </DialogContent>
+      <DialogActions sx={{ padding: "16px 24px" }}>
+        {!props.hideCancelButton && (
+          <Button
+            data-testid="bo-io-dialog-provider-cancel-button"
+            onClick={props.onClose}
+            variant="outlined"
+          >
+            {props.cancelButtonLabel
+              ? t(props.cancelButtonLabel)
+              : t("buttons.cancel")}
+          </Button>
+        )}
+        {!props.hideConfirmButton && (
+          <Button
+            data-testid="bo-io-dialog-provider-confirm-button"
+            onClick={props.onConfirm}
+            variant="contained"
+          >
+            {props.confirmButtonLabel
+              ? t(props.confirmButtonLabel)
+              : t("buttons.confirm")}
+          </Button>
+        )}
+      </DialogActions>
+    </Dialog>
   );
 };
 
