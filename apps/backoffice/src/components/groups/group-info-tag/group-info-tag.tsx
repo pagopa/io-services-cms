987971f18dd6329c48f59d369966503b3e56726e
Giuseppe Di Pinto <giuseppeantonio.dipinto@pagopa.it>
2025-02-12 16:01:11 +0100

    add implementation

--- apps/backoffice/src/components/groups/group-info-tag/group-info-tag.tsx
+++ apps/backoffice/src/components/groups/group-info-tag/group-info-tag.tsx
@@ -1,5 +1,6 @@
 import { ApiKeysGroupTag } from "@/components/api-keys/api-keys-groups/api-keys-group-tag";
-import { Edit } from "@mui/icons-material";
+import { useDialog } from "@/components/dialog-provider";
+import { Delete, Edit } from "@mui/icons-material";
 import { IconButton, Stack, Typography } from "@mui/material";
 import { ButtonNaked } from "@pagopa/mui-italia";
 import { useSession } from "next-auth/react";
@@ -9,11 +10,30 @@ export interface GroupInfoTagProps {
   /** Group name */
   name?: string;
   onAssociateClick?: () => void;
+  onUnboundClick?: () => void;
 }
 
-export const GroupInfoTag = ({ name, onAssociateClick }: GroupInfoTagProps) => {
+export const GroupInfoTag = ({
+  name,
+  onAssociateClick,
+  onUnboundClick,
+}: GroupInfoTagProps) => {
   const { t } = useTranslation();
   const { data: session } = useSession();
+  const showDialog = useDialog();
+
+  const handleUnbound = async () => {
+    const confirmUnbound = await showDialog({
+      confirmButtonLabel: t("buttons.delete"),
+      message: t("routes.service.group.unbound.modal.message"),
+      title: t("routes.service.group.unbound.modal.title"),
+    });
+    if (confirmUnbound && onUnboundClick) {
+      onUnboundClick();
+    } else {
+      console.warn("Operation canceled");
+    }
+  };
 
   // Unbounded service: only admin can bound it to a group
   if (!name) {
@@ -42,14 +62,24 @@ export const GroupInfoTag = ({ name, onAssociateClick }: GroupInfoTagProps) => {
     <Stack direction="row" spacing={1}>
       <ApiKeysGroupTag label={name} />
       {session?.user?.institution.role === "admin" && (
-        <IconButton
-          aria-label="edit"
-          color="primary"
-          onClick={onAssociateClick}
-          size="small"
-        >
-          <Edit fontSize="inherit" />
-        </IconButton>
+        <>
+          <IconButton
+            aria-label="edit"
+            color="primary"
+            onClick={onAssociateClick}
+            size="small"
+          >
+            <Edit fontSize="inherit" />
+          </IconButton>
+          <IconButton
+            aria-label="unbound"
+            color="inherit"
+            onClick={handleUnbound}
+            size="small"
+          >
+            <Delete color="error" fontSize="inherit" />
+          </IconButton>
+        </>
       )}
     </Stack>
   );
