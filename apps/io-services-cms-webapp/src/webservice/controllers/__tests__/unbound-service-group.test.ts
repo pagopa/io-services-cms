b8dd3baeca659f34f6931e1ff0e0bfa035b4309a
Leonardo De Marco <leodemarco94@gmail.com>
2025-02-12 17:41:27 +0100

    added unit test

---
+++ apps/io-services-cms-webapp/src/webservice/controllers/__tests__/unbound-service-group.test.ts
@@ -0,0 +1,190 @@
+import { ApimUtils } from "@io-services-cms/external-clients";
+import { ServiceLifecycle } from "@io-services-cms/models";
+import { SubscriptionCIDRsModel } from "@pagopa/io-functions-commons/dist/src/models/subscription_cidrs";
+import { UserGroup } from "@pagopa/io-functions-commons/dist/src/utils/middlewares/azure_api_auth";
+import { setAppContext } from "@pagopa/io-functions-commons/dist/src/utils/middlewares/context_middleware";
+import { ResponseErrorForbiddenNotAuthorized } from "@pagopa/ts-commons/lib/responses";
+import * as TE from "fp-ts/lib/TaskEither";
+import request from "supertest";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import { IConfig } from "../../../config";
+import { WebServerDependencies, createWebServer } from "../../index";
+
+const aService = {
+  id: "aServiceId",
+} as unknown as ServiceLifecycle.ItemType;
+
+const {
+  serviceOwnerCheckManageTaskMock,
+  logErrorResponseMock,
+  getLoggerMock,
+  patchMock,
+  fsmLifecycleClientCreatorMock,
+} = vi.hoisted(() => {
+  const logErrorResponseMock = vi.fn((err) => err);
+  const patchMock = vi.fn<any[], any>(() => TE.right(aService));
+  return {
+    serviceOwnerCheckManageTaskMock: vi.fn<any[], any>((_, serviceId) =>
+      TE.right(serviceId),
+    ),
+    logErrorResponseMock,
+    getLoggerMock: vi.fn(() => ({ logErrorResponse: logErrorResponseMock })),
+    patchMock,
+    fsmLifecycleClientCreatorMock: vi.fn(() => ({
+      getStore: () => ({
+        patch: patchMock,
+      }),
+    })),
+  };
+});
+
+vi.mock("../../../utils/subscription", () => ({
+  serviceOwnerCheckManageTask: serviceOwnerCheckManageTaskMock,
+}));
+
+vi.mock("../../../utils/logger", () => ({
+  getLogger: getLoggerMock,
+}));
+
+beforeEach(() => {
+  vi.restoreAllMocks();
+});
+
+describe("unboundServiceGroup", () => {
+  const logPrefix = "UnboundServiceGroupHandler";
+  const aManageSubscriptionId = "MANAGE-123";
+  const anUserId = "123";
+  const mockApimService = {} as unknown as ApimUtils.ApimService;
+  const mockConfig = {
+    BACKOFFICE_INTERNAL_SUBNET_CIDRS: ["127.0.0.1/32"],
+  } as unknown as IConfig;
+  const mockAppinsights = {
+    trackEvent: vi.fn(),
+    trackError: vi.fn(),
+  } as any;
+  const mockContext = {} as any;
+  const app = createWebServer({
+    basePath: "api",
+    apimService: mockApimService,
+    config: mockConfig,
+    fsmLifecycleClientCreator: fsmLifecycleClientCreatorMock,
+    fsmPublicationClient: vi.fn(),
+    subscriptionCIDRsModel: {} as SubscriptionCIDRsModel,
+    telemetryClient: mockAppinsights,
+    blobService: vi.fn(),
+    serviceTopicDao: vi.fn(),
+  } as unknown as WebServerDependencies);
+
+  setAppContext(app, mockContext);
+
+  it("should not allow the operation without right userId", async () => {
+    // given
+    const serviceId = aService.id;
+    const error = ResponseErrorForbiddenNotAuthorized;
+    serviceOwnerCheckManageTaskMock.mockReturnValueOnce(TE.left(error));
+
+    // when
+    const response = await request(app)
+      .delete(`/api/services/${serviceId}/group`)
+      .set("x-user-email", "example@email.com")
+      .set("x-user-groups", UserGroup.ApiServiceWrite)
+      .set("x-user-id", anUserId)
+      .set("x-subscription-id", aManageSubscriptionId);
+
+    // then
+    expect(response.statusCode).toBe(403);
+    expect(serviceOwnerCheckManageTaskMock).toHaveBeenCalledOnce();
+    expect(serviceOwnerCheckManageTaskMock).toHaveBeenCalledWith(
+      mockApimService,
+      serviceId,
+      aManageSubscriptionId,
+      anUserId,
+    );
+    expect(patchMock).not.toHaveBeenCalled();
+    expect(getLoggerMock).toHaveBeenCalledOnce();
+    expect(getLoggerMock).toHaveBeenCalledWith(mockContext, logPrefix);
+    expect(logErrorResponseMock).toHaveBeenCalledOnce();
+    expect(logErrorResponseMock).toHaveBeenCalledWith(expect.anything(), {
+      serviceId,
+      userSubscriptionId: aManageSubscriptionId,
+    });
+  });
+
+  it("should not allow the operation without right group", async () => {
+    // given
+    const serviceId = aService.id;
+
+    // when
+    const response = await request(app)
+      .delete(`/api/services/${serviceId}/group`)
+      .set("x-user-email", "example@email.com")
+      .set("x-user-groups", "OtherGroup")
+      .set("x-user-id", anUserId)
+      .set("x-subscription-id", aManageSubscriptionId);
+
+    // then
+    expect(response.statusCode).toBe(403);
+    expect(serviceOwnerCheckManageTaskMock).not.toHaveBeenCalled();
+    expect(patchMock).not.toHaveBeenCalled();
+    expect(getLoggerMock).not.toHaveBeenCalled();
+    expect(logErrorResponseMock).not.toHaveBeenCalled();
+  });
+
+  it("should fail when cannot find requested service", async () => {
+    // given
+    const serviceId = aService.id;
+    patchMock.mockReturnValueOnce(TE.left(new Error("patch error")));
+
+    // when
+    const response = await request(app)
+      .delete(`/api/services/${serviceId}/group`)
+      .set("x-user-email", "example@email.com")
+      .set("x-user-groups", UserGroup.ApiServiceWrite)
+      .set("x-user-id", anUserId)
+      .set("x-subscription-id", aManageSubscriptionId);
+
+    // then
+    expect(response.statusCode).toBe(500);
+    expect(serviceOwnerCheckManageTaskMock).toHaveBeenCalledOnce();
+    expect(serviceOwnerCheckManageTaskMock).toHaveBeenCalledWith(
+      mockApimService,
+      serviceId,
+      aManageSubscriptionId,
+      anUserId,
+    );
+    expect(patchMock).toHaveBeenCalledOnce();
+    expect(getLoggerMock).toHaveBeenCalledOnce();
+    expect(getLoggerMock).toHaveBeenCalledWith(mockContext, logPrefix);
+    expect(logErrorResponseMock).toHaveBeenCalledOnce();
+    expect(logErrorResponseMock).toHaveBeenCalledWith(expect.anything(), {
+      serviceId,
+      userSubscriptionId: aManageSubscriptionId,
+    });
+  });
+
+  it("should unbound a service from its group", async () => {
+    // given
+    const serviceId = aService.id;
+
+    // when
+    const response = await request(app)
+      .delete(`/api/services/${serviceId}/group`)
+      .set("x-user-email", "example@email.com")
+      .set("x-user-groups", UserGroup.ApiServiceWrite)
+      .set("x-user-id", anUserId)
+      .set("x-subscription-id", aManageSubscriptionId);
+
+    // then
+    expect(response.statusCode).toBe(204);
+    expect(serviceOwnerCheckManageTaskMock).toHaveBeenCalledOnce();
+    expect(serviceOwnerCheckManageTaskMock).toHaveBeenCalledWith(
+      mockApimService,
+      serviceId,
+      aManageSubscriptionId,
+      anUserId,
+    );
+    expect(patchMock).toHaveBeenCalledOnce();
+    expect(getLoggerMock).not.toHaveBeenCalled();
+    expect(logErrorResponseMock).not.toHaveBeenCalled();
+  });
+});
