e4a770bbbcf85ed348cd9522693b17f259e905c5
Rodolfo Viti <62432865+rodoviti@users.noreply.github.com>
2025-02-21 15:07:18 +0100

    [IOPAE-1304] add postgre flexible servers using dx modules (#1170)

--- infra/src/database_runtime_server_migration.tf
+++ infra/src/database_runtime_server_migration.tf
@@ -1,45 +1,35 @@
-# https://docs.microsoft.com/en-us/azure/postgresql/flexible-server/concepts-compare-single-server-flexible-server
 module "runtime_server_migration_postgres_flexible_server" {
-  source = "github.com/pagopa/terraform-azurerm-v3.git//postgres_flexible_server?ref=v8.44.2"
+  source  = "pagopa/dx-azure-postgres-server/azurerm"
+  version = "~> 1"
 
-  name                = "${local.project}-${local.application_basename}-runtime-server-migration-pgflex"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  environment = {
+    prefix          = var.prefix
+    env_short       = var.env_short
+    location        = azurerm_resource_group.rg.location
+    app_name        = "${local.application_basename}-runtime-server-migration"
+    instance_number = "01"
+  }
+  resource_group_name                  = azurerm_resource_group.rg.name
+  private_dns_zone_resource_group_name = var.io_common.resource_group_name
 
-  ### Network
-  private_endpoint_enabled = true
-  private_dns_zone_id      = local.is_prod ? data.azurerm_private_dns_zone.privatelink_postgres_database_azure_com[0].id : null
-  delegated_subnet_id      = module.postgres_flexible_snet.id
+  tier = "m"
 
-  ### Admin
-  administrator_login    = data.azurerm_key_vault_secret.devportalservicedata_db_server_adm_username.value
-  administrator_password = data.azurerm_key_vault_secret.devportalservicedata_db_server_adm_password.value
+  db_version = 11
 
-  sku_name   = "GP_Standard_D2ds_v5"
-  db_version = "11"
-  # Possible values are 32768, 65536, 131072, 262144, 524288, 1048576,
-  # 2097152, 4194304, 8388608, 16777216, and 33554432.
-  storage_mb = 32768
-
-  ### zones & HA
-  high_availability_enabled = false
-
-  maintenance_window_config = {
-    day_of_week  = 0
-    start_hour   = 2
-    start_minute = 0
+  administrator_credentials = {
+    name     = data.azurerm_key_vault_secret.devportalservicedata_db_server_adm_username.value
+    password = data.azurerm_key_vault_secret.devportalservicedata_db_server_adm_password.value
   }
 
+  subnet_pep_id = data.azurerm_subnet.private_endpoints_subnet[0].id
+
   ### backup
-  backup_retention_days        = 7
-  geo_redundant_backup_enabled = false
+  backup_retention_days = 7
 
   pgbouncer_enabled = true
 
   custom_metric_alerts = null
   alerts_enabled       = true
-
-  diagnostic_settings_enabled = false
 
   tags = var.tags
 }
