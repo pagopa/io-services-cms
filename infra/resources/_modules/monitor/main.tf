98f8c6be18a16685bbb80b215ee3257fd11697af
Rodolfo Viti <rodolfo.viti@pagopa.it>
2025-05-31 13:35:07 +0200

    add new domain-specific error action group

---
+++ infra/resources/_modules/monitor/main.tf
@@ -0,0 +1,22 @@
+##########
+# Alerts #
+##########
+resource "azurerm_monitor_action_group" "error_action_group" {
+  resource_group_name = var.resource_group_name
+  name                = "${var.prefix}${var.env_short}-${var.domain}-error-ag-01"
+  short_name          = "${var.prefix}${var.env_short}-${var.domain}-error-ag-01"
+
+  webhook_receiver {
+    name                    = "callslacksvcmonitor"
+    service_uri             = data.azurerm_key_vault_secret.slack_svc_monitor_webhook_url.value
+    use_common_alert_schema = true
+  }
+
+  webhook_receiver {
+    name                    = "callopsgenie"
+    service_uri             = data.azurerm_key_vault_secret.alert_error_notification_opsgenie.value
+    use_common_alert_schema = true
+  }
+
+  tags = var.tags
+}
---
+++ infra/resources/_modules/monitor/outputs.tf
@@ -0,0 +1,3 @@
+output "error_action_group_id" {
+  value = azurerm_monitor_action_group.error_action_group.id
+}
