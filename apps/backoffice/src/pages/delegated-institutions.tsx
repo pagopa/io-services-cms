118e3d52caf8cb4a8c920a8863ea8ac7c610e526
Giuseppe Di Pinto <118166285+giuseppedipinto@users.noreply.github.com>
2025-06-09 11:36:31 +0200

    [IOPAE-2048] Added delegated institutions basic table view (#1280)
    <!--- Please always add a PR description as if nobody knows anything
    about the context these changes come from. -->
    <!--- Even if we are all from our internal team, we may not be on the
    same page. -->
    <!--- Write this PR as you were contributing to a public OSS project,
    where nobody knows you and you have to earn their trust. -->
    <!--- This will improve our projects in the long run! Thanks. -->
    Added delegated institutions basic table view.
    
    **Note:** watch video below to better appreciate ux/ui result _(first
    step)_.
    
    #### List of Changes
    
    <!--- Describe your changes in detail -->
    
    #### Motivation and Context
    Need to show the list of delegated institutions.
    <!--- Why is this change required? What problem does it solve? -->
    
    #### How Has This Been Tested?
    Local `dev` mode with `msw` mocks.
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, tests ran to see how
    -->
    <!--- your change affects other areas of the code, etc. -->
    
    #### Screenshots (if appropriate):
    
    
    https://github.com/user-attachments/assets/967225ed-1f5c-49ef-b046-9d1f92be7a1b
    
    
    <!--- Attach screenshots in case changes impact UI. -->
    
    #### Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.

--- apps/backoffice/src/pages/delegated-institutions.tsx
+++ apps/backoffice/src/pages/delegated-institutions.tsx
@@ -1,19 +1,154 @@
+import { EmptyStateLayer } from "@/components/empty-state";
 import { PageHeader } from "@/components/headers";
+import {
+  TableRowMenuAction,
+  TableView,
+  TableViewColumn,
+} from "@/components/table-view";
+import { DelegatedInstitutionPagination } from "@/generated/api/DelegatedInstitutionPagination";
+import useFetch from "@/hooks/use-fetch";
 import { AppLayout, PageLayout } from "@/layouts";
 import { authOptions } from "@/lib/auth/auth-options";
 import { Typography } from "@mui/material";
+import * as E from "fp-ts/lib/Either";
 import { Session, getServerSession } from "next-auth";
 import { useSession } from "next-auth/react";
-import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { ReactElement } from "react";
+import { ReactElement, useEffect, useState } from "react";
 
 const pageTitleLocaleKey = "routes.delegated-institutions.title";
 const pageDescriptionLocaleKey = "routes.delegated-institutions.description";
 
+const DEFAULT_PAGE_LIMIT = 10;
+
+interface DelegatedInstitutionListItem {
+  id: string;
+  name: string;
+  primary_key?: string;
+  secondary_key?: string;
+}
+
+const delegatedInstitutionPlaceholder: DelegatedInstitutionListItem = {
+  id: "id",
+  name: "name",
+};
+
 export default function DelegatedInstitutions() {
-  const { t } = useTranslation();
   const { data: session } = useSession();
+
+  const tableViewColumns: TableViewColumn<DelegatedInstitutionListItem>[] = [
+    {
+      alignment: "left",
+      cellTemplate: (di) => (
+        <Typography fontWeight={600} variant="body2">
+          {di.name}
+        </Typography>
+      ),
+      label: "routes.delegated-institutions.tableHeader.name",
+      name: "name",
+    },
+    {
+      alignment: "left",
+      label: "routes.delegated-institutions.tableHeader.primaryKey",
+      name: "primary_key",
+    },
+    {
+      alignment: "left",
+      label: "routes.delegated-institutions.tableHeader.secondaryKey",
+      name: "secondary_key",
+    },
+  ];
+
+  const {
+    data: dipData,
+    fetchData: dipFetchData,
+    loading: dipLoading,
+  } = useFetch<DelegatedInstitutionPagination>();
+  const [delegatedInstitutions, setDelegatedInstitutions] =
+    useState<DelegatedInstitutionListItem[]>();
+  const [pagination, setPagination] = useState({
+    count: 0,
+    limit: DEFAULT_PAGE_LIMIT,
+    offset: 0,
+  });
+  const [noDelegatedInstitutions, setNoDelegatedInstitutions] = useState(false);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [currentSearchByInstitutionName, setCurrentSearchByInstitutionName] =
+    useState<string>();
+
+  const handlePageChange = (pageIndex: number) =>
+    setPagination({
+      count: pagination.count,
+      limit: pagination.limit,
+      offset: pagination.limit * pageIndex,
+    });
+
+  const handleRowsPerPageChange = (pageSize: number) =>
+    setPagination({
+      count: pagination.count,
+      limit: pageSize,
+      offset: 0,
+    });
+
+  /** Returns a list of `TableRowMenuAction` depending on delegated institution status */
+  const getDelegatedInstitutionMenu = (
+    _di: DelegatedInstitutionListItem,
+  ): TableRowMenuAction[] => {
+    const result: TableRowMenuAction[] = [];
+    // TODO
+    return result;
+  };
+
+  const getDelegatedInstitutions = () => {
+    dipFetchData(
+      "getDelegatedInstitutions",
+      {
+        institutionId: session?.user?.institution.id as string,
+        limit: pagination.limit,
+        offset: pagination.offset,
+      },
+      DelegatedInstitutionPagination,
+      {
+        notify: "errors",
+      },
+    );
+  };
+
+  // fetch services when table pagination or search by institution name change
+  useEffect(() => {
+    getDelegatedInstitutions();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pagination, currentSearchByInstitutionName]);
+
+  // make some checks and adjustments on delegated institutions fetch result
+  useEffect(() => {
+    const maybeDipData = DelegatedInstitutionPagination.decode(dipData);
+    if (E.isRight(maybeDipData)) {
+      // check fetch result (if no delegated institutions, an EmptyState component will be displayed)
+      if (
+        maybeDipData.right.value.length === 0 &&
+        !currentSearchByInstitutionName // no active search by institution name in progress
+      )
+        setNoDelegatedInstitutions(true);
+      // check pagination data and build a full pagination.count array of delegated institutions
+      // filling empty items with some placeholders (in order to have a working paginated MUI Table)
+      if (
+        maybeDipData.right.pagination.offset !== undefined &&
+        maybeDipData.right.pagination.limit
+      ) {
+        const delegatedInstitutionsPlaceholders = Array(
+          maybeDipData.right.pagination.count,
+        ).fill(delegatedInstitutionPlaceholder);
+        delegatedInstitutionsPlaceholders.splice(
+          maybeDipData.right.pagination.offset,
+          maybeDipData.right.value.length,
+          ...maybeDipData.right.value,
+        );
+        setDelegatedInstitutions(delegatedInstitutionsPlaceholders);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dipData]);
 
   return (
     <>
@@ -21,10 +156,23 @@ export default function DelegatedInstitutions() {
         description={pageDescriptionLocaleKey}
         title={pageTitleLocaleKey}
       />
-      <Typography color="text.secondary" fontStyle="italic" variant="body2">
-        {t("placeholder page for aggregator institution ")}{" "}
-        {session?.user?.institution.id}
-      </Typography>
+      {noDelegatedInstitutions ? (
+        <EmptyStateLayer
+          ctaLabel=""
+          ctaRoute=""
+          emptyStateLabel="routes.delegated-institutions.empty"
+        />
+      ) : (
+        <TableView
+          columns={tableViewColumns}
+          loading={dipLoading}
+          onPageChange={handlePageChange}
+          onRowsPerPageChange={handleRowsPerPageChange}
+          pagination={pagination}
+          rowMenu={getDelegatedInstitutionMenu}
+          rows={delegatedInstitutions}
+        />
+      )}
     </>
   );
 }
