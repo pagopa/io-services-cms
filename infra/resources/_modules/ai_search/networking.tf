494d14da9bac75caef031d21896fdd66b722390b
Stefano.D'aversa <stefano.daversa@pagopa.it>
2025-07-08 15:11:47 +0200

    change private_endpoint_network_policies to enabled

--- infra/resources/_modules/ai_search/networking.tf
+++ infra/resources/_modules/ai_search/networking.tf
@@ -5,7 +5,7 @@ module "srch_snet" {
   virtual_network_name = var.virtual_network.name
   address_prefixes     = local.snet_cidrs
 
-  private_endpoint_network_policies = "Disabled"
+  private_endpoint_network_policies = "Enabled"
 }
 
 resource "azurerm_private_endpoint" "srch" {
