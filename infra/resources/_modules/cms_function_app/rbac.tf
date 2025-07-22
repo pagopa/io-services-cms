1980b989df375261cb6f02df695755e1dd635053
Leonardo De Marco <leodemarco94@gmail.com>
2025-07-22 09:52:59 +0200

    add blob storage writer role to cms function app

--- infra/resources/_modules/cms_function_app/rbac.tf
+++ infra/resources/_modules/cms_function_app/rbac.tf
@@ -13,3 +13,19 @@ module "adf_to_blob_data_reader_db" {
     }
   ]
 }
+
+module "function_app_to_blob_data_writer" {
+  source  = "pagopa-dx/azure-role-assignments/azurerm"
+  version = "~> 1.0"
+
+  principal_id    = module.cms_fn.function_app.function_app.principal_id
+  subscription_id = data.azurerm_subscription.current.subscription_id
+  storage_blob = [{
+    storage_account_name = module.cms_storage_account.name
+    resource_group_name  = module.cms_storage_account.resource_group_name
+    description          = "To allow migrating the activations of special services's"
+    role                 = "writer"
+    container_name       = azurerm_storage_container.activations.name
+    }
+  ]
+}
