e1f0461080618c13eb0b39134b47e662e36cf138
Leonardo De Marco <leodemarco94@gmail.com>
2025-07-22 14:56:38 +0000

    add blob owner role to function app

--- infra/resources/_modules/cms_function_app/rbac.tf
+++ infra/resources/_modules/cms_function_app/rbac.tf
@@ -24,7 +24,22 @@ module "function_app_to_blob_data_writer" {
     storage_account_name = module.cms_storage_account.name
     resource_group_name  = module.cms_storage_account.resource_group_name
     description          = "To allow migrating the activations of special services's"
-    role                 = "writer"
+    role                 = "owner"
+    container_name       = azurerm_storage_container.activations.name
+    }
+  ]
+}
+
+module "function_app_staging_slot_to_blob_data_writer" {
+  source          = "pagopa-dx/azure-role-assignments/azurerm"
+  version         = "~> 1.0"
+  principal_id    = module.cms_fn.function_app.function_app.slot.principal_id
+  subscription_id = data.azurerm_subscription.current.subscription_id
+  storage_blob = [{
+    storage_account_name = module.cms_storage_account.name
+    resource_group_name  = module.cms_storage_account.resource_group_name
+    description          = "To allow migrating the activations of special services's"
+    role                 = "owner"
     container_name       = azurerm_storage_container.activations.name
     }
   ]
