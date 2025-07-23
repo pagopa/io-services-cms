4964accb3bd888441631680ab80066e6c2cbd8a3
Leonardo De Marco <46130702+leopoldo94@users.noreply.github.com>
2025-07-23 17:21:17 +0200

    chore: add blob storage writer role to cms function app (#1317)
    <!--- Please always add a PR description as if nobody knows anything
    about the context these changes come from. -->
    <!--- Even if we are all from our internal team, we may not be on the
    same page. -->
    <!--- Write this PR as you were contributing to a public OSS project,
    where nobody knows you and you have to earn their trust. -->
    <!--- This will improve our projects in the long run! Thanks. -->
    
    #### List of Changes
    [add blob storage writer role to cms function
    app](https://github.com/pagopa/io-services-cms/commit/ff40df55e108d3bdee0a3ba4c7fc1b351bd38e4e)
    <!--- Describe your changes in detail -->
    
    #### Motivation and Context
    The cms function app lacks the role of blob writer for the storage
    account
    <!--- Why is this change required? What problem does it solve? -->
    
    #### How Has This Been Tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, tests ran to see how
    -->
    <!--- your change affects other areas of the code, etc. -->
    
    #### Screenshots (if appropriate):
    
    <!--- Attach screenshots in case changes impact UI. -->
    
    #### Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    
    ---------
    
    Co-authored-by: Rodolfo Viti <rodolfo.viti@pagopa.it>

--- infra/resources/_modules/cms_function_app/rbac.tf
+++ infra/resources/_modules/cms_function_app/rbac.tf
@@ -13,3 +13,34 @@ module "adf_to_blob_data_reader_db" {
     }
   ]
 }
+
+module "function_app_to_blob_data_writer" {
+  source  = "pagopa-dx/azure-role-assignments/azurerm"
+  version = "~> 1.0"
+
+  principal_id    = module.cms_fn.function_app.function_app.principal_id
+  subscription_id = data.azurerm_subscription.current.subscription_id
+  storage_blob = [{
+    storage_account_name = module.cms_storage_account.name
+    resource_group_name  = module.cms_storage_account.resource_group_name
+    description          = "To allow migrating the activations of special services's"
+    role                 = "owner"
+    container_name       = azurerm_storage_container.activations.name
+    }
+  ]
+}
+
+module "function_app_staging_slot_to_blob_data_writer" {
+  source          = "pagopa-dx/azure-role-assignments/azurerm"
+  version         = "~> 1.0"
+  principal_id    = module.cms_fn.function_app.function_app.slot.principal_id
+  subscription_id = data.azurerm_subscription.current.subscription_id
+  storage_blob = [{
+    storage_account_name = module.cms_storage_account.name
+    resource_group_name  = module.cms_storage_account.resource_group_name
+    description          = "To allow migrating the activations of special services's"
+    role                 = "owner"
+    container_name       = azurerm_storage_container.activations.name
+    }
+  ]
+}
