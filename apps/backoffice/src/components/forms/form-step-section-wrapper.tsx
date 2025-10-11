94e83a20c9ce34c5edd116dfc32265931a64ddfa
dcaccamo3ainformatica <d.caccamo@3ainformatica.it>
2025-10-12 01:06:05 +0200

    removal of markdown parsing from the form-step-section-wrapper component

--- apps/backoffice/src/components/forms/form-step-section-wrapper.tsx
+++ apps/backoffice/src/components/forms/form-step-section-wrapper.tsx
@@ -2,8 +2,6 @@ import { Box, Stack, Typography } from "@mui/material";
 import { useTranslation } from "next-i18next";
 import { ReactNode } from "react";
 
-import { MarkdownView } from "../markdown-view";
-
 export interface FormStepSectionWrapperProps {
   children: ReactNode;
   description?: string;
@@ -40,13 +38,8 @@ export const FormStepSectionWrapper = ({
         </Stack>
       )}
       {description && (
-        <Typography
-          color="text.secondary"
-          component="div" // Prevents Typography from rendering a <p>: MarkdownView already outputs <p>, otherwise we'd get nested <p> (validateDOMNesting)
-          marginBottom={2}
-          variant="body2"
-        >
-          <MarkdownView>{description}</MarkdownView>
+        <Typography color="text.secondary" marginBottom={2} variant="body2">
+          {description}
         </Typography>
       )}
       {children}
