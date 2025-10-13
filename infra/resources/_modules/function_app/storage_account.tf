d34b79903bde11163278ebfb370af89b25f1fe1f
Rodolfo Viti <62432865+rodoviti@users.noreply.github.com>
2025-10-13 17:48:35 +0200

    switch app-be sa reference (#1373)
    Resolves IOPAE-2206

--- infra/resources/_modules/function_app/storage_account.tf
+++ infra/resources/_modules/function_app/storage_account.tf
@@ -31,37 +31,10 @@ module "storage_account" {
   tags = var.tags
 }
 
-# resource "azurerm_storage_blob" "featured_services" {
-#   name                   = "featured-services.json"
-#   storage_account_name   = module.storage_account.name
-#   storage_container_name = "static-content"
-#   type                   = "Block"
-#   source                 = "${path.module}/featured-services.json"
-#   content_type           = "application/json"
-#   content_md5            = filemd5("${path.module}/featured-services.json")
-# }
-
-# resource "azurerm_storage_blob" "featured_institutions" {
-#   name                   = "featured-institutions.json"
-#   storage_account_name   = module.storage_account.name
-#   storage_container_name = "static-content"
-#   type                   = "Block"
-#   source                 = "${path.module}/featured-institutions.json"
-#   content_type           = "application/json"
-#   content_md5            = filemd5("${path.module}/featured-institutions.json")
-# }
-
-
-resource "azurerm_storage_container" "static_content" {
-  name                  = "static-content"
-  storage_account_id    = module.app_be_fn.storage_account.id
-  container_access_type = "private"
-}
-
 resource "azurerm_storage_blob" "featured_services" {
   name                   = "featured-services.json"
-  storage_account_name   = module.app_be_fn.storage_account.name
-  storage_container_name = azurerm_storage_container.static_content.name
+  storage_account_name   = module.storage_account.name
+  storage_container_name = "static-content"
   type                   = "Block"
   source                 = "${path.module}/featured-services.json"
   content_type           = "application/json"
@@ -70,8 +43,8 @@ resource "azurerm_storage_blob" "featured_services" {
 
 resource "azurerm_storage_blob" "featured_institutions" {
   name                   = "featured-institutions.json"
-  storage_account_name   = module.app_be_fn.storage_account.name
-  storage_container_name = azurerm_storage_container.static_content.name
+  storage_account_name   = module.storage_account.name
+  storage_container_name = "static-content"
   type                   = "Block"
   source                 = "${path.module}/featured-institutions.json"
   content_type           = "application/json"
