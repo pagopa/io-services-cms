11384612e655fae35e5f2fe43a5c8e000730e636
Rodolfo Viti <rodolfo.viti@pagopa.it>
2025-10-15 14:46:52 +0000

    scale-down CA tier

--- infra/resources/_modules/container_apps/backend_func.tf
+++ infra/resources/_modules/container_apps/backend_func.tf
@@ -15,7 +15,7 @@ module "backend_func_itn" {
   user_assigned_identity_id    = module.svc_container_app_environment_itn.user_assigned_identity.id
   target_port                  = 80
   revision_mode                = "Single"
-  tier                         = "l"
+  tier                         = "m"
 
   function_settings = {
     key_vault_name                         = var.key_vault.name
@@ -32,7 +32,7 @@ module "backend_func_itn" {
     http_scalers = [
       {
         name                = "http-scale-rule"
-        concurrent_requests = 1000
+        concurrent_requests = 75
       }
     ]
   }
