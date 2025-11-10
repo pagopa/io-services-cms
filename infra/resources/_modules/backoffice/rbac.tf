a44de53aa57159aea27e160044fc58560c36b41e
Rodolfo Viti <rodolfo.viti@pagopa.it>
2025-11-10 16:07:06 +0000

    add apim write permission to bo

---
+++ infra/resources/_modules/backoffice/rbac.tf
@@ -0,0 +1,26 @@
+module "bo_roles" {
+  source  = "pagopa-dx/azure-role-assignments/azurerm"
+  version = "~> 1.2"
+
+  principal_id    = module.backoffice.app_service.app_service.principal_id
+  subscription_id = data.azurerm_subscription.current.subscription_id
+  apim = [{
+    name                = local.apim.name
+    resource_group_name = local.apim.resource_group_name
+    description         = "To allow interaction with APIM"
+    role                = "writer"
+  }]
+}
+
+module "bo_staging_slot_roles" {
+  source          = "pagopa-dx/azure-role-assignments/azurerm"
+  version         = "~> 1.2"
+  principal_id    = module.backoffice.app_service.app_service.slot.principal_id
+  subscription_id = data.azurerm_subscription.current.subscription_id
+  apim = [{
+    name                = local.apim.name
+    resource_group_name = local.apim.resource_group_name
+    description         = "To allow interaction with APIM"
+    role                = "writer"
+  }]
+}
