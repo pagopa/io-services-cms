898d43434772379facd6b3891890c2f5aa3a0603
Rodolfo Viti <rodolfo.viti@pagopa.it>
2025-11-26 15:06:24 +0000

    fix apim mock config

--- apps/backoffice/mocks/data/apim-data.ts
+++ apps/backoffice/mocks/data/apim-data.ts
@@ -1,62 +1,5 @@
 import { faker } from "@faker-js/faker/locale/it";
 
-export const getOpenIdConfig = (
-  AZURE_CLIENT_SECRET_CREDENTIAL_TENANT_ID: string,
-) => ({
-  authorization_endpoint: `https://login.microsoftonline.com/${AZURE_CLIENT_SECRET_CREDENTIAL_TENANT_ID}/oauth2/v2.0/authorize`,
-  claims_supported: [
-    "sub",
-    "iss",
-    "cloud_instance_name",
-    "cloud_instance_host_name",
-    "cloud_graph_host_name",
-    "msgraph_host",
-    "aud",
-    "exp",
-    "iat",
-    "auth_time",
-    "acr",
-    "nonce",
-    "preferred_username",
-    "name",
-    "tid",
-    "ver",
-    "at_hash",
-    "c_hash",
-    "email",
-  ],
-  cloud_graph_host_name: "graph.windows.net",
-  cloud_instance_name: "microsoftonline.com",
-  device_authorization_endpoint: `https://login.microsoftonline.com/${AZURE_CLIENT_SECRET_CREDENTIAL_TENANT_ID}/oauth2/v2.0/devicecode`,
-  end_session_endpoint: `https://login.microsoftonline.com/${AZURE_CLIENT_SECRET_CREDENTIAL_TENANT_ID}/oauth2/v2.0/logout`,
-  frontchannel_logout_supported: true,
-  http_logout_supported: true,
-  id_token_signing_alg_values_supported: ["RS256"],
-  issuer: `https://login.microsoftonline.com/${AZURE_CLIENT_SECRET_CREDENTIAL_TENANT_ID}/v2.0`,
-  jwks_uri: `https://login.microsoftonline.com/${AZURE_CLIENT_SECRET_CREDENTIAL_TENANT_ID}/discovery/v2.0/keys`,
-  kerberos_endpoint: `https://login.microsoftonline.com/${AZURE_CLIENT_SECRET_CREDENTIAL_TENANT_ID}/kerberos`,
-  msgraph_host: "graph.microsoft.com",
-  rbac_url: "https://pas.windows.net",
-  request_uri_parameter_supported: false,
-  response_modes_supported: ["query", "fragment", "form_post"],
-  response_types_supported: [
-    "code",
-    "id_token",
-    "code id_token",
-    "id_token token",
-  ],
-  scopes_supported: ["openid", "profile", "email", "offline_access"],
-  subject_types_supported: ["pairwise"],
-  tenant_region_scope: "EU",
-  token_endpoint: `https://login.microsoftonline.com/${AZURE_CLIENT_SECRET_CREDENTIAL_TENANT_ID}/oauth2/v2.0/token`,
-  token_endpoint_auth_methods_supported: [
-    "client_secret_post",
-    "private_key_jwt",
-    "client_secret_basic",
-  ],
-  userinfo_endpoint: "https://graph.microsoft.com/oidc/userinfo",
-});
-
 export const anOauth2TokenResponse = {
   access_token: "MTQ0NjJkZmQ5OTM2NDE1ZTZjNGZmZjI3",
   expires_in: 3600,
@@ -159,45 +102,6 @@ const getUser = ({
   registrationDate: "2015-09-22T01:57:39.677Z",
   state: "active",
   type: "Microsoft.ApiManagement/service/users",
-});
-
-export const getDiscoveryInstanceResponse = (
-  AZURE_CLIENT_SECRET_CREDENTIAL_TENANT_ID: string,
-) => ({
-  "api-version": "1.1",
-  metadata: [
-    {
-      aliases: [
-        "login.microsoftonline.com",
-        "login.windows.net",
-        "login.microsoft.com",
-        "sts.windows.net",
-      ],
-      preferred_cache: "login.windows.net",
-      preferred_network: "login.microsoftonline.com",
-    },
-    {
-      aliases: ["login.partner.microsoftonline.cn", "login.chinacloudapi.cn"],
-      preferred_cache: "login.partner.microsoftonline.cn",
-      preferred_network: "login.partner.microsoftonline.cn",
-    },
-    {
-      aliases: ["login.microsoftonline.de"],
-      preferred_cache: "login.microsoftonline.de",
-      preferred_network: "login.microsoftonline.de",
-    },
-    {
-      aliases: ["login.microsoftonline.us", "login.usgovcloudapi.net"],
-      preferred_cache: "login.microsoftonline.us",
-      preferred_network: "login.microsoftonline.us",
-    },
-    {
-      aliases: ["login-us.microsoftonline.com"],
-      preferred_cache: "login-us.microsoftonline.com",
-      preferred_network: "login-us.microsoftonline.com",
-    },
-  ],
-  tenant_discovery_endpoint: `https://login.microsoftonline.com/${AZURE_CLIENT_SECRET_CREDENTIAL_TENANT_ID}/v2.0/.well-known/openid-configuration`,
 });
 
 export const getMockManageSubscriptionGroup = (id?: string) => ({
