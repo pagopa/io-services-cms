abf969449044e8fe806bca6385075395692992a3
Iustin <iustinburlacu@gmail.com>
2025-12-01 14:52:20 +0100

    rollback decoder test fiscal code

--- apps/io-services-cms-webapp/src/utils/filter-test-user.ts
+++ apps/io-services-cms-webapp/src/utils/filter-test-user.ts
@@ -1,39 +1,10 @@
-import { CommaSeparatedListOf } from "@pagopa/ts-commons/lib/comma-separated-list";
-import { readableReport } from "@pagopa/ts-commons/lib/reporters";
 import { FiscalCode } from "@pagopa/ts-commons/lib/strings";
-import * as E from "fp-ts/lib/Either";
-import { pipe } from "fp-ts/lib/function";
 import * as S from "fp-ts/lib/string";
-import * as t from "io-ts";
 import { string } from "io-ts";
 import { readonlySetFromArray } from "io-ts-types";
 
 import { TestFiscalCodeConfiguration } from "../config";
-
-const FiscalCodeSetDecoder = CommaSeparatedListOf(FiscalCode).pipe(
-  readonlySetFromArray(FiscalCode, S.Ord),
-);
-
-export const TestFiscalCodesUsersDecoder = new t.Type<
-  ReadonlySet<FiscalCode>,
-  string | undefined
->(
-  "TestFiscalCodesUsersDecoder",
-  (s): s is ReadonlySet<FiscalCode> => s instanceof Set,
-  (s, ctx) =>
-    s === undefined || s === ""
-      ? t.success(new Set<FiscalCode>())
-      : pipe(
-          FiscalCodeSetDecoder.decode(s),
-          E.fold(
-            (e) => t.failure(s, ctx, readableReport(e)),
-            (set) => t.success(set),
-          ),
-        ),
-  (set) => Array.from(set).join(","),
-);
-
-export const PrefixCfTestArrayDecoder = CommaSeparatedListOf(string);
+import { CommaSeparatedListOf } from "./comma-separated-list";
 
 /**
  * Function to check if a fiscal code is in the test users set or matches a fiscal code prefix.
@@ -48,3 +19,9 @@ export const isTestUser: (
   (cf) =>
     TEST_FISCAL_CODES.has(cf) ||
     PREFIX_CF_TEST.some((prefix) => cf.startsWith(prefix));
+
+export const TestFiscalCodesUsersDecoder = CommaSeparatedListOf(
+  FiscalCode,
+).pipe(readonlySetFromArray(FiscalCode, S.Ord));
+
+export const PrefixCfTestArrayDecoder = CommaSeparatedListOf(string);
