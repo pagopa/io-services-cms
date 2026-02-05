6f2ea43382b155393ad039aaa9c74df33d4a277d
salvatorelaiso <salvatorelaiso@gmail.com>
2026-02-05 16:35:00 +0100

    fix: remove infra changes

--- infra/resources/_modules/apim_external/main.tf
+++ infra/resources/_modules/apim_external/main.tf
@@ -24,28 +24,6 @@ resource "azurerm_api_management_api" "api_services_cms" {
 
 }
 
-resource "azurerm_api_management_api" "api_app_backend" {
-  name                = "io-app-backend-api"
-  resource_group_name = var.api_management.resource_group_name
-  api_management_name = var.api_management.name
-  revision            = "1"
-  display_name        = "IO Services App Backend"
-  path                = "api/v2"
-  protocols           = ["https"]
-  service_url         = "https://${var.app_backend_hostname}/api/v2"
-
-  import {
-    content_format = "openapi"
-    content_value = templatefile("${path.module}/api/io_app_backend/v2/_swagger.yaml.tpl",
-      {
-        host     = "api.io.pagopa.it",
-        basePath = "api/v2"
-      }
-    )
-  }
-
-}
-
 
 ###############
 # API Product #
@@ -56,13 +34,6 @@ resource "azurerm_api_management_product_api" "this" {
   api_name            = azurerm_api_management_api.api_services_cms.name
   api_management_name = azurerm_api_management_api.api_services_cms.api_management_name
   resource_group_name = azurerm_api_management_api.api_services_cms.resource_group_name
-}
-
-resource "azurerm_api_management_product_api" "app_backend" {
-  product_id          = var.api_management.product_id
-  api_name            = azurerm_api_management_api.api_app_backend.name
-  api_management_name = azurerm_api_management_api.api_app_backend.api_management_name
-  resource_group_name = azurerm_api_management_api.api_app_backend.resource_group_name
 }
 
 ##########
@@ -105,14 +76,6 @@ resource "azurerm_api_management_api_operation_policy" "get_service_topics" {
 }
 
 
-resource "azurerm_api_management_api_policy" "app_backend" {
-  api_name            = azurerm_api_management_api.api_app_backend.name
-  api_management_name = azurerm_api_management_api.api_app_backend.api_management_name
-  resource_group_name = azurerm_api_management_api.api_app_backend.resource_group_name
-
-  xml_content = file("${path.module}/api/io_app_backend/v2/policy.xml")
-}
-
 resource "azurerm_api_management_logger" "cache_policy_app_insights" {
   name                = "cache-policy-appinsight-apimlogger"
   api_management_name = azurerm_api_management_api.api_services_cms.api_management_name
@@ -149,13 +112,3 @@ resource "azurerm_api_management_api_diagnostic" "services_cms_api_app_insights"
     body_bytes = 32
   }
 }
-
-resource "azurerm_api_management_named_value" "app_backend_apim_key" {
-  name                = "appbe-host-key-for-apim-platform"
-  api_management_name = azurerm_api_management_api.api_app_backend.api_management_name
-  resource_group_name = azurerm_api_management_api.api_app_backend.resource_group_name
-  display_name        = "appbe-host-key-for-apim-platform"
-  value               = var.appbe_host_key_for_apim_platform
-  secret              = true
-}
-
