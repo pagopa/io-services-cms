22aaf45177b1a3d655f6d1f5934983e0cca6aef6
Rodolfo Viti <rodolfo.viti@pagopa.it>
2026-04-17 16:03:36 +0000

    add new 'external' sa to bo

--- infra/resources/_modules/backoffice/storage_account.tf
+++ infra/resources/_modules/backoffice/storage_account.tf
@@ -1,4 +1,4 @@
-module "cms_storage_account" {
+module "bo_ext_storage_account" {
   source  = "pagopa-dx/azure-storage-account/azurerm"
   version = "~> 2.0"
 
@@ -7,28 +7,16 @@ module "cms_storage_account" {
     env_short       = var.env_short
     location        = var.location
     domain          = var.domain
-    app_name        = local.app_name
+    app_name        = "${local.app_name}-ext"
     instance_number = "01"
   }
   resource_group_name = var.resource_group_name
-  use_case            = "default"
+  use_case            = "delegated_access"
   subnet_pep_id       = var.peps_snet_id
 
   subservices_enabled = {
-    blob  = true
-    queue = true
+    blob = true
   }
-
-  blob_features = {
-    versioning = true
-  }
-
-  # queues = values(local.queues)
-  queues = flatten([
-    for queue in local.queues : (
-      lookup(queue, "hasPoison", false) ? [queue.name, "${queue.name}-poison"] : [queue.name]
-    )
-  ])
 
   containers = values(local.containers)
 
