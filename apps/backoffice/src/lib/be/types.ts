b6df713695e0651e82d15be6c6800034a86761e0
salvatorelaiso <salvatorelaiso@gmail.com>
2026-04-17 18:40:52 +0200

    feat: add UUID type and validation tests

--- apps/backoffice/src/lib/be/types.ts
+++ apps/backoffice/src/lib/be/types.ts
@@ -1,4 +1,5 @@
 import { IntegerFromString } from "@pagopa/ts-commons/lib/numbers";
+import { PatternString } from "@pagopa/ts-commons/lib/strings";
 import { tag } from "@pagopa/ts-commons/lib/types";
 import * as t from "io-ts";
 
@@ -21,3 +22,11 @@ export const PositiveIntegerFromString = tag<IPositiveIntegerTag>()(
   t.refinement(IntegerFromString, (i) => i > 0, "PositiveIntegerFromString"),
 );
 type PositiveIntegerFromString = t.TypeOf<typeof PositiveIntegerFromString>;
+
+/**
+ * A string matching the UUID format
+ */
+export const UUID = PatternString(
+  "^[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}$",
+);
+type UUID = t.TypeOf<typeof UUID>;
