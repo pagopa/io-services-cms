777c4ce0eb9b852da5e5d081925c0db9ea8b62d8
salvatorelaiso <salvatorelaiso@gmail.com>
2026-04-27 12:09:15 +0200

    refactor: simplify subscription update logic in `syncSubscription`

--- apps/io-services-cms-webapp/src/utils/selfcare-group-change/sync-subscription.ts
+++ apps/io-services-cms-webapp/src/utils/selfcare-group-change/sync-subscription.ts
@@ -1,54 +1,37 @@
+import { SubscriptionGetResponse } from "@azure/arm-apimanagement";
 import { ApimUtils } from "@io-services-cms/external-clients";
-import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
+import * as O from "fp-ts/lib/Option";
 import * as TE from "fp-ts/lib/TaskEither";
 import { pipe } from "fp-ts/lib/function";
-import * as t from "io-ts";
 
-import { mapStateFromGroupToSubscription } from "./utils";
-
-export type GroupChangeEvent = t.TypeOf<typeof GroupChangeEvent>;
-export const GroupChangeEvent = t.type({
-  id: NonEmptyString,
-  institutionId: NonEmptyString,
-  name: NonEmptyString,
-  productId: NonEmptyString,
-  status: t.union([
-    t.literal("ACTIVE"),
-    t.literal("SUSPENDED"),
-    t.literal("DELETED"),
-  ]),
-});
+import { GroupChangeEvent } from "./types";
+import {
+  getSubscription,
+  mapStateFromGroupToSubscription,
+  updateSubscription,
+} from "./utils";
 
 export const syncSubscription =
   (apimService: ApimUtils.ApimService) =>
   (group: GroupChangeEvent): TE.TaskEither<Error, void> =>
     pipe(
-      apimService.getSubscription(
+      getSubscription(apimService)(
         ApimUtils.SUBSCRIPTION_MANAGE_GROUP_PREFIX + group.id,
       ),
       TE.chain((subscription) =>
-        subscription.displayName !== group.name ||
-        subscription.state !== mapStateFromGroupToSubscription(group.status)
-          ? apimService.updateSubscription(
-              subscription.name ?? "",
-              {
-                displayName: group.name,
-                state: mapStateFromGroupToSubscription(group.status),
-              },
-              subscription.eTag,
+        shouldUpdateSubscription(subscription, group)
+          ? pipe(
+              updateSubscription(apimService)(group),
+              TE.map(() => void 0),
             )
-          : TE.right(void 0),
-      ),
-      TE.map(() => void 0),
-      TE.orElse((e) =>
-        "statusCode" in e
-          ? e.statusCode === 404
-            ? TE.right(void 0)
-            : TE.left(
-                new Error(
-                  `Failed to update subcription ${group.id}, reason: ${JSON.stringify(e.details)}`,
-                ),
-              )
-          : TE.left(e),
+          : TE.of(void 0),
       ),
     );
+
+const shouldUpdateSubscription = (
+  subscription: O.Option<SubscriptionGetResponse>,
+  group: GroupChangeEvent,
+): boolean =>
+  O.isSome(subscription) &&
+  (subscription.value.displayName !== group.name ||
+    subscription.value.state !== mapStateFromGroupToSubscription(group.status));
