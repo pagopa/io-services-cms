1f74a975e00e5a80df9a3525fe684f9595f7330b
salvatorelaiso <salvatorelaiso@gmail.com>
2026-05-07 14:32:03 +0200

    refactor: simplify `SelfcareClient` interface by removing unused methods

--- packages/external-clients/src/selfcare-client/index.ts
+++ packages/external-clients/src/selfcare-client/index.ts
@@ -1,18 +1,14 @@
 import { Agent, HttpAgentConfig } from "@io-services-cms/fetch-utils";
 import { readableReport } from "@pagopa/ts-commons/lib/reporters";
-import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import axios, { AxiosError, AxiosInstance } from "axios";
 import * as E from "fp-ts/lib/Either";
 import * as TE from "fp-ts/lib/TaskEither";
-import { flow, identity, pipe } from "fp-ts/lib/function";
+import { flow, pipe } from "fp-ts/lib/function";
 import * as t from "io-ts";
 
 import { StateEnum } from "../generated/api/Group";
-import { DelegationWithPaginationResponse } from "../generated/selfcare/DelegationWithPaginationResponse";
 import { InstitutionResponse } from "../generated/selfcare/InstitutionResponse";
-import { PageOfUserGroupResource } from "../generated/selfcare/PageOfUserGroupResource";
 import { ProductResource } from "../generated/selfcare/ProductResource";
-import { UserGroupResource } from "../generated/selfcare/UserGroupResource";
 import { UserInstitutionResource } from "../generated/selfcare/UserInstitutionResource";
 
 export const UserInstitutions = t.readonlyArray(
@@ -30,32 +26,9 @@ export const InstitutionProducts = t.readonlyArray(
 export type InstitutionProducts = t.TypeOf<typeof InstitutionProducts>;
 
 export interface SelfcareClient {
-  getGroup: (
-    id: NonEmptyString,
-  ) => TE.TaskEither<AxiosError | Error, UserGroupResource>;
   getInstitutionById: (
     id: string,
   ) => TE.TaskEither<AxiosError | Error, InstitutionResponse>;
-  getInstitutionDelegations: (
-    institutionId: string,
-    size?: number,
-    page?: number,
-    search?: string,
-  ) => TE.TaskEither<Error, DelegationWithPaginationResponse>;
-  getInstitutionGroups: (
-    institutionId: string,
-    size?: number,
-    page?: number,
-    state?: GroupFilter,
-    parentInstitutionId?: string,
-  ) => TE.TaskEither<Error, PageOfUserGroupResource>;
-  getInstitutionProducts: (
-    institutionId: string,
-    userId: string,
-  ) => TE.TaskEither<Error, InstitutionProducts>;
-  getUserAuthorizedInstitutions: (
-    userId: string,
-  ) => TE.TaskEither<Error, UserInstitutions>;
 }
 
 const institutionsApi = "/institutions";
@@ -87,39 +60,6 @@ const buildSelfcareClient = (
 ): SelfcareClient => {
   const axiosInstance = getAxiosInstance(url, apiKey, httpAgentConfig);
 
-  const getUserAuthorizedInstitutions: SelfcareClient["getUserAuthorizedInstitutions"] =
-    (userId) =>
-      pipe(
-        TE.tryCatch(
-          () =>
-            axiosInstance.get(usersApi, {
-              params: {
-                size: 10000,
-                states: "ACTIVE",
-                userId,
-              },
-            }),
-          identity,
-        ),
-        TE.mapLeft((e) => {
-          if (axios.isAxiosError(e)) {
-            return new Error(`Axios error catched ${e.message}`);
-          } else {
-            return new Error(
-              `Error calling selfcare getUserAuthorizedInstitutions API: ${e}`,
-            );
-          }
-        }),
-        TE.chainW((response) =>
-          pipe(
-            response.data,
-            UserInstitutions.decode,
-            E.mapLeft(flow(readableReport, E.toError)),
-            TE.fromEither,
-          ),
-        ),
-      );
-
   const getInstitutionById: SelfcareClient["getInstitutionById"] = (id) =>
     pipe(
       TE.tryCatch(
@@ -143,135 +83,8 @@ const buildSelfcareClient = (
       ),
     );
 
-  const getInstitutionGroups: SelfcareClient["getInstitutionGroups"] = (
-    institutionId,
-    size?,
-    page?,
-    state = StateEnum.ACTIVE,
-    parentInstitutionId?,
-  ) =>
-    pipe(
-      TE.tryCatch(
-        () =>
-          axiosInstance.get(`${groupsApi}`, {
-            params: {
-              institutionId,
-              page,
-              parentInstitutionId,
-              size,
-              status: state === "*" ? undefined : state,
-            },
-          }),
-        flow(
-          E.fromPredicate(
-            axios.isAxiosError,
-            (e) => new Error(`Error calling selfcare getUserGroups API: ${e}`),
-          ),
-          E.toUnion,
-        ),
-      ),
-      TE.chainW((response) =>
-        pipe(
-          response.data,
-          PageOfUserGroupResource.decode,
-          E.mapLeft(flow(readableReport, E.toError)),
-          TE.fromEither,
-        ),
-      ),
-    );
-
-  const getGroup: SelfcareClient["getGroup"] = (id) =>
-    pipe(
-      TE.tryCatch(
-        () => axiosInstance.get(`${groupsApi}/${id}`),
-        flow(
-          E.fromPredicate(
-            axios.isAxiosError,
-            (e) => new Error(`Error calling selfcare getGroup API: ${e}`),
-          ),
-          E.toUnion,
-        ),
-      ),
-      TE.chainEitherK((response) =>
-        pipe(
-          response.data,
-          UserGroupResource.decode,
-          E.mapLeft((e) => pipe(e, readableReport, E.toError)),
-        ),
-      ),
-    );
-
-  const getInstitutionDelegations: SelfcareClient["getInstitutionDelegations"] =
-    (institutionId, size, page, search) =>
-      pipe(
-        TE.tryCatch(
-          () =>
-            axiosInstance.get(`${delegationsApi}/delegations-with-pagination`, {
-              params: {
-                brokerId: institutionId,
-                order: "ASC",
-                page,
-                search,
-                size,
-              },
-            }),
-          flow(
-            E.fromPredicate(
-              axios.isAxiosError,
-              (e) =>
-                new Error(`Error calling selfcare getDelegations API: ${e}`),
-            ),
-            E.toUnion,
-          ),
-        ),
-        TE.chainEitherK((response) =>
-          pipe(
-            response.data,
-            DelegationWithPaginationResponse.decode,
-            E.mapLeft((e) => pipe(e, readableReport, E.toError)),
-          ),
-        ),
-      );
-
-  const getInstitutionProducts: SelfcareClient["getInstitutionProducts"] = (
-    institutionId,
-    userId,
-  ) =>
-    pipe(
-      TE.tryCatch(
-        () =>
-          axiosInstance.get(`${institutionsApi}/${institutionId}/products`, {
-            params: {
-              userId,
-            },
-          }),
-        flow(
-          E.fromPredicate(
-            axios.isAxiosError,
-            (e) =>
-              new Error(
-                `Error calling selfcare getInstitutionProducts API: ${e}`,
-              ),
-          ),
-          E.toUnion,
-        ),
-      ),
-      TE.chainEitherK((response) =>
-        pipe(
-          response.data,
-          InstitutionProducts.decode,
-          E.mapLeft((e) => pipe(e, readableReport, E.toError)),
-        ),
-      ),
-    );
-
   return {
-    getGroup,
     getInstitutionById,
-    getInstitutionDelegations,
-    getInstitutionGroups,
-    getInstitutionProducts,
-    getUserAuthorizedInstitutions,
   };
 };
 
