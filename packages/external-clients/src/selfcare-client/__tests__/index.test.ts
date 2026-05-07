67d6b8ef56187d9e72a2d16fd5c2277610217fb2
Salvatore Laiso <32564922+salvatorelaiso@users.noreply.github.com>
2026-05-07 15:12:05 +0200

    [#IOPID-3842] Selfcare Client (#1523)
    Duplicate the Selfcare Client found in
    `apps/backoffice/src/lib/be/selfcare-client.ts` in the
    `@io-services-cms/external-clients` to be used from the
    `io-services-cms-webapp`.

---
+++ packages/external-clients/src/selfcare-client/__tests__/index.test.ts
@@ -0,0 +1,224 @@
+import * as E from "fp-ts/lib/Either";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import { getMockInstitutionProducts } from "../mocks/data/selfcare-data";
+import { TypeEnum } from "../../generated/selfcare/DelegationResponse";
+import { DelegationWithPaginationResponse } from "../../generated/selfcare/DelegationWithPaginationResponse";
+import { InstitutionResponse } from "../../generated/selfcare/InstitutionResponse";
+import { PageOfUserGroupResource } from "../../generated/selfcare/PageOfUserGroupResource";
+import { ProductResource } from "../../generated/selfcare/ProductResource";
+import { StatusEnum } from "../../generated/selfcare/UserGroupResource";
+import { getSelfcareClient, resetInstance, UserInstitutions } from "../index";
+import { HttpAgentConfig } from "@io-services-cms/fetch-utils";
+
+const mocks: {
+  institution: InstitutionResponse;
+  userInstitutions: UserInstitutions;
+  aSelfcareUserId: string;
+  institutionGroups: PageOfUserGroupResource;
+  delegations: DelegationWithPaginationResponse;
+} = {
+  institution: {
+    id: "institutionId",
+    origin: "IPA",
+    originId: "originId",
+  } as InstitutionResponse,
+  userInstitutions: [
+    {
+      institutionId: "institutionId1",
+      userId: "userId",
+    },
+    {
+      institutionId: "institutionId2",
+      userId: "userId",
+    },
+  ],
+  aSelfcareUserId: "aSelfcareUserId",
+  institutionGroups: {
+    content: [
+      {
+        description: "institutionGroups description",
+        id: "institutionGroupsID",
+        institutionId: "institutionGroupsInstID",
+        name: "institutionGroupsName",
+        productId: "institutionGroupsProdID",
+        status: "ACTIVE" as StatusEnum,
+      },
+      {
+        description: "institutionGroupsDescription2",
+        id: "institutionGroupsID2",
+        institutionId: "institutionGroupsInstID2",
+        name: "institutionGroupsName2",
+        productId: "institutionGroupsProdID2",
+        status: "DELETED" as StatusEnum,
+      },
+    ],
+    number: 0,
+    size: 0,
+    totalElements: 0,
+    totalPages: 0,
+  },
+  delegations: {
+    delegations: [
+      {
+        id: "delegationId1",
+        institutionId: "institutionGroupsInstID",
+        institutionName: "institutionGroupsName",
+        type: "PT" as TypeEnum,
+        productId: "productId1",
+        brokerId: "brokerId1",
+      },
+      {
+        id: "delegationId2",
+        institutionId: "institutionGroupsInstID2",
+        institutionName: "institutionGroupsName2",
+        type: "PT" as TypeEnum,
+        productId: "productId2",
+        brokerId: "brokerId2",
+      },
+    ],
+    pageInfo: {
+      pageNo: 0,
+      pageSize: 10,
+      totalElements: 2,
+      totalPages: 1,
+    },
+  },
+};
+
+const { create, isAxiosError, getMock } = vi.hoisted(() => {
+  const getMock = vi.fn();
+  return {
+    create: vi.fn(() => ({
+      get: getMock,
+    })),
+    getMock,
+    isAxiosError: vi.fn().mockReturnValue(false),
+  };
+});
+
+vi.mock("axios", async () => {
+  const actual = await vi.importActual("axios");
+  return {
+    ...(actual as any),
+    default: { create, isAxiosError },
+  };
+});
+
+vi.hoisted(() => {
+  const originalEnv = process.env;
+  process.env = {
+    ...originalEnv,
+    SELFCARE_EXTERNAL_API_BASE_URL: "https://example.com",
+    SELFCARE_API_KEY: "abc123",
+  };
+});
+
+const selfcareExternalApiBaseUrl = process.env
+  .SELFCARE_EXTERNAL_API_BASE_URL as string;
+const selfcareApiKey = process.env.SELFCARE_API_KEY as string;
+const httpAgentConfig: HttpAgentConfig = {
+  FETCH_KEEPALIVE_SOCKET_ACTIVE_TTL: 110000,
+  FETCH_KEEPALIVE_MAX_SOCKETS: 40,
+  FETCH_KEEPALIVE_MAX_FREE_SOCKETS: 10,
+  FETCH_KEEPALIVE_FREE_SOCKET_TIMEOUT: 30000,
+  FETCH_KEEPALIVE_TIMEOUT: 60000,
+  FETCH_KEEPALIVE_KEEPALIVE_MSECS: 1000,
+};
+
+beforeEach(() => {
+  vi.restoreAllMocks();
+  // reset selfcare client instance
+  resetInstance(selfcareExternalApiBaseUrl, selfcareApiKey, httpAgentConfig);
+});
+
+describe("Selfcare Client", () => {
+  const commonExpectation = () => {
+    expect(create).toHaveBeenCalledOnce();
+    expect(create).toHaveBeenCalledWith({
+      baseURL: process.env.SELFCARE_EXTERNAL_API_BASE_URL,
+      headers: {
+        "Ocp-Apim-Subscription-Key": process.env.SELFCARE_API_KEY,
+      },
+      httpAgent: expect.anything(),
+      httpsAgent: expect.anything(),
+      timeout: 5000,
+    });
+  };
+
+  describe("getInstitutionById", () => {
+    it("should return the institution found", async () => {
+      getMock.mockResolvedValueOnce({ status: 200, data: mocks.institution });
+
+      const result = await getSelfcareClient(
+        selfcareExternalApiBaseUrl,
+        selfcareApiKey,
+        httpAgentConfig,
+      ).getInstitutionById(mocks.institution.id as string)();
+
+      expect(getMock).toHaveBeenCalledWith(
+        `/institutions/${mocks.institution.id}`,
+      );
+      expect(isAxiosError).not.toHaveBeenCalled();
+      expect(E.isRight(result)).toBeTruthy();
+      if (E.isRight(result)) {
+        expect(result.right).toEqual(mocks.institution);
+      }
+      commonExpectation();
+    });
+
+    it.each`
+      scenario             | isAxiosErrorMockResponse | error
+      ${"an axios error"}  | ${true}                  | ${"axios error"}
+      ${"a generic error"} | ${false}                 | ${"non axios error"}
+    `(
+      "should return an error when received $scenario from selfcare",
+      async ({ isAxiosErrorMockResponse, error }) => {
+        // given
+        isAxiosError.mockReturnValueOnce(isAxiosErrorMockResponse);
+        getMock.mockRejectedValueOnce(error);
+
+        // when
+        const result = await getSelfcareClient(
+          selfcareExternalApiBaseUrl,
+          selfcareApiKey,
+          httpAgentConfig,
+        ).getInstitutionById(mocks.institution.id as string)();
+
+        // then
+        expect(getMock).toHaveBeenCalledOnce();
+        expect(getMock).toHaveBeenCalledWith(
+          `/institutions/${mocks.institution.id}`,
+        );
+        expect(isAxiosError).toHaveBeenCalledOnce();
+        expect(E.isLeft(result)).toBeTruthy();
+        if (E.isLeft(result)) {
+          expect(result.left).toStrictEqual(
+            isAxiosErrorMockResponse
+              ? error
+              : new Error(
+                  `Error calling selfcare getInstitutionById API: ${error}`,
+                ),
+          );
+        }
+        commonExpectation();
+      },
+    );
+
+    it("should return an error when received a bad response from selfcare", async () => {
+      getMock.mockResolvedValueOnce({ status: 200, data: "" });
+
+      const result = await getSelfcareClient(
+        selfcareExternalApiBaseUrl,
+        selfcareApiKey,
+        httpAgentConfig,
+      ).getInstitutionById(mocks.institution.id as string)();
+
+      expect(getMock).toHaveBeenCalledWith(
+        `/institutions/${mocks.institution.id}`,
+      );
+      expect(isAxiosError).not.toHaveBeenCalled();
+      expect(E.isLeft(result)).toBeTruthy();
+      commonExpectation();
+    });
+  });
+});
---
+++ packages/external-clients/src/selfcare-client/index.ts
@@ -0,0 +1,105 @@
+import { Agent, HttpAgentConfig } from "@io-services-cms/fetch-utils";
+import { readableReport } from "@pagopa/ts-commons/lib/reporters";
+import axios, { AxiosError, AxiosInstance } from "axios";
+import * as E from "fp-ts/lib/Either";
+import * as TE from "fp-ts/lib/TaskEither";
+import { flow, pipe } from "fp-ts/lib/function";
+import * as t from "io-ts";
+
+import { StateEnum } from "../generated/api/Group";
+import { InstitutionResponse } from "../generated/selfcare/InstitutionResponse";
+import { ProductResource } from "../generated/selfcare/ProductResource";
+import { UserInstitutionResource } from "../generated/selfcare/UserInstitutionResource";
+
+export const UserInstitutions = t.readonlyArray(
+  UserInstitutionResource,
+  "UserInstitutions",
+);
+export type UserInstitutions = t.TypeOf<typeof UserInstitutions>;
+
+export type GroupFilter = "*" | StateEnum;
+
+export const InstitutionProducts = t.readonlyArray(
+  ProductResource,
+  "InstitutionProducts",
+);
+export type InstitutionProducts = t.TypeOf<typeof InstitutionProducts>;
+
+export interface SelfcareClient {
+  getInstitutionById: (
+    id: string,
+  ) => TE.TaskEither<AxiosError | Error, InstitutionResponse>;
+}
+
+const institutionsApi = "/institutions";
+let selfcareClient: SelfcareClient;
+
+const getAxiosInstance = (
+  url: string,
+  apiKey: string,
+  httpAgentConfig: HttpAgentConfig,
+): AxiosInstance => {
+  const endpoint = `${url}`;
+
+  return axios.create({
+    baseURL: endpoint,
+    headers: { "Ocp-Apim-Subscription-Key": apiKey },
+    httpAgent: Agent.getHttpAgent(httpAgentConfig),
+    httpsAgent: Agent.getHttpsAgent(httpAgentConfig),
+    timeout: 5000,
+  });
+};
+
+const buildSelfcareClient = (
+  url: string,
+  apiKey: string,
+  httpAgentConfig: HttpAgentConfig,
+): SelfcareClient => {
+  const axiosInstance = getAxiosInstance(url, apiKey, httpAgentConfig);
+
+  const getInstitutionById: SelfcareClient["getInstitutionById"] = (id) =>
+    pipe(
+      TE.tryCatch(
+        () => axiosInstance.get(`${institutionsApi}/${id}`),
+        flow(
+          E.fromPredicate(
+            axios.isAxiosError,
+            (e) =>
+              new Error(`Error calling selfcare getInstitutionById API: ${e}`),
+          ),
+          E.toUnion,
+        ),
+      ),
+      TE.chainW((response) =>
+        pipe(
+          response.data,
+          InstitutionResponse.decode,
+          E.mapLeft(flow(readableReport, E.toError)),
+          TE.fromEither,
+        ),
+      ),
+    );
+
+  return {
+    getInstitutionById,
+  };
+};
+
+export const getSelfcareClient = (
+  url: string,
+  apiKey: string,
+  httpAgentConfig: HttpAgentConfig,
+): SelfcareClient => {
+  if (!selfcareClient) {
+    selfcareClient = buildSelfcareClient(url, apiKey, httpAgentConfig);
+  }
+  return selfcareClient;
+};
+
+export const resetInstance = (
+  url: string,
+  apiKey: string,
+  httpAgentConfig: HttpAgentConfig,
+) => {
+  selfcareClient = buildSelfcareClient(url, apiKey, httpAgentConfig);
+};
