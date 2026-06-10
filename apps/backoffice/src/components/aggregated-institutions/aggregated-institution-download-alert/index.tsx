de5dce1c1344d63ad7b92a6cc053425b336b88ce
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2026-06-10 11:21:44 +0200

    Format date in AggregatedInstitutionDownloadAlert component

--- apps/backoffice/src/components/aggregated-institutions/aggregated-institution-download-alert/index.tsx
+++ apps/backoffice/src/components/aggregated-institutions/aggregated-institution-download-alert/index.tsx
@@ -100,10 +100,14 @@ export const AggregatedInstitutionDownloadAlert = ({
                   {
                     expirationDate: new Intl.DateTimeFormat(i18n.language, {
                       day: "2-digit",
+                      hour: "2-digit",
+                      hour12: false,
+                      minute: "2-digit",
                       month: "2-digit",
+                      second: "2-digit",
                       year: "numeric",
                     })
-                      .format(new Date(data.expirationDate ?? ""))
+                      .format(new Date(data.expirationDate))
                       .replace(/\//g, "-"),
                   },
                 )}
