be5bc03456faa0cd3b558e9f556a3dfa76ce07be
Danilo Spinelli <danilo.spinelli@pagopa.it>
2026-06-16 12:54:04 +0200

    Fix Event Hub managed identity role assignments

--- infra/resources/_modules/eventhub/rbac.tf
+++ infra/resources/_modules/eventhub/rbac.tf
@@ -2,36 +2,26 @@ locals {
   cms_event_hub_names = [
     for eventhub in local.evhns.eventhubs : replace(module.eventhub.name, "-evhns-", "-${eventhub.name}-")
   ]
+
+  cms_event_hub_ids = {
+    for event_hub_name in local.cms_event_hub_names : event_hub_name => module.eventhub.hub_ids[event_hub_name]
+  }
 }
 
-module "cms_fn_event_hub_roles" {
-  source  = "pagopa-dx/azure-role-assignments/azurerm"
-  version = "~> 1.2"
+resource "azurerm_role_assignment" "cms_fn_event_hub_data_sender" {
+  for_each = local.cms_event_hub_ids
 
-  principal_id    = var.cms_fn_principal_id
-  subscription_id = data.azurerm_subscription.current.subscription_id
-
-  event_hub = [{
-    namespace_name      = module.eventhub.name
-    resource_group_name = var.resource_group_name
-    event_hub_names     = local.cms_event_hub_names
-    role                = "writer"
-    description         = "Allow CMS Function App to send service ingestion events"
-  }]
+  scope                = each.value
+  role_definition_name = "Azure Event Hubs Data Sender"
+  principal_id         = var.cms_fn_principal_id
+  description          = "Allow CMS Function App to send service ingestion events"
 }
 
-module "cms_fn_staging_slot_event_hub_roles" {
-  source  = "pagopa-dx/azure-role-assignments/azurerm"
-  version = "~> 1.2"
+resource "azurerm_role_assignment" "cms_fn_staging_slot_event_hub_data_sender" {
+  for_each = local.cms_event_hub_ids
 
-  principal_id    = var.cms_fn_staging_slot_principal_id
-  subscription_id = data.azurerm_subscription.current.subscription_id
-
-  event_hub = [{
-    namespace_name      = module.eventhub.name
-    resource_group_name = var.resource_group_name
-    event_hub_names     = local.cms_event_hub_names
-    role                = "writer"
-    description         = "Allow CMS Function App staging slot to send service ingestion events"
-  }]
+  scope                = each.value
+  role_definition_name = "Azure Event Hubs Data Sender"
+  principal_id         = var.cms_fn_staging_slot_principal_id
+  description          = "Allow CMS Function App staging slot to send service ingestion events"
 }
