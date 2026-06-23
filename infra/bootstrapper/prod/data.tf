f75cd08a859db094e91a4f2eb6da32794e1310ce
Danilo Spinelli <gunzip@users.noreply.github.com>
2026-06-23 12:49:30 +0200

    Add permissions for legacy Cosmos DB (#1588)
    Introduce permissions for the legacy Cosmos DB account, including the
    necessary role assignments and local configurations. This enhances
    access control for the infrastructure.

--- infra/bootstrapper/prod/data.tf
+++ infra/bootstrapper/prod/data.tf
@@ -7,3 +7,8 @@ data "azurerm_key_vault_secret" "slack_webhook_url" {
   key_vault_id = data.azurerm_key_vault.svc_kv.id
   name         = "slack-webhook-url-terraform-drift"
 }
+
+data "azurerm_cosmosdb_account" "legacy_api" {
+  name                = local.legacy_cosmosdb.name
+  resource_group_name = local.legacy_cosmosdb.resource_group_name
+}
