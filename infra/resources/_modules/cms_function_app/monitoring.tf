833f054c5eb6f173663eb2a3a95dfaac9b8565ec
Gianmaria Scorza <gianmaria@scorza.dev>
2026-07-03 15:55:54 +0200

    fix unwanted behaviour on app service plang scaling (#1601)
    Remove memory-based scaling rule to prevent instances getting stuck at
    max capacity.

--- infra/resources/_modules/cms_function_app/monitoring.tf
+++ infra/resources/_modules/cms_function_app/monitoring.tf
@@ -40,20 +40,6 @@ module "function_profile_autoscale" {
       decrease_by               = 1
       cooldown_decrease         = 5
     }
-    memory = {
-      upper_threshold           = 85
-      statistic_increase        = "Average"
-      time_aggregation_increase = "Average"
-      time_window_increase      = 1
-      increase_by               = 1
-      cooldown_increase         = 5
-      lower_threshold           = 55
-      statistic_decrease        = "Average"
-      time_aggregation_decrease = "Average"
-      time_window_decrease      = 5
-      decrease_by               = 1
-      cooldown_decrease         = 5
-    }
     requests = {
       upper_threshold           = 500
       statistic_increase        = "Max"
