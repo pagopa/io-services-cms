d559952c01606dcbaa7e510f13d1a51ea3fb55e8
Luca Mancosu <luca.mancosu@it.ey.com>
2026-07-06 11:39:05 +0200

    update comment

--- infra/resources/_modules/container_apps/services_app.tf
+++ infra/resources/_modules/container_apps/services_app.tf
@@ -1,8 +1,9 @@
 module "services_ca" {
-  /* Cannot use the 4.0 version of the module because the backend_func_itn uses an old azure_container_app
-  module with azapi ~>2.7.0 constraint while the new pagopa-dx/azure-container-app/azurerm 5.0 requires the 
-  azapi ~>2.9.0. Also we cannot update the backend_func_itn module because its use_case type(function_app) is
-  no longer supported on the newest azure_container_app module.
+  /* Cannot use the 5.0 version of the pagopa-dx/azure-container-app/azurerm module because the 
+  backend_func_itn uses an old azure_container_app module with azapi ~>2.7.0 constraint while 
+  the new pagopa-dx/azure-container-app/azurerm 5.0 requires the azapi ~>2.9.0. Also we cannot 
+  update the backend_func_itn module because its use_case type(function_app) is no longer supported 
+  on the newest azure_container_app module.
   */
   source  = "pagopa-dx/azure-container-app/azurerm"
   version = "~> 4.0"
