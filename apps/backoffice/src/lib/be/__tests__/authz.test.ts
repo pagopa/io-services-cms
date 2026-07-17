9c138d622da9a5eda2d35790027e3d5ce0778de5
Rodolfo Viti <rodolfo.viti@pagopa.it>
2026-07-17 07:11:37 +0000

    add/fix unit tests

--- apps/backoffice/src/lib/be/__tests__/authz.test.ts
+++ apps/backoffice/src/lib/be/__tests__/authz.test.ts
@@ -1,77 +1,7 @@
-import { faker } from "@faker-js/faker/locale/it";
-import { afterEach, describe, expect, it, vi } from "vitest";
+import { describe, expect, it } from "vitest";
 import { SelfcareRoles } from "../../../types/auth";
-import { isAdmin, isInstitutionIdSameAsCaller, userAuthz } from "../authz";
+import { userAuthz } from "../authz";
 import { BackOfficeUserEnriched } from "../wrappers";
-
-const backofficeUserMock: BackOfficeUserEnriched = {
-  id: faker.string.uuid(),
-  institution: {
-    id: "institutionId",
-    name: faker.company.name(),
-    fiscalCode: faker.string.numeric(),
-    role: SelfcareRoles.admin,
-    logo_url: faker.image.url(),
-    isAggregator: false,
-    isAggregate: false,
-    selcSpecialGroups: [],
-  },
-  permissions: {
-    apimGroups: faker.helpers.multiple(faker.string.alpha),
-    selcGroups: [],
-  },
-  parameters: {
-    userId: faker.string.uuid(),
-    userEmail: faker.internet.email(),
-    subscriptionId: faker.string.uuid(),
-  },
-};
-
-const differentBackofficeOperatorUserMock: BackOfficeUserEnriched = {
-  ...backofficeUserMock,
-  institution: {
-    id: "differentInstitutionId",
-    name: faker.company.name(),
-    fiscalCode: faker.string.numeric(),
-    role: SelfcareRoles.operator,
-    logo_url: faker.image.url(),
-    isAggregator: false,
-    isAggregate: false,
-    selcSpecialGroups: [],
-  },
-};
-
-const institutionId = "institutionId";
-
-afterEach(() => {
-  vi.resetAllMocks();
-  vi.restoreAllMocks();
-});
-
-describe("isAdmin", () => {
-  it('should return true for "admin" user', () => {
-    expect(isAdmin(backofficeUserMock)).toBe(true);
-  });
-  it('should return false for "operator" user', () => {
-    expect(isAdmin(differentBackofficeOperatorUserMock)).toBe(false);
-  });
-});
-
-describe("isInstitutionSameAsCaller", () => {
-  it("should return true if caller institutionId is the same as user", () => {
-    expect(isInstitutionIdSameAsCaller(backofficeUserMock, institutionId)).toBe(
-      true,
-    );
-  });
-  it("should return false if caller institutionId is not the same as user", () => {
-    expect(
-      isInstitutionIdSameAsCaller(
-        differentBackofficeOperatorUserMock,
-        institutionId,
-      ),
-    ).toBe(false);
-  });
-});
 
 describe("userAuthz", () => {
   describe("isAdmin", () => {
@@ -150,39 +80,10 @@ describe("userAuthz", () => {
     );
   });
 
-  describe("isGroupAllowed", () => {
-    it.each`
-      scenario                                                                                                                               | expectedResult | role                      | selcGroups                                                    | groupId                | active
-      ${"is admin"}                                                                                                                          | ${true}        | ${SelfcareRoles.admin}    | ${[]}                                                         | ${""}                  | ${undefined}
-      ${"is not admin and selcGroups is empty"}                                                                                              | ${true}        | ${SelfcareRoles.operator} | ${[]}                                                         | ${""}                  | ${undefined}
-      ${"is not admin and provided groupId is included in selcGroups"}                                                                       | ${true}        | ${SelfcareRoles.operator} | ${["groupId"]}                                                | ${"groupId"}           | ${undefined}
-      ${"is not admin and provided groupId is not included in selcGroups"}                                                                   | ${false}       | ${SelfcareRoles.operator} | ${["groupId"]}                                                | ${"different_groupId"} | ${undefined}
-      ${"is not admin and provided groupId is included in selcGroups enriched"}                                                              | ${true}        | ${SelfcareRoles.operator} | ${[{ id: "groupId", name: "groupName", state: "ACTIVE" }]}    | ${"groupId"}           | ${false}
-      ${"is not admin and provided groupId is not included in selcGroups enriched"}                                                          | ${false}       | ${SelfcareRoles.operator} | ${[{ id: "groupId", name: "groupName", state: "ACTIVE" }]}    | ${"different_groupId"} | ${false}
-      ${"is not admin and provided groupId is included in selcGroups enriched and the group is not active"}                                  | ${false}       | ${SelfcareRoles.operator} | ${[{ id: "groupId", name: "groupName", state: "SUSPENDED" }]} | ${"groupId"}           | ${true}
-      ${"is not admin and provided groupId is included in selcGroups enriched and the group is not active but the flag active is false"}     | ${true}        | ${SelfcareRoles.operator} | ${[{ id: "groupId", name: "groupName", state: "SUSPENDED" }]} | ${"groupId"}           | ${false}
-      ${"is not admin and provided groupId is included in selcGroups enriched and the group is not active but the flag active is undefined"} | ${true}        | ${SelfcareRoles.operator} | ${[{ id: "groupId", name: "groupName", state: "SUSPENDED" }]} | ${"groupId"}           | ${undefined}
-      ${"is not admin and provided groupId is not included in selcGroups enriched the group is active"}                                      | ${false}       | ${SelfcareRoles.operator} | ${[{ id: "groupId", name: "groupName", state: "ACTIVE" }]}    | ${"different_groupId"} | ${true}
-    `(
-      "should return $expectedResult when $scenario",
-      ({ expectedResult, role, selcGroups, groupId, active }) => {
-        expect(
-          userAuthz({
-            institution: { role },
-            permissions: { selcGroups },
-          } as unknown as BackOfficeUserEnriched).isGroupAllowed(
-            groupId,
-            active,
-          ),
-        ).toBe(expectedResult);
-      },
-    );
-  });
-
   describe("isUserAllowedOnGroup", () => {
     it.each`
       scenario                                                                                             | expectedResult | selcGroups                                                    | groupId                | checkActive
-      ${"selcGroups is empty"}                                                                             | ${false}       | ${[]}                                                         | ${"groupId"}           | ${undefined}
+      ${"selcGroups is empty"}                                                                             | ${true}        | ${[]}                                                         | ${"groupId"}           | ${undefined}
       ${"groupId is included in selcGroups as string"}                                                     | ${true}        | ${["groupId"]}                                                | ${"groupId"}           | ${undefined}
       ${"groupId is not included in selcGroups as string"}                                                 | ${false}       | ${["groupId"]}                                                | ${"different_groupId"} | ${undefined}
       ${"groupId is included in enriched selcGroups and active check is not required"}                     | ${true}        | ${[{ id: "groupId", name: "groupName", state: "SUSPENDED" }]} | ${"groupId"}           | ${undefined}
--- apps/backoffice/src/lib/be/authz.ts
+++ apps/backoffice/src/lib/be/authz.ts
@@ -1,22 +1,7 @@
 import { StateEnum } from "@/generated/api/Group";
 import { SelfcareRoles } from "@/types/auth";
 
-import { BackOfficeUser } from "../../../types/next-auth";
 import { BackOfficeUserEnriched } from "./wrappers";
-
-/**
- * @deprecated use userAuthz.isAdmin instead
- */
-export const isAdmin = (user: BackOfficeUser): boolean =>
-  user.institution.role === SelfcareRoles.admin;
-
-/**
- * @deprecated use userAuthz.isInstitutionAllowed instead
- */
-export const isInstitutionIdSameAsCaller = (
-  user: BackOfficeUser,
-  institutionId: string,
-): boolean => user.institution.id === institutionId;
 
 export const userAuthz = (user: BackOfficeUserEnriched) => {
   const isAdmin = (): boolean => user.institution.role === SelfcareRoles.admin;
@@ -65,22 +50,6 @@ export const userAuthz = (user: BackOfficeUserEnriched) => {
     isAnInstitutionSpecialGroup: (groupId: string): boolean =>
       user.institution.selcSpecialGroups.some((group) => group.id === groupId),
 
-    // TODO: refactor this method and its usage to simplify the logic of the permission check.
-    /**
-     * Checks if a group is allowed based on user permissions, optionally verifying its active state
-     * @param groupId - The group identifier to check
-     * @param checkActive - Optional flag to enforce active state check on group
-     * @returns Whether the group is permitted for the current user
-     */
-    isGroupAllowed: (groupId: string, checkActive?: boolean): boolean => {
-      const { selcGroups } = user.permissions;
-      if (isAdmin() || selcGroups.length === 0) {
-        return true;
-      }
-
-      return isUserAllowedOnGroup(groupId, checkActive);
-    },
-
     /**
      * Check if the provided institutionId is allowed by the user
      * @param institutionId the instutution id
@@ -94,7 +63,6 @@ export const userAuthz = (user: BackOfficeUserEnriched) => {
      * @param groupId - The group identifier to check
      * @param checkActive - Optional flag to enforce active state check on group
      * @returns Whether the user is permitted for the specified group
-     * @note This method does not consider the case of users without groups, which are allowed to access all groups. It strictly checks if the user has explicit permission for the given group.
      */
     isUserAllowedOnGroup: isUserAllowedOnGroup,
   };
